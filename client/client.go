5064d9c76e96cfb400394f3647cf275079fcc024
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2017-06-14 21:23:47 +0200

    gRPC-connect from workhorse

--- client/client.go
+++ client/client.go
@@ -1,7 +1,10 @@
 package client
 
 import (
+	"fmt"
 	"net"
+	"net/url"
+	"strings"
 	"time"
 
 	"google.golang.org/grpc"
@@ -14,13 +17,7 @@ type Client struct {
 
 // NewClient connects to a gRPC-server and returns the Client
 func NewClient(addr string) (*Client, error) {
-	connOpts := []grpc.DialOption{
-		grpc.WithInsecure(),
-		grpc.WithDialer(func(addr string, _ time.Duration) (net.Conn, error) {
-			return net.Dial("unix", addr)
-		}),
-	}
-	conn, err := grpc.Dial(addr, connOpts...)
+	conn, err := newConnection(addr)
 	if err != nil {
 		return nil, err
 	}
@@ -31,4 +28,52 @@ func NewClient(addr string) (*Client, error) {
 // Close the gRPC-connection
 func (cli *Client) Close() error {
 	return cli.conn.Close()
+}
+
+// Thankfully borrowed from Workhorse
+
+func newConnection(rawAddress string) (*grpc.ClientConn, error) {
+	network, addr, err := parseAddress(rawAddress)
+	if err != nil {
+		return nil, err
+	}
+
+	connOpts := []grpc.DialOption{
+		grpc.WithInsecure(), // Since we're connecting to Gitaly over UNIX, we don't need to use TLS credentials.
+		grpc.WithDialer(func(a string, _ time.Duration) (net.Conn, error) {
+			return net.Dial(network, a)
+		}),
+	}
+	conn, err := grpc.Dial(addr, connOpts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func parseAddress(rawAddress string) (network, addr string, err error) {
+	// Parsing unix:// URL's with url.Parse does not give the result we want
+	// so we do it manually.
+	for _, prefix := range []string{"unix://", "unix:"} {
+		if strings.HasPrefix(rawAddress, prefix) {
+			return "unix", strings.TrimPrefix(rawAddress, prefix), nil
+		}
+	}
+
+	u, err := url.Parse(rawAddress)
+	if err != nil {
+		return "", "", err
+	}
+
+	if u.Scheme != "tcp" {
+		return "", "", fmt.Errorf("unknown scheme: %q", rawAddress)
+	}
+	if u.Host == "" {
+		return "", "", fmt.Errorf("network tcp requires host: %q", rawAddress)
+	}
+	if u.Path != "" {
+		return "", "", fmt.Errorf("network tcp should have no path: %q", rawAddress)
+	}
+	return "tcp", u.Host, nil
 }
