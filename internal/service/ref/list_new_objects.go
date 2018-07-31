a294acb3d6311ee4c02fa1627bef5fcb0eec1098
Rubén Dávila <ruben@gitlab.com>
2018-07-31 00:56:05 -0500

    First impl of ListNewObjects

---
+++ internal/service/ref/list_new_objects.go
@@ -0,0 +1,64 @@
+package ref
+
+import (
+	"bufio"
+	"regexp"
+	"strings"
+
+	pb "gitlab.com/gitlab-org/gitaly-proto/go"
+	"gitlab.com/gitlab-org/gitaly/internal/git"
+	"gitlab.com/gitlab-org/gitaly/internal/git/catfile"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *server) ListNewObjects(in *pb.ListNewObjectsRequest, stream pb.RefService_ListNewObjectsServer) error {
+	oid := in.GetCommitId()
+	if match, err := regexp.MatchString(`\A[0-9a-f]{40}\z`, oid); !match || err != nil {
+		return status.Errorf(codes.InvalidArgument, "commit id shoud have 40 hexidecimal characters")
+	}
+
+	ctx := stream.Context()
+	revList, err := git.Command(ctx, in.GetRepository(), "rev-list", oid, "--objects", "--all")
+	if err != nil {
+		if _, ok := status.FromError(err); ok {
+			return err
+		}
+		return status.Errorf(codes.Internal, "ListNewObjects: gitCommand: %v", err)
+	}
+
+	batch, err := catfile.New(ctx, in.GetRepository())
+	if err != nil {
+		return status.Errorf(codes.Internal, "ListNewObjects: catfile: %v", err)
+	}
+
+	i := 0
+	var newBlobs []*pb.NewBlobObject
+	scanner := bufio.NewScanner(revList)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Fields(line)
+
+		if len(parts) == 2 {
+			info, err := batch.Info(parts[0])
+			if err != nil {
+				return status.Errorf(codes.Internal, "ListNewObjects: catfile: %v", err)
+			}
+
+			path := []byte(parts[1])
+
+			newBlobs = append(newBlobs, &pb.NewBlobObject{Oid: info.Oid, Size: info.Size, Path: path,})
+		}
+
+		if i%10 == 0 {
+			response := &pb.ListNewObjectsResponse{NewBlobObjects: newBlobs}
+			stream.Send(response)
+			newBlobs = newBlobs[:0]
+		}
+	}
+
+	response := &pb.ListNewObjectsResponse{NewBlobObjects: newBlobs}
+	stream.Send(response)
+
+	return revList.Wait()
+}
