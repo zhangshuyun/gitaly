0fa8caf1a1d393bb4dcfb1ef82483e0c2985a544
Jacob Vosmaer <jacob@gitlab.com>
2018-10-16 18:44:41 +0200

    Add example for "leave pool" scenario

---
+++ helper.rb
@@ -0,0 +1,28 @@
+def run_pipeline(pipeline, dir)
+  warn "#{File.basename(dir)}$ #{pipeline.map { |c| c.join(' ') }.join(' | ')}"
+
+  statuses = Open3.pipeline(*pipeline, chdir: dir)
+
+  statuses.all? { |s| s && s.success? }
+end
+
+def run_pipeline!(pipeline, dir)
+  abort "failed" unless run_pipeline(pipeline, dir)
+end
+
+# Note: tricks with the 'dir' argument and File.basename are there only
+# to make the script output prettier.
+def run!(cmd, dir=nil)
+ abort "failed" unless run(cmd, dir)
+end
+
+def run(cmd, dir=nil)
+  dir ||= Dir.pwd
+  cmd_s = cmd.join(' ')
+  warn "#{File.basename(dir)}$ #{cmd_s}"
+  start = Time.now
+  status = system(*cmd, chdir: dir)
+  delta = Time.now - start
+  warn sprintf("time: %.3fs\n", delta) if delta > 1.0
+  status
+end
