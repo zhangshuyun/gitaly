f9ac26855fbdb46813aa71288e4590472c52b7a3
Jacob Vosmaer <jacob@gitlab.com>
2019-04-10 17:22:41 +0200

    Remove testcase layer

--- internal/git/packfile_test.go
+++ internal/git/packfile_test.go
@@ -9,39 +9,33 @@ import (
 )
 
 type packExample struct {
+	desc    string
 	n       uint32
 	raw     []byte
 	content []byte
 }
 
-var smallPackExample = &packExample{
-	n:       3,
-	raw:     []byte("PACK\x00\x00\x00\x02\x00\x00\x00\x03hello=?\x1A$\xB3\x8F\xCC\x96\xE0\xB0\xAC\xF0\x93\t\x85\xD8\x87K\xC5p"),
-	content: []byte("hello"),
+var packExamples = []packExample{
+	{
+		desc:    "small, 3 objects",
+		n:       3,
+		raw:     []byte("PACK\x00\x00\x00\x02\x00\x00\x00\x03hello=?\x1A$\xB3\x8F\xCC\x96\xE0\xB0\xAC\xF0\x93\t\x85\xD8\x87K\xC5p"),
+		content: []byte("hello"),
+	},
 }
 
 func TestPackReader(t *testing.T) {
-	testCases := []struct {
-		desc string
-		pack *packExample
-	}{
-		{
-			desc: "packfile with 3 objects",
-			pack: smallPackExample,
-		},
-	}
-
-	for _, tc := range testCases {
+	for _, tc := range packExamples {
 		t.Run(tc.desc, func(t *testing.T) {
-			pr, err := NewPackReader(bytes.NewReader(tc.pack.raw))
+			pr, err := NewPackReader(bytes.NewReader(tc.raw))
 			require.NoError(t, err)
 
-			require.Equal(t, tc.pack.n, pr.NumObjects(), "number of objects in packfile")
+			require.Equal(t, tc.n, pr.NumObjects(), "number of objects in packfile")
 
 			out, err := ioutil.ReadAll(pr)
 			require.NoError(t, err, "read all data")
 
-			require.Equal(t, string(tc.pack.content), string(out), "packfile content")
+			require.Equal(t, string(tc.content), string(out), "packfile content")
 		})
 	}
 }
@@ -49,29 +43,19 @@ func TestPackReader(t *testing.T) {
 // TODO add more PackReader tests: invalid header, length < 32, invalid checksum
 
 func TestPackWriter(t *testing.T) {
-	testCases := []struct {
-		desc string
-		pack *packExample
-	}{
-		{
-			desc: "packfile with 3 objects",
-			pack: smallPackExample,
-		},
-	}
-
-	for _, tc := range testCases {
+	for _, tc := range packExamples {
 		t.Run(tc.desc, func(t *testing.T) {
 			out := &bytes.Buffer{}
-			pw, err := NewPackWriter(out, tc.pack.n)
+			pw, err := NewPackWriter(out, tc.n)
 			require.NoError(t, err)
 
-			in := tc.pack.content
+			in := tc.content
 			nBytes, err := pw.Write(in)
 			require.NoError(t, err)
 			require.Equal(t, nBytes, len(in), "bytes written")
 
 			require.NoError(t, pw.Flush(), "flush")
-			require.Equal(t, string(tc.pack.raw), out.String())
+			require.Equal(t, string(tc.raw), out.String())
 		})
 	}
 }
