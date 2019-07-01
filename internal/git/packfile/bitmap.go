033ca341591638b0227a0c7693537bc941245c66
Jacob Vosmaer <jacob@gitlab.com>
2019-07-01 12:11:54 +0200

    Uncompress EWAH into *big.Int

--- internal/git/packfile/bitmap.go
+++ internal/git/packfile/bitmap.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io"
 	"math"
+	"math/big"
 	"os"
 
 	"gitlab.com/gitlab-org/gitaly/internal/git/gitio"
@@ -94,9 +95,9 @@ func (idx *Index) parseBitmapHeader(r io.Reader) (uint32, error) {
 }
 
 type EWAH struct {
-	raw   []byte
-	bits  uint32
-	words uint32
+	bits  int
+	words int
+	bm    *big.Int
 }
 
 const ewahTrailerLen = 4
@@ -107,67 +108,83 @@ func ReadEWAH(r io.Reader) (*EWAH, error) {
 		return nil, err
 	}
 
-	e := &EWAH{
-		bits:  binary.BigEndian.Uint32(header[:4]),
-		words: binary.BigEndian.Uint32(header[4:]),
+	e := &EWAH{}
+
+	uBits := binary.BigEndian.Uint32(header[:4])
+	if uBits > math.MaxInt32 {
+		return nil, fmt.Errorf("too many bits in bitmap: %d", uBits)
 	}
+	e.bits = int(uBits)
+
+	uWords := binary.BigEndian.Uint32(header[4:])
+	if uWords > math.MaxInt32 {
+		return nil, fmt.Errorf("too many words in bitmap: %d", uWords)
+	}
+	e.words = int(uWords)
 
 	rawSize := int64(e.words)*8 + ewahTrailerLen
 	if rawSize > math.MaxInt32 {
 		return nil, fmt.Errorf("EWAH bitmap does not fit in Go slice")
 	}
 
-	e.raw = make([]byte, int(rawSize))
+	raw := make([]byte, int(rawSize))
 
-	if _, err := io.ReadFull(r, e.raw); err != nil {
+	if _, err := io.ReadFull(r, raw); err != nil {
 		return nil, err
 	}
 
-	return e, nil
-}
+	nUnpackedWords := e.bits / 64
+	if e.bits%64 > 0 {
+		nUnpackedWords++
+	}
 
-func (e *EWAH) Scan(f func(uint32) error) error {
-	bit := uint32(0)
-	cursor := 0
-	lastByte := len(e.raw) - ewahTrailerLen
-	for cursor < lastByte && bit < e.bits {
-		header := binary.BigEndian.Uint64(e.raw[cursor : cursor+8])
-		cursor += 8
+	buf := make([]byte, nUnpackedWords*8)
+	bufPos := len(buf)
+
+	for i := 0; i < e.words; {
+		header := binary.BigEndian.Uint64(raw[8*i : 8*(i+1)])
+		i++
 
 		cleanBit := int(header & 1)
 		nClean := uint32(header >> 1)
 		nDirty := uint32(header >> 33)
 
-		for i := uint32(0); i < nClean; i++ {
-			for j := 0; j < 64; j++ {
-				if cleanBit == 1 {
-					if err := f(bit); err != nil {
-						return err
-					}
-				}
-
-				bit++
+		for ; nClean > 0; nClean-- {
+			if cleanBit == 1 {
+				copy(
+					buf[bufPos-8:bufPos],
+					[]byte{0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff},
+				)
+			} else {
+				// No need to copy zeros into buf
 			}
+
+			bufPos -= 8
 		}
 
-		for i := uint32(0); i < nDirty; i++ {
-			word := binary.BigEndian.Uint64(e.raw[cursor : cursor+8])
-			cursor += 8
-
-			for j := uint(0); j < 64; j++ {
-				if mask := uint64(1 << j); word&mask >= mask {
-					if err := f(bit); err != nil {
-						return err
-					}
-				}
-
-				bit++
-			}
+		for ; nDirty > 0; nDirty-- {
+			copy(
+				buf[bufPos-8:bufPos],
+				raw[8*i:8*(i+1)],
+			)
+			bufPos -= 8
+			i++
 		}
 	}
 
-	if cursor != lastByte {
-		return fmt.Errorf("expected bitmap to use %d bytes, but consumed only %d", lastByte, cursor)
+	e.bm = big.NewInt(0)
+	e.bm.SetBytes(buf)
+
+	return e, nil
+}
+
+func (e *EWAH) Scan(f func(uint32) error) error {
+	for i := 0; i < e.bits; i++ {
+		if e.bm.Bit(i) == 1 {
+			if err := f(uint32(i)); err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil
