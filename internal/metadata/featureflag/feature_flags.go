26f737af5508f1ba906779b989bc6e335a9204a7
Paul Okstad <pokstad@gitlab.com>
2019-12-19 11:51:52 -0800

    Protect against improper feature flags

--- internal/metadata/featureflag/feature_flags.go
+++ internal/metadata/featureflag/feature_flags.go
@@ -1,20 +1,37 @@
 package featureflag
 
-const (
-	// UploadPackFilter enables partial clones by sending uploadpack.allowFilter and uploadpack.allowAnySHA1InWant
-	// to upload-pack
-	UploadPackFilter = "upload_pack_filter"
-	// LinguistFileCountStats will invoke an additional git-linguist command to get the number of files per language
-	LinguistFileCountStats = "linguist_file_count_stats"
-	// HooksRPC will invoke update, pre receive, and post receive hooks by using RPCs
-	HooksRPC = "hooks_rpc"
-	// CacheInvalidator controls the tracking of repo state via gRPC
-	// annotations (i.e. accessor and mutator RPC's). This enables cache
-	// invalidation by changing state when the repo is modified.
-	CacheInvalidator = "cache-invalidator"
+import (
+	"fmt"
+	"regexp"
+)
+
+// UploadPackFilter enables partial clones by sending uploadpack.allowFilter and uploadpack.allowAnySHA1InWant
+// to upload-pack
+//
+// LinguistFileCountStats will invoke an additional git-linguist command to get the number of files per language
+//
+// HooksRPC will invoke update, pre receive, and post receive hooks by using RPCs
+//
+// CacheInvalidator controls the tracking of repo state via gRPC
+// annotations (i.e. accessor and mutator RPC's). This enables cache
+// invalidation by changing state when the repo is modified.
+var (
+	UploadPackFilter       = mustValidateFF("upload_pack_filter")
+	LinguistFileCountStats = mustValidateFF("linguist_file_count_stats")
+	HooksRPC               = mustValidateFF("hooks_rpc")
+	CacheInvalidator       = mustValidateFF("cache-invalidator")
 )
 
 const (
 	// HooksRPCEnvVar is the name of the environment variable we use to pass the feature flag down into gitaly-hooks
 	HooksRPCEnvVar = "GITALY_HOOK_RPCS_ENABLED"
 )
+
+var ffRegex = regexp.MustCompile(`^[[:alpha:]_]+$`)
+
+func mustValidateFF(ff string) string {
+	if !ffRegex.MatchString(ff) {
+		panic(fmt.Sprintf("invalid chars found in feature flag: %q", ff))
+	}
+	return ff
+}
