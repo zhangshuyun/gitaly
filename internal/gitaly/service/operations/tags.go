8b08635084240888b5a06efbb56ebca836a13dbf
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2020-11-16 16:27:38 +0100

    Implement basic UserDeleteTagGo
    The code is pretty much copy/pasted as-is from c1e3ccca ("Initial go
    implementation of UserCreateBranch", 2020-09-30), turns out that doing
    this for branches & tags is quite similar.
    
    I'm not doing GetTag() here, which doesn't exist, which would be the
    same as the GetBranch(), but GetReference().
    
    TODO: I think the GetBranch() is probably a bug, see GetBranch() in
    internal/git/repository.go, i.e. how it DWYM with "refs/heads/foo" and
    "foo". Seems like we should do the "refs/heads/" check here earlier
    and then use the fully-qualified name in the branch code too.

--- internal/gitaly/service/operations/tags.go
+++ internal/gitaly/service/operations/tags.go
@@ -2,18 +2,23 @@ package operations
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
+	"gitlab.com/gitlab-org/gitaly/internal/git"
 	"gitlab.com/gitlab-org/gitaly/internal/gitaly/rubyserver"
+	"gitlab.com/gitlab-org/gitaly/internal/helper"
 	"gitlab.com/gitlab-org/gitaly/internal/metadata/featureflag"
 	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 func (s *server) UserDeleteTag(ctx context.Context, req *gitalypb.UserDeleteTagRequest) (*gitalypb.UserDeleteTagResponse, error) {
 	if featureflag.IsDisabled(ctx, featureflag.GoUserDeleteTag) {
 		return s.UserDeleteTagRuby(ctx, req)
 	}
-	// TODO: Implement GoUserDeleteTag
-	return s.UserDeleteTagRuby(ctx, req)
+	return s.UserDeleteTagGo(ctx, req)
 }
 
 func (s *server) UserDeleteTagRuby(ctx context.Context, req *gitalypb.UserDeleteTagRequest) (*gitalypb.UserDeleteTagResponse, error) {
@@ -28,6 +33,44 @@ func (s *server) UserDeleteTagRuby(ctx context.Context, req *gitalypb.UserDelete
 	}
 
 	return client.UserDeleteTag(clientCtx, req)
+}
+
+func (s *server) UserDeleteTagGo(ctx context.Context, req *gitalypb.UserDeleteTagRequest) (*gitalypb.UserDeleteTagResponse, error) {
+	if len(req.TagName) == 0 {
+		return nil, status.Errorf(codes.InvalidArgument, "Bad Request (empty tag name)")
+	}
+
+	if req.User == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "Bad Request (empty user)")
+	}
+
+	revision, err := git.NewRepository(req.Repository).GetReference(ctx, "refs/tags/"+string(req.TagName))
+	if err != nil {
+		return nil, helper.ErrPreconditionFailed(err)
+	}
+
+	tag := fmt.Sprintf("refs/tags/%s", req.TagName)
+
+	if err := s.updateReferenceWithHooks(ctx, req.Repository, req.User, tag, git.NullSHA, revision.Name); err != nil {
+		var preReceiveError preReceiveError
+		if errors.As(err, &preReceiveError) {
+			return &gitalypb.UserDeleteTagResponse{
+				PreReceiveError: preReceiveError.message,
+			}, nil
+		}
+
+		var updateRefError updateRefError
+		if errors.As(err, &updateRefError) {
+			// TODO: COpy/pasted from branches.go. See
+			// "When an error happens[...]". Does it apply
+			// here too?
+			return &gitalypb.UserDeleteTagResponse{}, nil
+		}
+
+		return nil, err
+	}
+
+	return &gitalypb.UserDeleteTagResponse{}, nil
 }
 
 func (s *server) UserCreateTag(ctx context.Context, req *gitalypb.UserCreateTagRequest) (*gitalypb.UserCreateTagResponse, error) {
