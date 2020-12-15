zhangshuyun/gitaly

commit 777a12cfd19ccad20ded6aac0309d36ea3cf1c0e

wrap errors with grpc context
Currently we create a new gRPC status when decorating errors with
gRPC codes. This has the downside of losing the original type when
decorating an error with a gRPC status code. This commit wraps
the original error with gRPC context and allows for accessing the
original error by unwrapping it.

internal/helper/errors_test.go
@@ -1,0 +1,76 @@
+package helper
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func TestError(t *testing.T) {
+	input := errors.New("sentinel error")
+	for _, tc := range []struct {
+		desc     string
+		decorate func(err error) error
+		code     codes.Code
+	}{
+		{
+			desc:     "Internal",
+			decorate: ErrInternal,
+			code:     codes.Internal,
+		},
+		{
+			desc:     "InvalidArgument",
+			decorate: ErrInvalidArgument,
+			code:     codes.InvalidArgument,
+		},
+		{
+			desc:     "PreconditionFailed",
+			decorate: ErrPreconditionFailed,
+			code:     codes.FailedPrecondition,
+		},
+		{
+			desc:     "NotFound",
+			decorate: ErrNotFound,
+			code:     codes.NotFound,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			err := tc.decorate(input)
+			require.True(t, errors.Is(err, input))
+			require.Equal(t, tc.code, status.Code(err))
+		})
+	}
+}
+
+func TestErrorf(t *testing.T) {
+	for _, tc := range []struct {
+		desc   string
+		errorf func(format string, a ...interface{}) error
+		code   codes.Code
+	}{
+		{
+			desc:   "Internalf",
+			errorf: ErrInternalf,
+			code:   codes.Internal,
+		},
+		{
+			desc:   "InvalidArgumentf",
+			errorf: ErrInvalidArgumentf,
+			code:   codes.InvalidArgument,
+		},
+		{
+			desc:   "PreconditionFailedf",
+			errorf: ErrPreconditionFailedf,
+			code:   codes.FailedPrecondition,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			err := tc.errorf("expected %s", "message")
+			require.EqualError(t, err, "expected message")
+			require.Equal(t, tc.code, status.Code(err))
+		})
+	}
+}

internal/helper/error.go
@@ -8,13 +8,26 @@ import (
 )
 
 // Unimplemented is a Go error with gRPC error code 'Unimplemented'
-var Unimplemented = status.Errorf(codes.Unimplemented, "this rpc is not implemented")
+var Unimplemented = status.Error(codes.Unimplemented, "this rpc is not implemented")
+
+type statusWrapper struct {
+	error
+	status *status.Status
+}
+
+func (sw statusWrapper) GRPCStatus() *status.Status {
+	return sw.status
+}
+
+func (sw statusWrapper) Unwrap() error {
+	return sw.error
+}
 
 // DecorateError unless it's already a grpc error.
 //  If given nil it will return nil.
 func DecorateError(code codes.Code, err error) error {
 	if err != nil && GrpcCode(err) == codes.Unknown {
-		return status.Errorf(code, "%v", err)
+		return statusWrapper{err, status.New(code, err.Error())}
 	}
 	return err
 }
@@ -24,7 +37,7 @@ func ErrInternal(err error) error { return DecorateError(codes.Internal, err) }
 
 // ErrInternalf wrapps a formatted error with codes.Internal, unless err is already a grpc error
 func ErrInternalf(format string, a ...interface{}) error {
-	return DecorateError(codes.Internal, fmt.Errorf(format, a...))
+	return ErrInternal(fmt.Errorf(format, a...))
 }
 
 // ErrInvalidArgument wraps err with codes.InvalidArgument, unless err is already a grpc error
@@ -32,7 +45,7 @@ func ErrInvalidArgument(err error) error { return DecorateError(codes.InvalidArg
 
 // ErrInvalidArgumentf wraps a formatted error with codes.InvalidArgument, unless err is already a grpc error
 func ErrInvalidArgumentf(format string, a ...interface{}) error {
-	return DecorateError(codes.InvalidArgument, fmt.Errorf(format, a...))
+	return ErrInvalidArgument(fmt.Errorf(format, a...))
 }
 
 // ErrPreconditionFailed wraps err with codes.FailedPrecondition, unless err is already a grpc error
@@ -40,7 +53,7 @@ func ErrPreconditionFailed(err error) error { return DecorateError(codes.FailedP
 
 // ErrPreconditionFailedf wraps a formatted error with codes.FailedPrecondition, unless err is already a grpc error
 func ErrPreconditionFailedf(format string, a ...interface{}) error {
-	return DecorateError(codes.FailedPrecondition, fmt.Errorf(format, a...))
+	return ErrPreconditionFailed(fmt.Errorf(format, a...))
 }
 
 // ErrNotFound wraps error with codes.NotFound, unless err is already a grpc error

internal/praefect/server_test.go
@@ -30,7 +30,6 @@ import (
 	"gitlab.com/gitlab-org/gitaly/internal/version"
 	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
 	"google.golang.org/grpc/health/grpc_health_v1"
-	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 func TestServerRouteServerAccessor(t *testing.T) {
@@ -226,8 +225,8 @@ func TestHealthCheck(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
-	client := healthpb.NewHealthClient(cc)
-	_, err := client.Check(ctx, &healthpb.HealthCheckRequest{})
+	client := grpc_health_v1.NewHealthClient(cc)
+	_, err := client.Check(ctx, &grpc_health_v1.HealthCheckRequest{})
 	require.NoError(t, err)
 }
 

internal/service/objectpool/create_test.go
@@ -12,6 +12,7 @@ import (
 	"gitlab.com/gitlab-org/gitaly/internal/git/objectpool"
 	"gitlab.com/gitlab-org/gitaly/internal/testhelper"
 	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
+	"google.golang.org/grpc/status"
 )
 
 func TestCreate(t *testing.T) {
@@ -154,7 +155,7 @@ func TestUnsuccessfulCreate(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.desc, func(t *testing.T) {
 			_, err := client.CreateObjectPool(ctx, tc.request)
-			require.Equal(t, tc.error, err)
+			require.Equal(t, status.Convert(tc.error).Err(), err)
 		})
 	}
 }
@@ -228,7 +229,7 @@ func TestDelete(t *testing.T) {
 					RelativePath: tc.relativePath,
 				},
 			}})
-			require.Equal(t, tc.error, err)
+			require.Equal(t, status.Convert(tc.error).Err(), err)
 		})
 	}
 }

commit b48ccd43d46ee60af1cf43b4a5c097fbda125d93

Error helper tests: demonstrate regression in 777a12cfd
The change made in 777a12cfd (wrap errors with grpc context,
2020-05-07) intended to preserve existing GRPC error codes, but as
these tests demonstrate it did the opposite. These new tests are
easier to read if we unify the ErrorSomething and ErrorSomethingf
functions, so let's do that. A follow-up cleanup change will unify
them a bit further.

internal/helper/errors_test.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,68 +11,97 @@ import (
 )
 
 func TestError(t *testing.T) {
-	input := errors.New("sentinel error")
+	errorFormat := "expected %s"
+	errorMessage := "sentinel error"
+	input := errors.New(errorMessage)
+	inputGRPC := status.Error(codes.Unauthenticated, errorMessage)
+	inputGRPCFmt := status.Errorf(codes.Unauthenticated, errorFormat, errorMessage)
+
 	for _, tc := range []struct {
-		desc     string
-		decorate func(err error) error
-		code     codes.Code
+		desc      string
+		function  func(err error) error
+		functionf func(format string, a ...interface{}) error
+		code      codes.Code
+		format    bool
+		wrapped   bool
 	}{
+		// Format-less functions
 		{
 			desc:     "Internal",
-			decorate: ErrInternal,
+			function: ErrInternal,
 			code:     codes.Internal,
+			format:   false,
+			wrapped:  true,
 		},
 		{
 			desc:     "InvalidArgument",
-			decorate: ErrInvalidArgument,
+			function: ErrInvalidArgument,
 			code:     codes.InvalidArgument,
+			format:   false,
+			wrapped:  true,
 		},
 		{
 			desc:     "PreconditionFailed",
-			decorate: ErrPreconditionFailed,
+			function: ErrPreconditionFailed,
 			code:     codes.FailedPrecondition,
+			format:   false,
+			wrapped:  true,
 		},
 		{
 			desc:     "NotFound",
-			decorate: ErrNotFound,
+			function: ErrNotFound,
 			code:     codes.NotFound,
+			format:   false,
+			wrapped:  true,
 		},
-	} {
-		t.Run(tc.desc, func(t *testing.T) {
-			err := tc.decorate(input)
-			require.True(t, errors.Is(err, input))
-			require.Equal(t, tc.code, status.Code(err))
-		})
-	}
-}
 
-func TestErrorf(t *testing.T) {
-	input := errors.New("sentinel error")
-	for _, tc := range []struct {
-		desc     string
-		decorate func(format string, a ...interface{}) error
-		code     codes.Code
-	}{
+		// Format-y functions
 		{
-			desc:     "Internalf",
-			decorate: ErrInternalf,
-			code:     codes.Internal,
+			desc:      "Internalf",
+			functionf: ErrInternalf,
+			code:      codes.Internal,
+			format:    true,
+			wrapped:   false,
 		},
 		{
-			desc:     "InvalidArgumentf",
-			decorate: ErrInvalidArgumentf,
-			code:     codes.InvalidArgument,
+			desc:      "InvalidArgumentf",
+			functionf: ErrInvalidArgumentf,
+			code:      codes.InvalidArgument,
+			format:    true,
+			wrapped:   false,
 		},
 		{
-			desc:     "PreconditionFailedf",
-			decorate: ErrPreconditionFailedf,
-			code:     codes.FailedPrecondition,
+			desc:      "PreconditionFailedf",
+			functionf: ErrPreconditionFailedf,
+			code:      codes.FailedPrecondition,
+			format:    true,
+			wrapped:   false,
 		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
-			err := tc.decorate("expected %s", input)
-			require.EqualError(t, err, "expected sentinel error")
+			var err error
+			if tc.format {
+				err = tc.functionf(errorFormat, input)
+				require.EqualError(t, err, fmt.Sprintf(errorFormat, errorMessage))
+				require.False(t, errors.Is(err, input))
+			} else {
+				err = tc.function(input)
+				require.EqualError(t, err, errorMessage)
+				require.True(t, errors.Is(err, input))
+			}
 			require.Equal(t, tc.code, status.Code(err))
+
+			// Does an existing GRPC's error's code get
+			// preserved?
+			if tc.format {
+				err = tc.functionf(errorFormat, inputGRPCFmt)
+				require.Equal(t, tc.code, status.Code(err))
+			} else {
+				err = tc.function(inputGRPC)
+				require.True(t, errors.Is(err, inputGRPC))
+				require.NotEqual(t, tc.code, status.Code(inputGRPC))
+				require.Equal(t, status.Code(inputGRPC), status.Code(err))
+			}
 		})
 	}
 }