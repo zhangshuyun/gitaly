67e2ded8abdec8add11b0130c3be09f8469e2b5b
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2020-12-15 13:21:32 +0100

    Error helper tests: demonstrate regression in 777a12cfd
    The change made in 777a12cfd (wrap errors with grpc context,
    2020-05-07) intended to preserve existing GRPC error codes, but as
    these tests demonstrate it did the opposite.
    
    We now likely have new code relying on that behavior, so let's
    document it in the relevant comments and test for it.
    
    The reason this happens is not because of the removal of the explicit
    DecorateError() in that commit, but because of the addition of a
    wrapper and the behavior of %v and %w.
    
    The addition of the oldPreconditionFailedf() function demonstrates
    that. I.e. the regression didn't have to do with the removal of
    DecorateError() (a mere refactoring, we now call
    ErrPreconditionFailed() which calls it for us), but the way we're
    wrapping the error. See https://blog.golang.org/go1.13-errors and the
    %v and %w tests here. I.e. you can still get to the wrapped value, but
    what we're going to unpack by default is a different matter.
    
    To test this try:
    
        # Fails, the old behavior
        git checkout 495a384d4 -- internal/helper/error.go
        go test -v ./internal/helper/ -run TestError -count=1
        # OK, the new (buggy) behavior
        git checkout -f 777a12cfd -- internal/helper/error.go
        go test -v ./internal/helper/ -run TestError -count=1

--- internal/helper/errors_test.go
+++ internal/helper/errors_test.go
@@ -2,6 +2,8 @@ package helper
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -10,7 +12,11 @@ import (
 )
 
 func TestError(t *testing.T) {
-	input := errors.New("sentinel error")
+	errorMessage := "sentinel error"
+	input := errors.New(errorMessage)
+	inputGRPCCode := codes.Unauthenticated
+	inputGRPC := status.Error(inputGRPCCode, errorMessage)
+
 	for _, tc := range []struct {
 		desc   string
 		errorf func(err error) error
@@ -38,15 +44,54 @@ func TestError(t *testing.T) {
 		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
+			// tc.code and our canary test code must not
+			// clash!
+			require.NotEqual(t, tc.code, inputGRPCCode)
+
+			// When not re-throwing an error we get the
+			// GRPC error code corresponding to the
+			// function's name.
 			err := tc.errorf(input)
-			require.True(t, errors.Is(err, input))
+			require.EqualError(t, err, errorMessage)
+			require.False(t, errors.Is(err, inputGRPC))
 			require.Equal(t, tc.code, status.Code(err))
+
+			// When re-throwing an error an existing GRPC
+			// error code will get preserved, instead of
+			// the one corresponding to the function's
+			// name.
+			err = tc.errorf(inputGRPC)
+			require.True(t, errors.Is(err, inputGRPC))
+			require.False(t, errors.Is(err, input))
+			require.Equal(t, inputGRPCCode, status.Code(err))
+			require.NotEqual(t, tc.code, status.Code(inputGRPC))
 		})
 	}
 }
 
-func TestErrorf(t *testing.T) {
-	input := errors.New("sentinel error")
+func TestErrorF_withVFormat(t *testing.T) {
+	testErrorfFormat(t, "expected %v", "expected %v")
+}
+
+func TestErrorF_withWFormat(t *testing.T) {
+	testErrorfFormat(t, "expected %w", "expected %s")
+}
+
+// oldPreconditionFailedf shows ErrPreconditionFailedf looked like
+// before 777a12cfd. We're testing the nature of a regression in that
+// change.
+func oldPreconditionFailedf(format string, a ...interface{}) error {
+	return DecorateError(codes.FailedPrecondition, fmt.Errorf(format, a...))
+}
+
+func testErrorfFormat(t *testing.T, errorFormat, errorFormatEqual string) {
+	isFormatW := strings.Contains(errorFormat, "%w")
+	errorMessage := "sentinel error"
+	input := errors.New(errorMessage)
+	inputGRPCCode := codes.Unauthenticated
+	inputGRPC := status.Error(inputGRPCCode, errorMessage)
+	inputGRPCFmt := status.Errorf(inputGRPCCode, errorFormat, errorMessage)
+
 	for _, tc := range []struct {
 		desc   string
 		errorf func(format string, a ...interface{}) error
@@ -67,11 +112,39 @@ func TestErrorf(t *testing.T) {
 			errorf: ErrPreconditionFailedf,
 			code:   codes.FailedPrecondition,
 		},
+		{
+			desc:   "oldPreconditionFailedf",
+			errorf: oldPreconditionFailedf,
+			code:   codes.FailedPrecondition,
+		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
-			err := tc.errorf("expected %s", input)
-			require.EqualError(t, err, "expected sentinel error")
+			// tc.code and our canary test code must not
+			// clash!
+			require.NotEqual(t, tc.code, inputGRPCCode)
+
+			// When not re-throwing an error we get the
+			// GRPC error code corresponding to the
+			// function's name. Just like the non-f functions.
+			err := tc.errorf(errorFormat, input)
+			require.EqualError(t, err, fmt.Sprintf(errorFormatEqual, errorMessage))
+			require.False(t, errors.Is(err, inputGRPC))
 			require.Equal(t, tc.code, status.Code(err))
+
+			// When re-throwing an error an existing GRPC
+			// error code will get clobbered, not the one
+			// corresponding to the function's name. This
+			// is unlike the non-f functions.
+			err = tc.errorf(errorFormat, inputGRPCFmt)
+			require.False(t, errors.Is(err, input))
+			require.Equal(t, tc.code, status.Code(err))
+			require.NotEqual(t, tc.code, status.Code(inputGRPC))
+
+			errX := tc.errorf(errorFormat, inputGRPC)
+			require.Equal(t, errors.Is(errX, inputGRPC), isFormatW) // .True() for non-f
+			require.False(t, errors.Is(errX, input))
+			require.NotEqual(t, inputGRPCCode, status.Code(errX)) // .Equal() for non-f
+			require.NotEqual(t, tc.code, status.Code(inputGRPC))
 		})
 	}
 }
