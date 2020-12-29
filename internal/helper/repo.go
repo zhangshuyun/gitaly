0fa3fe2309e4e42c790d46ccfad03aa43f020f9e
Pavlo Strokov <pstrokov@gitlab.com>
2020-12-29 13:36:36 +0200

    Removal of unused GetStorageByName func
    Part of: https://gitlab.com/gitlab-org/gitaly/-/issues/2699

--- internal/helper/repo.go
+++ internal/helper/repo.go
@@ -2,28 +2,13 @@ package helper
 
 import (
 	"gitlab.com/gitlab-org/gitaly/internal/git/repository"
-	"gitlab.com/gitlab-org/gitaly/internal/gitaly/config"
 	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 // RepoPathEqual compares if two repositories are in the same location
 func RepoPathEqual(a, b repository.GitRepo) bool {
 	return a.GetStorageName() == b.GetStorageName() &&
 		a.GetRelativePath() == b.GetRelativePath()
-}
-
-// GetStorageByName will return the path for the storage, which is fetched by
-// its key. An error is return if it cannot be found.
-// Deprecated: please use storage.Locator to define the storage path.
-func GetStorageByName(storageName string) (string, error) {
-	storagePath, ok := config.Config.StoragePath(storageName)
-	if !ok {
-		return "", status.Errorf(codes.InvalidArgument, "Storage can not be found by name '%s'", storageName)
-	}
-
-	return storagePath, nil
 }
 
 // ProtoRepoFromRepo allows object pools and repository abstractions to be used
