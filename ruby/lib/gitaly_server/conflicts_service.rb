98bdbb28bc954246c6e5eec97dd5e761c850f2b4
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-01-13 08:22:12 +0100

    conflicts: Drop Ruby implementation of ListConflictsFiles
    The ListConflictsFiles RPC has been ported to Go in 8d01fd4b3
    (conflicts: Port ListConflictFiles to Go, 2020-10-15), default-enabled
    in 181cbf030 (featureflag: Enable Go implementation of
    ListConflictFiles, 2020-11-12) and the feature flag finally got removed
    in e58464bac (conflicts: Remove GoListConflictFiles feature flag,
    2020-12-04). We are thus fine to drop the supporting Ruby code now.

--- ruby/lib/gitaly_server/conflicts_service.rb
+++ ruby/lib/gitaly_server/conflicts_service.rb
@@ -2,41 +2,6 @@ module GitalyServer
   class ConflictsService < Gitaly::ConflictsService::Service
     include Utils
 
-    def list_conflict_files(request, call)
-      repo = Gitlab::Git::Repository.from_gitaly(request.repository, call)
-      resolver = Gitlab::Git::Conflict::Resolver.new(repo, request.our_commit_oid, request.their_commit_oid)
-      conflicts = resolver.conflicts
-      files = []
-      msg_size = 0
-
-      Enumerator.new do |y|
-        enumerate_conflicts(conflicts) do |file|
-          files << Gitaly::ConflictFile.new(header: conflict_file_header(file))
-
-          strio = StringIO.new(file.content)
-          while chunk = strio.read(Gitlab.config.git.write_buffer_size - msg_size)
-            files << Gitaly::ConflictFile.new(content: chunk)
-            msg_size += chunk.bytesize
-
-            # We don't send a message for each chunk because the content of
-            # a file may be smaller than the size limit, which means we can
-            # keep adding data to the message
-            next if msg_size < Gitlab.config.git.write_buffer_size
-
-            y.yield(Gitaly::ListConflictFilesResponse.new(files: files))
-
-            files = []
-            msg_size = 0
-          end
-        end
-
-        # Send leftover data, if any
-        y.yield(Gitaly::ListConflictFilesResponse.new(files: files)) if files.any?
-      end
-    rescue Gitlab::Git::Conflict::Resolver::ListError => e
-      raise GRPC::FailedPrecondition.new(e.message)
-    end
-
     def resolve_conflicts(call)
       header = nil
       files_json = ""
@@ -67,26 +32,6 @@ module GitalyServer
       rescue Gitlab::Git::Conflict::Resolver::ResolutionError => e
         Gitaly::ResolveConflictsResponse.new(resolution_error: e.message)
       end
-    end
-
-    private
-
-    def conflict_file_header(file)
-      Gitaly::ConflictFileHeader.new(
-        commit_oid: file.commit_oid,
-        their_path: file.their_path.b,
-        our_path: file.our_path.b,
-        our_mode: file.our_mode,
-        ancestor_path: file.ancestor_path&.b
-      )
-    end
-
-    def enumerate_conflicts(conflicts)
-      conflicts.each do |file|
-        yield file
-      end
-    rescue Gitlab::Git::Conflict::File::UnsupportedEncoding, Gitlab::Git::Conflict::Resolver::ListError => e
-      raise GRPC::FailedPrecondition.new(e.message)
     end
   end
 end
