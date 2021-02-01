c4486a582b90baec3cf88c3d2917856295649293
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-02-01 15:01:13 +0100

    transactions: Fix race between counting and creating subtransactions
    Given that we may concurrently create new subtransactions when calling
    `CountSubtransactions()`, we need to make sure to lock the transaction
    correctly before countig. We don't though, which leads to a race between
    counting and creating new subtransactions.
    
    Fix this by properly taking the lock.

--- internal/praefect/transactions/transaction.go
+++ internal/praefect/transactions/transaction.go
@@ -166,6 +166,9 @@ func (t *Transaction) State() (map[string]VoteResult, error) {
 // CountSubtransactions counts the number of subtransactions created as part of
 // the transaction.
 func (t *Transaction) CountSubtransactions() int {
+	t.lock.Lock()
+	defer t.lock.Unlock()
+
 	return len(t.subtransactions)
 }
 
