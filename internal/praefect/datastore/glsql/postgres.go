f9e1a5280fd3391d56919750a10bf97fdd8534e8
Sami Hiltunen <shiltunen@gitlab.com>
2021-02-15 15:16:12 +0100

    remove TxQuery type and funnctions
    TxQuery tests are flaking and causing a lot of pipeline failures.
    The code is not used anywhere, so this commit fixes the problem
    by removing all related code.

--- internal/praefect/datastore/glsql/postgres.go
+++ internal/praefect/datastore/glsql/postgres.go
@@ -8,7 +8,6 @@ import (
 	// Blank import to enable integration of github.com/lib/pq into database/sql
 	_ "github.com/lib/pq"
 	migrate "github.com/rubenv/sql-migrate"
-	"github.com/sirupsen/logrus"
 	"gitlab.com/gitlab-org/gitaly/internal/praefect/config"
 	"gitlab.com/gitlab-org/gitaly/internal/praefect/datastore/migrations"
 )
@@ -58,74 +57,6 @@ type ListenHandler interface {
 	Disconnect(error)
 	// Connected would be triggered once a connection to remote service is established.
 	Connected()
-}
-
-// TxQuery runs operations inside transaction and commits|rollbacks on Done.
-type TxQuery interface {
-	// Exec calls op function with provided ctx.
-	// Returns true on success and false in case operation failed or wasn't called because of previously failed op.
-	Exec(ctx context.Context, op func(context.Context, *sql.Tx) error) bool
-	// Done must be called after work is finished to complete transaction.
-	// errPtr must not be nil.
-	// COMMIT will be executed if no errors happen during TxQuery usage.
-	// Otherwise it will be ROLLBACK operation.
-	Done(errPtr *error)
-}
-
-// NewTxQuery creates entity that allows to run queries in scope of a transaction.
-// It always returns non-nil value.
-func NewTxQuery(ctx context.Context, logger logrus.FieldLogger, db *sql.DB) TxQuery {
-	tx, err := db.BeginTx(ctx, nil)
-	return &txQuery{
-		tx:     tx,
-		err:    err,
-		logger: logger,
-	}
-}
-
-type txQuery struct {
-	tx     *sql.Tx
-	err    error
-	logger logrus.FieldLogger
-}
-
-// Exec calls op function with provided ctx.
-// Returns true on success and false in case operation failed or wasn't called because of previously failed op.
-func (txq *txQuery) Exec(ctx context.Context, op func(context.Context, *sql.Tx) error) bool {
-	if txq.err != nil {
-		return false
-	}
-
-	txq.err = op(ctx, txq.tx)
-	return txq.err == nil
-}
-
-// Done must be called after work is finished to complete transaction.
-// errPtr must not be nil.
-// COMMIT will be executed if no errors happen during txQuery usage.
-// Otherwise it will be ROLLBACK operation.
-func (txq *txQuery) Done(errPtr *error) {
-	if txq.err == nil {
-		txq.err = txq.tx.Commit()
-		if txq.err != nil {
-			txq.log(txq.err, "commit failed")
-		}
-	} else {
-		// Don't overwrite txq.err because it's already non-nil
-		if err := txq.tx.Rollback(); err != nil {
-			txq.log(err, "rollback failed")
-		}
-	}
-
-	if *errPtr == nil {
-		*errPtr = txq.err
-	}
-}
-
-func (txq *txQuery) log(err error, msg string) {
-	if txq.logger != nil {
-		txq.logger.WithError(err).Error(msg)
-	}
 }
 
 // DestProvider returns list of pointers that will be used to scan values into.
