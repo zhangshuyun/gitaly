b50c37aa455cb32026c62683141705316902b86e
Sami Hiltunen <shiltunen@gitlab.com>
2021-02-23 16:23:32 +0100

    add unique index for delete_replica replication events
    There should always be only one 'delete_replica' event scheduled to
    prevent accidentally deleting too many replicas of a repository. If
    we allow more than one, we could schedule multiple deletion jobs if
    the assignments are shuffled around. This could lead to Praefect
    deleting all of the replicas. To prevent this from happening, this
    commit adds a unique index that ensures we only have a single
    delete_replica job scheduled for any given (virtual_storage, relative_path)
    tuple. This index can additionally be used by the reconciler to check
    whether a deletion job already exists or not.

---
+++ internal/praefect/datastore/migrations/20210223130233_delete_replica_unique_index.go
@@ -0,0 +1,23 @@
+package migrations
+
+import migrate "github.com/rubenv/sql-migrate"
+
+func init() {
+	m := &migrate.Migration{
+		Id: "20210223130233_delete_replica_unique_index",
+		Up: []string{`
+CREATE UNIQUE INDEX CONCURRENTLY delete_replica_unique_index
+ON replication_queue (
+	(job->>'virtual_storage'),
+	(job->>'relative_path')
+)
+WHERE state NOT IN ('completed', 'cancelled', 'dead')
+AND job->>'change' = 'delete_replica'
+			`,
+		},
+		Down:                 []string{"DROP INDEX delete_replica_unique_index"},
+		DisableTransactionUp: true,
+	}
+
+	allMigrations = append(allMigrations, m)
+}
