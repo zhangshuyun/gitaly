zhangshuyun/gitaly

commit c75c02f52decfb1f89e30e15420d29207ccfa3ca

Makefile: Do not re-clone repos when rebuilding git/libgit2
When the Makefile changes, we always rebuild both git and libgit2
because we must assume that their requested version has changed. This is
perfectly fine, but whenever we do we also throw away their repositories
and just re-clone them anew, which is a complete waste of resources.

Fix this shortcoming by only conditionally cloning the repos if they do
not exist yet. If they do exist, we must make sure to update it
accordingly in case a revision was requested which isn't yet known.

This commit also partly fixes a shortcoming of the current approach: it
isn't possible to directly specify an object ID either as git or libgit2
version as we used `git clone --branch ${GIT_VERSION}` to perform the
clone. It's still not possible for the initial clone as to the best of
my knowledge, git-clone(1) doesn't support directly cloning a specific
commit. This could be fixed by doing a non-shallow clone, but the
performance hit on CI systems is probably not worth it. It now is
possible to specify an object ID when the repository already exists and
we only update it though.

Makefile
@@ -400,8 +400,12 @@ ${BUILD_DIR}/git_full_bins.tgz: ${BUILD_DIR}/Makefile.sha256
 	${Q}mv $@.tmp $@
 
 ${LIBGIT2_INSTALL_DIR}/lib/libgit2.a: ${BUILD_DIR}/Makefile.sha256
-	${Q}rm -rf ${LIBGIT2_SOURCE_DIR}
-	${GIT} clone --depth 1 --branch ${LIBGIT2_VERSION} --quiet ${LIBGIT2_REPO_URL} ${LIBGIT2_SOURCE_DIR}
+	${Q}if ! [ -d "${LIBGIT2_SOURCE_DIR}" ]; then \
+	    ${GIT} clone --depth 1 --branch ${LIBGIT2_VERSION} --quiet ${LIBGIT2_REPO_URL} ${LIBGIT2_SOURCE_DIR}; \
+	elif ! git -C "${LIBGIT2_SOURCE_DIR}" rev-parse --quiet --verify ${LIBGIT2_VERSION}^{tree} >/dev/null; then \
+	    ${GIT} -C "${LIBGIT2_SOURCE_DIR}" fetch --quiet ${LIBGIT2_REPO_URL} ${LIBGIT2_VERSION}; \
+	fi
+	${Q}rm -rf ${LIBGIT2_BUILD_DIR}
 	${Q}mkdir -p ${LIBGIT2_BUILD_DIR}
 	${Q}cd ${LIBGIT2_BUILD_DIR} && cmake ${LIBGIT2_SOURCE_DIR} ${LIBGIT2_BUILD_OPTIONS}
 	${Q}CMAKE_BUILD_PARALLEL_LEVEL=$(shell nproc) cmake --build ${LIBGIT2_BUILD_DIR} --target install
@@ -409,8 +413,12 @@ ${LIBGIT2_INSTALL_DIR}/lib/libgit2.a: ${BUILD_DIR}/Makefile.sha256
 
 ifeq (${GIT_USE_PREBUILT_BINARIES},)
 ${GIT_INSTALL_DIR}/bin/git: ${BUILD_DIR}/Makefile.sha256
-	${Q}rm -rf ${GIT_SOURCE_DIR} ${GIT_INSTALL_DIR}
-	${GIT} clone --depth 1 --branch ${GIT_VERSION} --quiet ${GIT_REPO_URL} ${GIT_SOURCE_DIR}
+	${Q}if ! [ -d "${GIT_SOURCE_DIR}" ]; then \
+	    ${GIT} clone --depth 1 --branch ${GIT_VERSION} --quiet ${GIT_REPO_URL} ${GIT_SOURCE_DIR}; \
+	elif ! git -C "${GIT_SOURCE_DIR}" rev-parse --quiet --verify ${GIT_VERSION}^{tree} >/dev/null; then \
+	    ${GIT} -C "${GIT_SOURCE_DIR}" fetch --quiet ${GIT_REPO_URL} ${GIT_VERSION}; \
+	fi
+	${Q}${GIT} -C "${GIT_SOURCE_DIR}" switch --quiet --detach ${GIT_VERSION}
 	${Q}rm -rf ${GIT_INSTALL_DIR}
 	${Q}mkdir -p ${GIT_INSTALL_DIR}
 	env -u MAKEFLAGS -u GIT_VERSION ${MAKE} -C ${GIT_SOURCE_DIR} -j$(shell nproc) prefix=${GIT_PREFIX} ${GIT_BUILD_OPTIONS} install

commit 29b7128d6bfea6f5357c2bd30d40a1aa26fd7d8f

Makefile: start fixing regression in c75c02f52
Start fixing the regressions in c75c02f52 (Makefile: Do not re-clone
repos when rebuilding git/libgit2, 2021-02-17) noted in [A]. What I
said in that comment:

    This breaks e.g. upgrading from v2.29.0 to v2.29.1.

    There's multiple bugs here:

    1. The clone is --depth=1, but not the fetch, so if you need to bump a
       version we switch to a full clone

    2. You do e.g. "git fetch v2.29.1", but this does not create a
       refs/tags/v2.29.1 locally, we just put it into FETCH_HEAD, this is
       unlike the clone --branch option. Thus upgrading a version will
       fetch it successfully, but then we die on the "switch".

    3. You're doing rev-parse --verify <thing>^{tree}. Presumably to not
       pass a full SHA-1, why not just cat-file -t <thing>?  I tried
       fixing #1 (closed) by instead doing --no-tags <obj>:<obj> but that
       fails with trying to write a tag into refs/heads/. As an aside I
       think that's a bug in git's fetching logic, it should pick
       refs/tags/* as the local namespace in that case.

    Then writing everything into refs/tags/*. But now e.g. if you specify
    some SHA-1 it's just happenstance whether it works, did we slurp it
    down in the initial clone?

This starts to fix at least some of that. Now if we see a new version
we're going to create a refs/tags/* name for it (even if it's
e.g. "master").

The --no-tags option is needed because that option really doesn't mean
"no tags", it means "don't do tag following". Without it the "--depth
1" is pretty pointless.

This is still going to be buggy in the case where you specify
e.g. "master" but that ref has moved.

A. https://gitlab.com/gitlab-org/gitaly/-/merge_requests/3151/diffs?commit_id=c75c02f52decfb1f89e30e15420d29207ccfa3ca#note_515460714

Makefile
@@ -408,8 +408,9 @@ ${DEPENDENCY_DIR}/git.version: dependency-version | ${DEPENDENCY_DIR}
 ${LIBGIT2_INSTALL_DIR}/lib/libgit2.a: ${DEPENDENCY_DIR}/libgit2.version
 	${Q}if ! [ -d "${LIBGIT2_SOURCE_DIR}" ]; then \
 	    ${GIT} clone --depth 1 --branch ${LIBGIT2_VERSION} --quiet ${LIBGIT2_REPO_URL} ${LIBGIT2_SOURCE_DIR}; \
-	elif ! git -C "${LIBGIT2_SOURCE_DIR}" rev-parse --quiet --verify ${LIBGIT2_VERSION}^{tree} >/dev/null; then \
-	    ${GIT} -C "${LIBGIT2_SOURCE_DIR}" fetch --quiet ${LIBGIT2_REPO_URL} ${LIBGIT2_VERSION}; \
+	fi
+	${Q}if ! git -C "${LIBGIT2_SOURCE_DIR}" cat-file -t ${LIBGIT2_VERSION} 2>/dev/null; then \
+	    ${GIT} -C "${LIBGIT2_SOURCE_DIR}" fetch --depth 1 --no-tags --quiet ${LIBGIT2_REPO_URL} ${LIBGIT2_VERSION}:refs/tags/${LIBGIT2_VERSION}; \
 	fi
 	${Q}rm -rf ${LIBGIT2_BUILD_DIR}
 	${Q}mkdir -p ${LIBGIT2_BUILD_DIR}
@@ -421,10 +422,11 @@ ifeq (${GIT_USE_PREBUILT_BINARIES},)
 ${GIT_INSTALL_DIR}/bin/git: ${DEPENDENCY_DIR}/git.version
 	${Q}if ! [ -d "${GIT_SOURCE_DIR}" ]; then \
 	    ${GIT} clone --depth 1 --branch ${GIT_VERSION} --quiet ${GIT_REPO_URL} ${GIT_SOURCE_DIR}; \
-	elif ! git -C "${GIT_SOURCE_DIR}" rev-parse --quiet --verify ${GIT_VERSION}^{tree} >/dev/null; then \
-	    ${GIT} -C "${GIT_SOURCE_DIR}" fetch --quiet ${GIT_REPO_URL} ${GIT_VERSION}; \
 	fi
-	${Q}${GIT} -C "${GIT_SOURCE_DIR}" switch --quiet --detach ${GIT_VERSION}
+	${Q}if ! git -C "${GIT_SOURCE_DIR}" cat-file -t ${GIT_VERSION} 2>/dev/null; then \
+	    ${GIT} -C "${GIT_SOURCE_DIR}" fetch --depth 1 --no-tags --quiet ${GIT_REPO_URL} ${GIT_VERSION}:refs/tags/${GIT_VERSION}; \
+	fi
+	${Q}${GIT} -C "${GIT_SOURCE_DIR}" -c advice.detachedHead=false switch --quiet --detach ${GIT_VERSION}
 	${Q}rm -rf ${GIT_INSTALL_DIR}
 	${Q}mkdir -p ${GIT_INSTALL_DIR}
 	env -u MAKEFLAGS -u GIT_VERSION ${MAKE} -C ${GIT_SOURCE_DIR} -j$(shell nproc) prefix=${GIT_PREFIX} ${GIT_BUILD_OPTIONS} install