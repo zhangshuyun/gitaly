3befca6bf6893789a232b5cf7f463c104e11d2a7
Sean McGivern <sean@gitlab.com>
2021-03-04 16:06:48 +0000

    Remove frequency stats
    We don't need these to answer the questions we have.

--- analyze-log.rb
+++ analyze-log.rb
@@ -32,7 +32,7 @@ def main
     records << rec
   end
 
-  [1, 5, 10, 30, 60].each do |minutes|
+  [2, 5, 10].each do |minutes|
     simulate(records, minutes*60)
   end
 end
@@ -40,25 +40,22 @@ end
 def simulate(records, expiry)
   cache = {}
   stats = Hash.new(0)
-  frequencies = Hash.new(0)
 
   puts "Expiry: #{expiry}s"
   records.each do |rec|
     _, first = cache.first
     while first && rec.created_at - first[:rec].created_at > expiry
       cache.shift
-      frequencies[first[:frequency]] += 1
       stats[:size] -= first[:rec].size
       _, first = cache.first
     end
 
     if cache.has_key?(rec.key)
-      cache[rec.key][:frequency] += 1
       stats[:hit] += 1
       next
     end
 
-    cache[rec.key] = { rec: rec, frequency: 1 }
+    cache[rec.key] = { rec: rec }
     stats[:miss] += 1
     stats[:size] += rec.size
 
@@ -67,18 +64,9 @@ def simulate(records, expiry)
     end
   end
 
-  cache.values.each do |value|
-    frequencies[value[:frequency]] += 1
-  end
-
-  puts frequencies.sort_by(&:first).to_h
   puts stats
   puts "hit ratio: #{Float(stats[:hit])/records.size}"
   puts "max cache size: #{Float(stats[:max_size])/(1024*1024*1024)}GB"  # puts cache.values
-end
-
-def increment_last(array)
-  array[0..-2] + [array.last + 1]
 end
 
 def next_record
