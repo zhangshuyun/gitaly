f5007f855cca436d4d66e3fb769153541ecee6fb
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-03-24 10:07:23 +0100

    featureflag: Remove per-RPC transactional feature flags
    The per-RPC transactional feature flags have all been default-enabled in
    v13.10 and tested in production without any issues. Let's remove them
    altogether.

--- internal/metadata/featureflag/feature_flags.go
+++ internal/metadata/featureflag/feature_flags.go
@@ -40,71 +40,6 @@ var (
 	LFSPointersUseBitmapIndex = FeatureFlag{Name: "lfs_pointers_use_bitmap_index", OnByDefault: false}
 	// GoUpdateRemoteMirror enables the Go implementation of UpdateRemoteMirror
 	GoUpdateRemoteMirror = FeatureFlag{Name: "go_update_remote_mirror", OnByDefault: false}
-
-	// TxApplyBfgObjectMapStream enables transactions for ApplyBfgObjectMapStream
-	TxApplyBfgObjectMapStream = FeatureFlag{Name: "tx_apply_bfg_object_map_stream", OnByDefault: true}
-	// TxApplyGitattributes enables transactions for ApplyGitattributes
-	TxApplyGitattributes = FeatureFlag{Name: "tx_apply_gitattributes", OnByDefault: true}
-	// TxResolveConflicts enables transactions for ResolveConflicts
-	TxResolveConflicts = FeatureFlag{Name: "tx_resolve_conflicts", OnByDefault: true}
-	// TxFetchIntoObjectPool enables transactions for FetchIntoObjectPool
-	TxFetchIntoObjectPool = FeatureFlag{Name: "tx_fetch_into_object_pool", OnByDefault: true}
-	// TxUserApplyPatch enables transactions for UserApplyPatch
-	TxUserApplyPatch = FeatureFlag{Name: "tx_user_apply_patch", OnByDefault: true}
-	// TxUserCherryPick enables transactions for UserCherryPick
-	TxUserCherryPick = FeatureFlag{Name: "tx_user_cherry_pick", OnByDefault: true}
-	// TxUserCommitFiles enables transactions for UserCommitFiles
-	TxUserCommitFiles = FeatureFlag{Name: "tx_user_commit_files", OnByDefault: true}
-	// TxUserFFBranch enables transactions for UserFFBranch
-	TxUserFFBranch = FeatureFlag{Name: "tx_user_ff_branch", OnByDefault: true}
-	// TxUserMergeBranch enables transactions for UserMergeBranch
-	TxUserMergeBranch = FeatureFlag{Name: "tx_user_merge_branch", OnByDefault: true}
-	// TxUserMergeToRef enables transactions for UserMergeToRef
-	TxUserMergeToRef = FeatureFlag{Name: "tx_user_merge_to_ref", OnByDefault: true}
-	// TxUserRebaseConfirmable enables transactions for UserRebaseConfirmable
-	TxUserRebaseConfirmable = FeatureFlag{Name: "tx_user_rebase_confirmable", OnByDefault: true}
-	// TxUserRevert enables transactions for UserRevert
-	TxUserRevert = FeatureFlag{Name: "tx_user_revert", OnByDefault: true}
-	// TxUserSquash enables transactions for UserSquash
-	TxUserSquash = FeatureFlag{Name: "tx_user_squash", OnByDefault: true}
-	// TxUserUpdateSubmodule enables transactions for UserUpdateSubmodule
-	TxUserUpdateSubmodule = FeatureFlag{Name: "tx_user_update_submodule", OnByDefault: true}
-	// TxDeleteRefs enables transactions for DeleteRefs
-	TxDeleteRefs = FeatureFlag{Name: "tx_delete_refs", OnByDefault: true}
-	// TxAddRemote enables transactions for AddRemote
-	TxAddRemote = FeatureFlag{Name: "tx_add_remote", OnByDefault: true}
-	// TxFetchInternalRemote enables transactions for FetchInternalRemote
-	TxFetchInternalRemote = FeatureFlag{Name: "tx_fetch_internal_remote", OnByDefault: true}
-	// TxRemoveRemote enables transactions for RemoveRemote
-	TxRemoveRemote = FeatureFlag{Name: "tx_remove_remote", OnByDefault: true}
-	// TxUpdateRemoteMirror enables transactions for UpdateRemoteMirror
-	TxUpdateRemoteMirror = FeatureFlag{Name: "tx_update_remote_mirror", OnByDefault: true}
-	// TxCloneFromPool enables transactions for CloneFromPool
-	TxCloneFromPool = FeatureFlag{Name: "tx_clone_from_pool", OnByDefault: true}
-	// TxCloneFromPoolInternal enables transactions for CloneFromPoolInternal
-	TxCloneFromPoolInternal = FeatureFlag{Name: "tx_clone_from_pool_internal", OnByDefault: true}
-	// TxCreateFork enables transactions for CreateFork
-	TxCreateFork = FeatureFlag{Name: "tx_create_fork", OnByDefault: true}
-	// TxCreateRepositoryFromBundle enables transactions for CreateRepositoryFromBundle
-	TxCreateRepositoryFromBundle = FeatureFlag{Name: "tx_create_repository_from_bundle", OnByDefault: true}
-	// TxCreateRepositoryFromSnapshot enables transactions for CreateRepositoryFromSnapshot
-	TxCreateRepositoryFromSnapshot = FeatureFlag{Name: "tx_create_repository_from_snapshot", OnByDefault: true}
-	// TxCreateRepositoryFromURL enables transactions for CreateRepositoryFromURL
-	TxCreateRepositoryFromURL = FeatureFlag{Name: "tx_create_repository_from_u_r_l", OnByDefault: true}
-	// TxFetchRemote enables transactions for FetchRemote
-	TxFetchRemote = FeatureFlag{Name: "tx_fetch_remote", OnByDefault: true}
-	// TxFetchSourceBranch enables transactions for FetchSourceBranch
-	TxFetchSourceBranch = FeatureFlag{Name: "tx_fetch_source_branch", OnByDefault: true}
-	// TxReplicateRepository enables transactions for ReplicateRepository
-	TxReplicateRepository = FeatureFlag{Name: "tx_replicate_repository", OnByDefault: true}
-	// TxWriteRef enables transactions for WriteRef
-	TxWriteRef = FeatureFlag{Name: "tx_write_ref", OnByDefault: true}
-	// TxWikiDeletePage enables transactions for WikiDeletePage
-	TxWikiDeletePage = FeatureFlag{Name: "tx_wiki_delete_page", OnByDefault: true}
-	// TxWikiUpdatePage enables transactions for WikiUpdatePage
-	TxWikiUpdatePage = FeatureFlag{Name: "tx_wiki_update_page", OnByDefault: true}
-	// TxWikiWritePage enables transactions for WikiWritePage
-	TxWikiWritePage = FeatureFlag{Name: "tx_wiki_write_page", OnByDefault: true}
 )
 
 // All includes all feature flags.
@@ -123,37 +58,5 @@ var All = []FeatureFlag{
 	GoGetNewLFSPointers,
 	LFSPointersUseBitmapIndex,
 	GoUpdateRemoteMirror,
-	TxApplyBfgObjectMapStream,
-	TxApplyGitattributes,
-	TxResolveConflicts,
-	TxFetchIntoObjectPool,
-	TxUserApplyPatch,
-	TxUserCherryPick,
-	TxUserCommitFiles,
-	TxUserFFBranch,
-	TxUserMergeBranch,
-	TxUserMergeToRef,
-	TxUserRebaseConfirmable,
-	TxUserRevert,
-	TxUserSquash,
-	TxUserUpdateSubmodule,
-	TxDeleteRefs,
-	TxAddRemote,
-	TxFetchInternalRemote,
-	TxRemoveRemote,
-	TxUpdateRemoteMirror,
-	TxCloneFromPool,
-	TxCloneFromPoolInternal,
-	TxCreateFork,
-	TxCreateRepositoryFromBundle,
-	TxCreateRepositoryFromSnapshot,
-	TxCreateRepositoryFromURL,
-	TxFetchRemote,
-	TxFetchSourceBranch,
-	TxReplicateRepository,
-	TxWriteRef,
-	TxWikiDeletePage,
-	TxWikiUpdatePage,
-	TxWikiWritePage,
 	UploadPackGitalyHooks,
 }
