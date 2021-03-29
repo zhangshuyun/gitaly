5ee6602f09b481921b8b84a28581ad34525c384b
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-03-29 08:17:44 +0200

    cleanup: Use internal git command factory instead of ad-hoc one
    While the Cleanup service server has a git command factory available, we
    still create an ad-hoc one in `ApplyBfgObjectMapStream`. This is likely
    an oversight from the conversions.
    
    Convert the function to use the internal git command factory instead of
    creating one ad-hoc.

--- internal/gitaly/service/cleanup/apply_bfg_object_map_stream.go
+++ internal/gitaly/service/cleanup/apply_bfg_object_map_stream.go
@@ -5,7 +5,6 @@ import (
 	"io"
 
 	"github.com/golang/protobuf/proto"
-	"gitlab.com/gitlab-org/gitaly/internal/git"
 	"gitlab.com/gitlab-org/gitaly/internal/gitaly/service/cleanup/internalrefs"
 	"gitlab.com/gitlab-org/gitaly/internal/gitaly/service/cleanup/notifier"
 	"gitlab.com/gitlab-org/gitaly/internal/helper"
@@ -48,7 +47,7 @@ func (s *server) ApplyBfgObjectMapStream(server gitalypb.CleanupService_ApplyBfg
 
 	// It doesn't matter if new internal references are added after this RPC
 	// starts running - they shouldn't point to the objects removed by the BFG
-	cleaner, err := internalrefs.NewCleaner(ctx, s.cfg, git.NewExecCommandFactory(s.cfg), repo, notifier.Notify)
+	cleaner, err := internalrefs.NewCleaner(ctx, s.cfg, s.gitCmdFactory, repo, notifier.Notify)
 	if err != nil {
 		return helper.ErrInternal(err)
 	}
