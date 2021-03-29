58227523dde7806384fdb4460dc89a7456616209
Sami Hiltunen <shiltunen@gitlab.com>
2021-03-29 17:55:03 +0200

    configure defaults for yamux
    This commit configures defaults for yamux that match our fit our
    use case better:
    
    1. AcceptBacklog is set to 1, as we initiate at most one stream from
       bot sides of the connection. With that in mind, we should never have
       more than one stream waiting to be accepted on both sides. If we do,
       something is wrong and it's fine to error out.
    
    2. Yamux keep alives are disabled as we are already using gRPC to send
       keep alives. gRPC should be used for this so keep alives get sent to
       non-multiplexed connections as well.
    
    3. Maximum stream receive window size was increased from the default of
       256Kib to 16Mib. We only have two streams per connection  which
       carry all of the gRPC traffic. As such, we can give a more generous
       receive buffer to ensure the multiplexing does not bottleneck the
       connection. The buffer size is quite generous to ensure we have a good
       throughput even if the latency is higher between the Praefect's and the
       Gitalys. Given each Praefect holds a single connection to each Gitaly and
       we have two mux streams on each connection, the memory impact of of each
       connection is increased by a maximumx of 32Mib. This should be fine given
       even our 50k reference architechture only recommends 3 Gitaly nodes.
    
       That said, given that Gitaly Cluster should be deployed in a low latency
       environment, we probably could get by with a smaller buffer. We probably
       want to tweak the value further in the future or allow it to be configured.
       For now, this value should cover most cases and not have too big of a memory
       impact.

--- internal/backchannel/backchannel.go
+++ internal/backchannel/backchannel.go
@@ -46,6 +46,18 @@ var magicBytes = []byte("backchannel")
 func muxConfig(logger io.Writer) *yamux.Config {
 	cfg := yamux.DefaultConfig()
 	cfg.LogOutput = logger
+	// The server only accepts a single stream from the client, which is the client's gRPC stream.
+	// The backchannel server should only receive a single stream from the server. As such, we can
+	// limit maximum pending streams to 1 as there should never be more streams waiting.
+	cfg.AcceptBacklog = 1
+	// gRPC is already configured to send keep alives so we don't need yamux to do this for us.
+	// gRPC is a better choice as it sends the keep alives also to non-multiplexed connections.
+	cfg.EnableKeepAlive = false
+	// MaxStreamWindowSize configures the maximum receive buffer size for each stream. The sender
+	// is allowed to send the configured amount of bytes without receiving an acknowledgement from the
+	// receiver. This is can have a big impact on throughput as the latency increases, as the sender
+	// can't proceed sending without receiving an acknowledgement back.
+	cfg.MaxStreamWindowSize = 16 * 1024 * 1024
 	return cfg
 }
 
