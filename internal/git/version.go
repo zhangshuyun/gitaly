b4046f488f78d08a0e89dd0a3406324391ff9c93
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-04-07 08:18:39 +0200

    git: Update locations which need update on version bumps
    We have recently adopted a list of all locations which need to be
    updated when the minimum required git version is bumped. Because of the
    efforts to distribute a single git distribution across GitLab
    distributions with Gitaly's `make git` target, this list is now out of
    date:
    
        - Build images do not need to be updated when bumping the minimum
          required version. They must've been updated beforehand already.
    
        - CNG has recently started to use `make git` and thus does not need
          updating anymore.
    
    Drop both of these locations.

--- internal/git/version.go
+++ internal/git/version.go
@@ -13,10 +13,8 @@ var (
 	// also update the following locations:
 	// - https://gitlab.com/gitlab-org/gitaly/blob/master/README.md#installation
 	// - https://gitlab.com/gitlab-org/gitaly/blob/master/.gitlab-ci.yml
-	// - https://gitlab.com/gitlab-org/gitlab-build-images/blob/master/.gitlab-ci.yml
 	// - https://gitlab.com/gitlab-org/gitlab-foss/blob/master/.gitlab-ci.yml
 	// - https://gitlab.com/gitlab-org/gitlab-foss/blob/master/lib/system_check/app/git_version_check.rb
-	// - https://gitlab.com/gitlab-org/build/CNG/blob/master/ci_files/variables.yml
 	minimumVersion = Version{
 		versionString: "2.29.0",
 		major:         2,
