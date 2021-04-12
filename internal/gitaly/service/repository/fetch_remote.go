247d0a5dfa3363cb0f784ab064c5bb489583683f
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-04-12 09:29:11 +0200

    repository: Manually vote for `FetchRemote()` RPC
    When fetching references, git-fetch(1) will by default update each
    fetched ref in a separate reference transaction. As a conseuence, when
    fetching thousands of references, it'll be happy to execute the hook
    thousands of times. It goes without saying that this is expensive even
    without doing transactional voting, but in combination it's even more
    so. We've thus turned off transactional voting in the past as
    performance had been taking a huge hit.
    
    With git v2.31.0, a new `--atomic` flag was added to git-fetch(1) which
    causes it to update all references in a single pass, avoiding the
    problem. But while this flag is usable for RPCs which do force-fetches,
    it's not suitable for `FetchRemote()`: it may do unforced fetches, and
    it may even do mixed fetches where some refspecs are forced, while
    others are not. And naturally, callers rely on the fact that we update
    non-divergent refs while keeping divergent ones at their current value.
    But with `--atomic`, we'd convert this to an all-or-nothing fetch, where
    non-divergent refs would not be updated if there is any divergent one.
    So it's unfortunately a non-startet for `FetchRemote()`.
    
    To still enable transactional semantics, this commit implements manual
    voting on refs: after the fetch, we iterate over all refs and vote on
    the resulting hash. This is not an ideal solution as the fetch may have
    updated only parts of the local ref namespace, and furthermore it's
    inherently racy. But right now, we're always doing work twice: we first
    do the fetch on primary and all secondaries. Because none of the nodes
    are doing a vote, Praefect takes the safe way and schedules replication
    jobs from primary to secondaries, causing another update of the
    secondaries. So with this manual vote, we should at least be able to
    avoid replication for most cases, trading it for a failing RPC in case
    there was a race.
    
    This is only a rough first iteration: if we see that this is racing too
    often, there are some refinements we can do: first, we can start only
    hashing refs which are actually covered by a refspec. And second, we may
    enable the reference-transaction hook with atomic fetches in cases where
    we know that either all refspecs are forced ones, or `Force` flag is
    set.

--- internal/gitaly/service/repository/fetch_remote.go
+++ internal/gitaly/service/repository/fetch_remote.go
@@ -12,8 +12,11 @@ import (
 	"gitlab.com/gitlab-org/gitaly/internal/errors"
 	"gitlab.com/gitlab-org/gitaly/internal/git"
 	"gitlab.com/gitlab-org/gitaly/internal/git/localrepo"
+	"gitlab.com/gitlab-org/gitaly/internal/gitaly/transaction"
 	"gitlab.com/gitlab-org/gitaly/internal/helper"
+	"gitlab.com/gitlab-org/gitaly/internal/praefect/metadata"
 	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
@@ -95,6 +98,35 @@ func (s *server) FetchRemote(ctx context.Context, req *gitalypb.FetchRemoteReque
 		}
 
 		return nil, fmt.Errorf("fetch remote: %w", err)
+	}
+
+	// Ideally, we'd do the voting process via git-fetch(1) using the reference-transaction
+	// hook. But by default this would lead to one hook invocation per updated ref, which is
+	// infeasible performance-wise. While this could be fixed via the `--atomic` flag, that's
+	// not a solution either: we rely on the fact that refs get updated even if a subset of refs
+	// diverged, and with atomic transactions it would instead be an all-or-nothing operation.
+	//
+	// Instead, we do the second-best thing, which is to vote on the resulting references. This
+	// is of course racy and may conflict with other mutators, causing the vote to fail. But it
+	// is arguably preferable to accept races in favour always replicating. If loosing the race,
+	// we'd fail this RPC and schedule a replication job afterwards.
+	if err := transaction.RunOnContext(ctx, func(tx metadata.Transaction, praefect metadata.PraefectServer) error {
+		hash := transaction.NewVoteHash()
+
+		if err := repo.ExecAndWait(ctx, git.SubCmd{
+			Name: "for-each-ref",
+		}, git.WithStdout(hash)); err != nil {
+			return fmt.Errorf("cannot compute references vote: %w", err)
+		}
+
+		vote, err := hash.Vote()
+		if err != nil {
+			return err
+		}
+
+		return s.txManager.Vote(ctx, tx, praefect, vote)
+	}); err != nil {
+		return nil, status.Errorf(codes.Aborted, "failed vote on refs: %v", err)
 	}
 
 	out := &gitalypb.FetchRemoteResponse{TagsChanged: true}
