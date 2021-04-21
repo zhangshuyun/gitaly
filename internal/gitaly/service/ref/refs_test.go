d247c4f4aba136975f045b87dfa3360b7e4c0578
Pavlo Strokov <pstrokov@gitlab.com>
2021-04-21 15:05:12 +0300

    Introduce gittest.Exec to exec git commands
    The function testhelper.MustRunCommand designed for
    running commands in the shell with reporting back result
    of the execution. It server for too many purposes and
    behaves differently in case 'git' command needs to be run.
    Then it uses globally defined variable config.Config to
    get path to the git binary. Most of the tests now
    doesn't rely on that global variable and use one created
    locally for each test.
    In order to run only git related commands with locally defined
    configuration the new gittest.Exec function is added.
    As a use case example gittest.CreateRemoteBranch was changed
    to use gittest.Exec function and local configuration.
    
    Part of: https://gitlab.com/gitlab-org/gitaly/-/issues/2699

--- internal/gitaly/service/ref/refs_test.go
+++ internal/gitaly/service/ref/refs_test.go
@@ -929,7 +929,7 @@ func TestEmptyFindLocalBranchesRequest(t *testing.T) {
 }
 
 func TestSuccessfulFindAllBranchesRequest(t *testing.T) {
-	_, repo, repoPath, client := setupRefService(t)
+	cfg, repo, repoPath, client := setupRefService(t)
 
 	remoteBranch := &gitalypb.FindAllBranchesResponse_Branch{
 		Name: []byte("refs/remotes/origin/fake-remote-branch"),
@@ -956,7 +956,7 @@ func TestSuccessfulFindAllBranchesRequest(t *testing.T) {
 		},
 	}
 
-	gittest.CreateRemoteBranch(t, repoPath, "origin",
+	gittest.CreateRemoteBranch(t, cfg, repoPath, "origin",
 		"fake-remote-branch", remoteBranch.Target.Id)
 
 	request := &gitalypb.FindAllBranchesRequest{Repository: repo}
