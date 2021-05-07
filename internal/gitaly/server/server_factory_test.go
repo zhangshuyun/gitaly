e6cc68dfc7654a88f486402ec919ec82d131a19f
Sami Hiltunen <shiltunen@gitlab.com>
2021-05-07 16:04:44 +0200

    Close internal socket after the external ones
    During the graceful shutdown process, Gitaly's internal socket should
    close after the external request accepting sockets. If an external
    request is accepted into Gitaly and the graceful shutdown is initiated
    on all of the sockets, the request will not complete successfully if
    it tries to contact the HookService via the internal socket. This is
    due to the internal socket already performing a graceful shutdown
    as well, causing it to stop accept new RPCs. This can cause requests
    to fail during graceful upgrades.
    
    This commit fixes the problem by separating the internal sockets from
    the external sockets in the GitalyServerFactory. Doing so, the
    GracefulStop method can first close the external sockets and only
    after successfully draining them, also close the internal sockets.
    This ensures internal socket does not reject any RPCs already in-flight.
    When the external sockets complete their graceful shutdown, the
    connections should be fully drained and closing the internal sockets is
    safe.
    
    Changelog: fixed

--- internal/gitaly/server/server_factory_test.go
+++ internal/gitaly/server/server_factory_test.go
@@ -1,13 +1,16 @@
 package server
 
 import (
+	"context"
 	"crypto/tls"
 	"crypto/x509"
+	"errors"
 	"io/ioutil"
 	"net"
 	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/gitaly/client"
 	"gitlab.com/gitlab-org/gitaly/internal/backchannel"
@@ -36,7 +39,7 @@ func TestGitalyServerFactory(t *testing.T) {
 			require.NoError(t, err)
 			t.Cleanup(func() { listener.Close() })
 
-			srv, err := sf.Create(true)
+			srv, err := sf.CreateExternal(true)
 			require.NoError(t, err)
 			healthpb.RegisterHealthServer(srv, health.NewServer())
 			go srv.Serve(listener)
@@ -61,7 +64,7 @@ func TestGitalyServerFactory(t *testing.T) {
 			require.NoError(t, err)
 			t.Cleanup(func() { listener.Close() })
 
-			srv, err := sf.Create(false)
+			srv, err := sf.CreateExternal(false)
 			require.NoError(t, err)
 			healthpb.RegisterHealthServer(srv, health.NewServer())
 			go srv.Serve(listener)
@@ -124,4 +127,165 @@ func TestGitalyServerFactory(t *testing.T) {
 		_, socketErr := socketHealthClient.Check(ctx, &healthpb.HealthCheckRequest{})
 		require.Equal(t, codes.Unavailable, status.Code(socketErr))
 	})
+}
+
+func TestGitalyServerFactory_closeOrder(t *testing.T) {
+	ctx, cancel := testhelper.Context()
+	defer cancel()
+
+	cfg := testcfg.Build(t)
+	sf := NewGitalyServerFactory(cfg, backchannel.NewRegistry())
+	defer sf.Stop()
+
+	errQuickRPC := status.Error(codes.Internal, "quick RPC")
+	errBlockingRPC := status.Error(codes.Internal, "blocking RPC")
+
+	invokeQuick := func(conn *grpc.ClientConn, shouldSucceed bool) {
+		err := conn.Invoke(ctx, "/Service/Quick", &healthpb.HealthCheckRequest{}, &healthpb.HealthCheckRequest{})
+		if !shouldSucceed {
+			testhelper.RequireGrpcError(t, err, codes.Unavailable)
+			return
+		}
+
+		require.Equal(t, errQuickRPC, err)
+	}
+
+	invokeBlocking := func(conn *grpc.ClientConn) chan struct{} {
+		rpcFinished := make(chan struct{})
+
+		go func() {
+			defer close(rpcFinished)
+			assert.Equal(t,
+				errBlockingRPC,
+				conn.Invoke(ctx, "/Service/Blocking", &healthpb.HealthCheckRequest{}, &healthpb.HealthCheckRequest{}),
+			)
+		}()
+
+		return rpcFinished
+	}
+
+	waitUntilFailure := func(conn *grpc.ClientConn) {
+		for {
+			err := conn.Invoke(ctx, "/Service/Quick", &healthpb.HealthCheckRequest{}, &healthpb.HealthCheckRequest{})
+			if errors.Is(err, errQuickRPC) {
+				continue
+			}
+
+			testhelper.RequireGrpcError(t, err, codes.Unavailable)
+			break
+		}
+	}
+
+	var internalConn, externalConn *grpc.ClientConn
+	var internalIsBlocking, externalIsBlocking chan struct{}
+	var releaseInternalBlock, releaseExternalBlock chan struct{}
+	for _, builder := range []struct {
+		createServer func() *grpc.Server
+		conn         **grpc.ClientConn
+		isBlocking   *chan struct{}
+		releaseBlock *chan struct{}
+	}{
+		{
+			createServer: func() *grpc.Server {
+				server, err := sf.CreateInternal()
+				require.NoError(t, err)
+				return server
+			},
+			conn:         &internalConn,
+			isBlocking:   &internalIsBlocking,
+			releaseBlock: &releaseInternalBlock,
+		},
+		{
+			createServer: func() *grpc.Server {
+				server, err := sf.CreateExternal(false)
+				require.NoError(t, err)
+				return server
+			},
+			conn:         &externalConn,
+			isBlocking:   &externalIsBlocking,
+			releaseBlock: &releaseExternalBlock,
+		},
+	} {
+		server := builder.createServer()
+
+		releaseBlock := make(chan struct{})
+		*builder.releaseBlock = releaseBlock
+
+		isBlocking := make(chan struct{})
+		*builder.isBlocking = isBlocking
+
+		server.RegisterService(&grpc.ServiceDesc{
+			ServiceName: "Service",
+			Methods: []grpc.MethodDesc{
+				{
+					MethodName: "Quick",
+					Handler: func(interface{}, context.Context, func(interface{}) error, grpc.UnaryServerInterceptor) (interface{}, error) {
+						return nil, errQuickRPC
+					},
+				},
+				{
+					MethodName: "Blocking",
+					Handler: func(interface{}, context.Context, func(interface{}) error, grpc.UnaryServerInterceptor) (interface{}, error) {
+						close(isBlocking)
+						<-releaseBlock
+						return nil, errBlockingRPC
+					},
+				},
+			},
+			HandlerType: (*interface{})(nil),
+		}, server)
+
+		ln, err := net.Listen("tcp", "localhost:0")
+		require.NoError(t, err)
+		defer ln.Close()
+
+		go server.Serve(ln)
+
+		*builder.conn, err = grpc.DialContext(ctx, ln.Addr().String(), grpc.WithInsecure())
+		require.NoError(t, err)
+	}
+
+	// both servers should be up and accepting RPCs
+	invokeQuick(externalConn, true)
+	invokeQuick(internalConn, true)
+
+	// invoke a blocking RPC on the external server to block the graceful shutdown
+	invokeBlocking(externalConn)
+	<-externalIsBlocking
+
+	shutdownCompeleted := make(chan struct{})
+	go func() {
+		defer close(shutdownCompeleted)
+		sf.GracefulStop()
+	}()
+
+	// wait until the graceful shutdown is in progress and new RPCs are no longer accepted on the
+	// external servers
+	waitUntilFailure(externalConn)
+
+	// internal sockets should still accept RPCs even if external sockets are gracefully closing.
+	invokeQuick(internalConn, true)
+
+	// block on the internal server
+	internalBlockingRPCFinished := invokeBlocking(internalConn)
+	<-internalIsBlocking
+
+	// release the external server's blocking RPC so the graceful shutdown can complete and proceed to
+	// shutting down the internal servers.
+	close(releaseExternalBlock)
+
+	// wait until the graceful shutdown is in progress and new RPCs are no longer accepted on the internal
+	// servers
+	waitUntilFailure(internalConn)
+
+	// neither internal nor external servers should be accepting new RPCs anymore
+	invokeQuick(externalConn, false)
+	invokeQuick(internalConn, false)
+
+	// wait until the blocking rpc has successfully completed
+	close(releaseInternalBlock)
+	<-internalBlockingRPCFinished
+
+	// wait until the graceful shutdown completes
+	<-shutdownCompeleted
 }
