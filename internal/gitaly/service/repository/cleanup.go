7a1d224d0ee6208350df12279b0f7319e9e0070c
Will Chandler <wchandler@gitlab.com>
2021-05-17 23:49:52 -0400

    repository: Remove housekeeping from Cleanup RPC
    The `Cleanup()` RPC is run synchronously when a push is received,
    creating the risk that a failure here will block users from pushing
    their changes. In https://gitlab.com/gitlab-org/gitaly/-/issues/3581 we
    found that the stale files and refs checks run as part of housekeeping
    may exceed 60 seconds for larger repositories hosted on NFS, causing
    pushes to consistently timeout for some self-managed customers.
    
    This commit removes the housekeeping call from `Cleanup()` and moves
    the tests performed on housekeeping-specific functionality over to
    `GarbageCollect()`, which is now the most frequently executed RPC that
    runs housekeeping.
    
    Changelog: fixed

--- internal/gitaly/service/repository/cleanup.go
+++ internal/gitaly/service/repository/cleanup.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"gitlab.com/gitlab-org/gitaly/internal/git"
-	"gitlab.com/gitlab-org/gitaly/internal/git/housekeeping"
 	"gitlab.com/gitlab-org/gitaly/internal/git/localrepo"
 	"gitlab.com/gitlab-org/gitaly/proto/go/gitalypb"
 	"google.golang.org/grpc/codes"
@@ -37,10 +36,6 @@ func (s *server) cleanupRepo(ctx context.Context, repo *localrepo.Repo) error {
 
 	if err := s.cleanDisconnectedWorktrees(ctx, repo); err != nil {
 		return status.Errorf(codes.Internal, "Cleanup: cleanDisconnectedWorktrees: %v", err)
-	}
-
-	if err := housekeeping.Perform(ctx, repo); err != nil {
-		return status.Errorf(codes.Internal, "Cleanup: houskeeping: %v", err)
 	}
 
 	return nil
