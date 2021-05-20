0e96a90fdc8881561e7ec31b129df96015b82269
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-05-20 12:41:13 +0200

    catfile: Evict cached processes when stopped
    When stopping the catfile batch cache, we should also evict all current
    entries. This fixes a bunch of races we have seen where there's still
    git-cat-file(1) processes around after tests have concluded.

--- internal/git/catfile/batch_cache.go
+++ internal/git/catfile/batch_cache.go
@@ -87,11 +87,13 @@ func NewCache(cfg config.Cfg) *BatchCache {
 	return newCache(defaultBatchfileTTL, cfg.Git.CatfileCacheSize, defaultEvictionInterval)
 }
 
-// Stop stops the monitoring Goroutine. This must only be called once.
+// Stop stops the monitoring Goroutine and evicts all cached processes. This must only be called
+// once.
 func (bc *BatchCache) Stop() {
 	bc.monitorTicker.Stop()
 	bc.monitorDone <- struct{}{}
 	<-bc.monitorDone
+	bc.Evict()
 }
 
 func newCache(ttl time.Duration, maxLen int, refreshInterval time.Duration) *BatchCache {
