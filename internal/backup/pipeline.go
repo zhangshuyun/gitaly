e6126d6eaca389be3d1557c8a64a270fa710b85d
James Fargher <proglottis@gmail.com>
2021-05-21 12:12:00 +1200

    Add new backup pipeline that creates parallel backups limited by storage

--- internal/backup/pipeline.go
+++ internal/backup/pipeline.go
@@ -135,7 +135,10 @@ func (p *ParallelCreatePipeline) Done() error {
 	if err := p.next.Done(); err != nil {
 		return err
 	}
-	return p.err
+	if p.err != nil {
+		return fmt.Errorf("pipeline: %w", p.err)
+	}
+	return nil
 }
 
 func (p *ParallelCreatePipeline) startWorkers() {
@@ -158,6 +161,98 @@ func (p *ParallelCreatePipeline) worker() {
 }
 
 func (p *ParallelCreatePipeline) setErr(err error) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if p.err != nil {
+		return
+	}
+	p.err = err
+}
+
+// ParallelStorageCreatePipeline is a pipeline that creates backups in parallel limited per storage
+type ParallelStorageCreatePipeline struct {
+	next CreatePipeline
+	n    int
+
+	wg   sync.WaitGroup
+	done chan struct{}
+
+	mu       sync.Mutex
+	requests map[string]chan *CreateRequest
+	err      error
+}
+
+// NewParallelStorageCreatePipeline creates a new ParallelStorageCreatePipeline
+// where `next` is the pipeline called to create the backups and `n` is the
+// number of parallel backups that will run per storage. Since the number of
+// storages is unknown at initialisation, workers are created lazily as new
+// storage names are encountered.
+func NewParallelStorageCreatePipeline(next CreatePipeline, n int) *ParallelStorageCreatePipeline {
+	return &ParallelStorageCreatePipeline{
+		next:     next,
+		n:        n,
+		done:     make(chan struct{}),
+		requests: make(map[string]chan *CreateRequest),
+	}
+}
+
+// Create queues a request to create a backup. Requests are processed by
+// n-workers per storage.
+func (p *ParallelStorageCreatePipeline) Create(ctx context.Context, req *CreateRequest) {
+	ch := p.getStorage(req.Repository.StorageName)
+
+	select {
+	case <-ctx.Done():
+		p.setErr(ctx.Err())
+	case ch <- req:
+	}
+}
+
+// Done waits for any in progress calls to `Create` to complete then reports any accumulated errors
+func (p *ParallelStorageCreatePipeline) Done() error {
+	close(p.done)
+	p.wg.Wait()
+	if err := p.next.Done(); err != nil {
+		return err
+	}
+	if p.err != nil {
+		return fmt.Errorf("pipeline: %w", p.err)
+	}
+	return nil
+}
+
+// getStorage finds the channel associated with a storage. When no channel is
+// found, one is created and n-workers are started to process requests.
+func (p *ParallelStorageCreatePipeline) getStorage(storage string) chan<- *contextCreateRequest {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+
+	ch, ok := p.requests[storage]
+	if !ok {
+		ch = make(chan *CreateRequest)
+		p.requests[storage] = ch
+
+		for i := 0; i < p.n; i++ {
+			p.wg.Add(1)
+			go p.worker(ch)
+		}
+	}
+	return ch
+}
+
+func (p *ParallelStorageCreatePipeline) worker(ch <-chan *CreateRequest) {
+	defer p.wg.Done()
+	for {
+		select {
+		case <-p.done:
+			return
+		case req := <-ch:
+			p.next.Create(context.TODO(), req)
+		}
+	}
+}
+
+func (p *ParallelStorageCreatePipeline) setErr(err error) {
 	p.mu.Lock()
 	defer p.mu.Unlock()
 	if p.err != nil {
