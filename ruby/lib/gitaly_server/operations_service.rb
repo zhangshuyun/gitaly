02d6da5f73c180a334f86a22e1adb39451528c77
Toon Claes <toon@gitlab.com>
2021-06-30 09:37:12 +0200

    ruby: Remove UserRebaseConfirmable implementation
    In [1] we removed all code to call the Ruby implementation of
    UserRebaseConfirmable. This happened in the previous minor release so
    now it's time to remove the Ruby implementation completely.
    
    1. 8f5574579 (UserRebaseConfirmable: Remove feature flag, 2021-05-31)

--- ruby/lib/gitaly_server/operations_service.rb
+++ ruby/lib/gitaly_server/operations_service.rb
@@ -2,45 +2,6 @@ module GitalyServer
   class OperationsService < Gitaly::OperationService::Service
     include Utils
 
-    # rubocop:disable Metrics/AbcSize
-    def user_rebase_confirmable(session, call)
-      Enumerator.new do |y|
-        header = session.next.header
-        transaction = Praefect::Transaction.from_metadata(call.metadata)
-
-        repo = Gitlab::Git::Repository.from_gitaly(header.repository, call)
-        user = Gitlab::Git::User.from_gitaly(header.user)
-        remote_repository = Gitlab::Git::GitalyRemoteRepository.new(header.remote_repository, call)
-
-        begin
-          repo.rebase(
-            user,
-            header.rebase_id,
-            branch: header.branch,
-            branch_sha: header.branch_sha,
-            remote_repository: remote_repository,
-            remote_branch: header.remote_branch,
-            push_options: Gitlab::Git::PushOptions.new(header.git_push_options),
-            timestamp: header.timestamp,
-            transaction: transaction
-          ) do |rebase_sha|
-            y << Gitaly::UserRebaseConfirmableResponse.new(rebase_sha: rebase_sha)
-
-            raise GRPC::FailedPrecondition.new('rebase aborted by client') unless session.next.apply
-          end
-
-          y << Gitaly::UserRebaseConfirmableResponse.new(rebase_applied: true)
-        rescue Gitlab::Git::PreReceiveError => e
-          y << Gitaly::UserRebaseConfirmableResponse.new(pre_receive_error: set_utf8!(e.message))
-        rescue Gitlab::Git::Repository::GitError => e
-          y << Gitaly::UserRebaseConfirmableResponse.new(git_error: set_utf8!(e.message))
-        rescue Gitlab::Git::CommitError => e
-          raise GRPC::FailedPrecondition.new(e.message)
-        end
-      end
-    end
-    # rubocop:enable Metrics/AbcSize
-
     def user_apply_patch(call)
       stream = call.each_remote_read
       first_request = stream.next
@@ -71,15 +32,6 @@ module GitalyServer
         repo_created: gitlab_update_result.repo_created,
         branch_created: gitlab_update_result.branch_created
       )
-    end
-
-    def get_param!(request, name)
-      value = request[name.to_s]
-
-      return value if value.present?
-
-      field_name = name.to_s.tr('_', ' ')
-      raise GRPC::InvalidArgument.new("empty #{field_name}")
     end
   end
 end
