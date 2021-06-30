62ed25098036ea988d3592ede7b72ac4220029a1
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-06-30 15:43:36 +0200

    repository: Fix repo replication with transactions
    It was reported that repository replication is broken when it's invoked
    via Praefect with strong consistency. The root cause for this is the
    same as in commit 0a92b3a09 (repository: Fix voting when cloning from
    internal pools, 2021-06-14): we do nested mutating RPC calls to the same
    Gitaly node with the incoming context converted to an outgoing one. As a
    result, we also forward transaction information to the Gitaly peer. This
    causes the peer to cast votes on the backchannel, but the backchannel
    information refers to the Praefect peer and not to the immediate Gitaly
    peer, and the vote thus fails.
    
    Fix the issue by invoking `FetchInternalRemote()` directly instead of
    doing so via a gRPC connection. The backchannel information thus applies
    to the correct connection and Gitaly can correctly cast its votes.
    
    Changelog: fixed

--- internal/metadata/featureflag/feature_flags.go
+++ internal/metadata/featureflag/feature_flags.go
@@ -20,6 +20,10 @@ var (
 	// order to reduce the number of transactional votes.
 	CreateRepositoryFromBundleAtomicFetch = FeatureFlag{Name: "create_repository_from_bundle_atomic_fetch", OnByDefault: false}
 	ResolveConflictsWithHooks             = FeatureFlag{Name: "resolve_conflicts_with_hooks", OnByDefault: false}
+	// ReplicateRepositoryDirectFetch will cause the ReplicateRepository RPC to perform fetches
+	// via a direct call instead of doing an RPC call to its own server. This fixes calls of
+	// `ReplicateRepository()` in case it's invoked via Praefect with transactions enabled.
+	ReplicateRepositoryDirectFetch = FeatureFlag{Name: "replicate_repository_direct_fetch", OnByDefault: false}
 )
 
 // All includes all feature flags.
@@ -29,4 +33,5 @@ var All = []FeatureFlag{
 	LFSPointersPipeline,
 	CreateRepositoryFromBundleAtomicFetch,
 	ResolveConflictsWithHooks,
+	ReplicateRepositoryDirectFetch,
 }
