56a5207b90abc40e67cbf2f7bedcf916daa8e2eb
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-07-07 07:38:54 +0200

    tempdir: Refactor how new tempdirs are created
    Currently, creating new tempdirs is handled mostly in
    `newAsRepository()`, which creates a tempdir and then returns both the
    tempdir and a Repository protobuf pointing into that tempdir. This is
    quite weird given that two of three callers don't actually care about
    the repository and just discard it.
    
    Refactor the function into a generic `newDirectory()` function whose
    only concern it is to create the tempdir in a specific location.
    Assembly of the protobuf is moved into `NewAsRepository()`.

--- internal/tempdir/tempdir.go
+++ internal/tempdir/tempdir.go
@@ -15,43 +15,51 @@ import (
 // ForDeleteAllRepositories returns a temporary directory for the given storage. It is not context-scoped but it will get removed eventuall (after MaxAge).
 func ForDeleteAllRepositories(locator storage.Locator, storageName string) (string, error) {
 	prefix := fmt.Sprintf("%s-repositories.old.%d.", storageName, time.Now().Unix())
-	_, path, err := newAsRepository(context.Background(), storageName, prefix, locator)
-
-	return path, err
+	return newDirectory(context.Background(), storageName, prefix, locator)
 }
 
-// New returns the path of a new temporary directory for use with the
-// repository. The directory is removed with os.RemoveAll when ctx
-// expires.
+// New returns the path of a new temporary directory for use with the repository. The directory is
+// removed with os.RemoveAll when the context expires.
 func New(ctx context.Context, repo *gitalypb.Repository, locator storage.Locator) (string, error) {
-	_, path, err := NewAsRepository(ctx, repo, locator)
+	return newDirectory(ctx, repo.StorageName, "repo", locator)
+}
+
+// NewAsRepository is the same as New, but it returns a *gitalypb.Repository for the created
+// directory as well as the bare path as a string
+func NewAsRepository(ctx context.Context, repo *gitalypb.Repository, locator storage.Locator) (*gitalypb.Repository, string, error) {
+	storagePath, err := locator.GetStorageByName(repo.StorageName)
+	if err != nil {
+		return nil, "", err
+	}
+
+	path, err := New(ctx, repo, locator)
+	if err != nil {
+		return nil, "", err
+	}
+
+	newRepo := &gitalypb.Repository{StorageName: repo.StorageName}
+	newRepo.RelativePath, err = filepath.Rel(storagePath, path)
+	if err != nil {
+		return nil, "", err
+	}
+
+	return newRepo, path, nil
+}
+
+func newDirectory(ctx context.Context, storageName string, prefix string, loc storage.Locator) (string, error) {
+	storagePath, err := loc.GetStorageByName(storageName)
 	if err != nil {
 		return "", err
 	}
 
-	return path, nil
-}
-
-// NewAsRepository is the same as New, but it returns a *gitalypb.Repository for the
-// created directory as well as the bare path as a string
-func NewAsRepository(ctx context.Context, repo *gitalypb.Repository, loc storage.Locator) (*gitalypb.Repository, string, error) {
-	return newAsRepository(ctx, repo.StorageName, "repo", loc)
-}
-
-func newAsRepository(ctx context.Context, storageName string, prefix string, loc storage.Locator) (*gitalypb.Repository, string, error) {
-	storagePath, err := loc.GetStorageByName(storageName)
-	if err != nil {
-		return nil, "", err
-	}
-
 	root := AppendTempDir(storagePath)
 	if err := os.MkdirAll(root, 0700); err != nil {
-		return nil, "", err
+		return "", err
 	}
 
 	tempDir, err := ioutil.TempDir(root, prefix)
 	if err != nil {
-		return nil, "", err
+		return "", err
 	}
 
 	go func() {
@@ -59,7 +67,5 @@ func newAsRepository(ctx context.Context, storageName string, prefix string, loc
 		os.RemoveAll(tempDir)
 	}()
 
-	newAsRepo := &gitalypb.Repository{StorageName: storageName}
-	newAsRepo.RelativePath, err = filepath.Rel(storagePath, tempDir)
-	return newAsRepo, tempDir, err
+	return tempDir, err
 }
