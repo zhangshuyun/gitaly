547e6dd90b652641bec24536bc56efd97a5d74a5
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-07-28 09:26:03 +0200

    git2go: rebase: Allow passing object IDs
    The git2go rebase command currently only accepts a branch name and
    upstream revision as inputs. While the latter is fine, requiring a
    branch name is overly restrictive: all we want to have is a commit which
    shall be rebased onto the upstream revision, and we may not necessarily
    have a branch available. Updating references never happens inside of
    gitaly-git2go either, but instead in the calling context.
    
    Amend the command struct to also support a commit ID as input,
    deprecating the old branch name parameter. For backwards compatibility
    reasons, the branch name needs to stay around for at least one release.
    To align types, this also introduces a new `UpstreamCommitID` field
    which deprecates the previous `UpstreamRevision` field.

--- internal/git2go/rebase.go
+++ internal/git2go/rebase.go
@@ -13,10 +13,19 @@ type RebaseCommand struct {
 	Repository string
 	// Committer contains the the committer signature.
 	Committer Signature
-	// BranchName is the branch that is rebased.
+	// BranchName is the branch that is rebased. Deprecated, can be removed in the next release.
 	BranchName string
-	// UpstreamRevision is the revision where the branch is rebased onto.
+	// UpstreamRevision is the revision where the branch is rebased onto. Deprecated, can be
+	// removed in the next release.
 	UpstreamRevision string
+	// CommitID is the object ID of the commit that shall be rebased. Deprecates BranchName.
+	CommitID git.ObjectID
+	// UpstreamCommitID is the object ID of the commit which is considered to be the
+	// upstream branch. This parameter determines both the commit onto which we're
+	// about to rebase, which is the merge base of the upstream commit and rebased
+	// commit, and which commits should be rebased, which is the commit range
+	// upstream..commit. Deprecates the UpstreamRevision.
+	UpstreamCommitID git.ObjectID
 }
 
 // Rebase performs the rebase via gitaly-git2go
