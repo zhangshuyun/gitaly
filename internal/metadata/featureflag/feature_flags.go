6490360d504c39bf6ebe9bf5414ba91ed4b7f6c5
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-07-28 12:50:43 +0200

    repository: Remove feature flag for direct fetches
    With the `ReplicateRepositoryDirectFetch` feature flag, we have fixed
    `FetchInternalRemote()` to work in a context where called via Praefect.
    Previously, we have propagated transactional information via an RPC call,
    which then resulted in the remote side trying to cast votes against a
    Gitaly connection, which cannot work. With the new code, we don't do an
    RPC call but instead just call the relevant function directly.
    
    This feature has been default-enabled for three weeks in production now,
    and furthermore it's been backported to v13.12 and v14.0. No problems
    were observed in production, and it fixed the original issue for
    customers.
    
    Given its exposure, let's remove the feature flag without taking the
    intermediate step of first default-enabling it.
    
    Changelog: fixed

--- internal/metadata/featureflag/feature_flags.go
+++ internal/metadata/featureflag/feature_flags.go
@@ -12,10 +12,6 @@ var (
 	// ResolveConflictsWithHooks will cause the ResolveConflicts RPC to run Git hooks after committing changes
 	// to the branch.
 	ResolveConflictsWithHooks = FeatureFlag{Name: "resolve_conflicts_with_hooks", OnByDefault: true}
-	// ReplicateRepositoryDirectFetch will cause the ReplicateRepository RPC to perform fetches
-	// via a direct call instead of doing an RPC call to its own server. This fixes calls of
-	// `ReplicateRepository()` in case it's invoked via Praefect with transactions enabled.
-	ReplicateRepositoryDirectFetch = FeatureFlag{Name: "replicate_repository_direct_fetch", OnByDefault: false}
 	// FindAllTagsPipeline enables the alternative pipeline implementation for finding
 	// tags via FindAllTags.
 	FindAllTagsPipeline = FeatureFlag{Name: "find_all_tags_pipeline", OnByDefault: false}
@@ -37,7 +33,6 @@ var All = []FeatureFlag{
 	GoSetConfig,
 	CreateRepositoryFromBundleAtomicFetch,
 	ResolveConflictsWithHooks,
-	ReplicateRepositoryDirectFetch,
 	FindAllTagsPipeline,
 	TxRemoveRepository,
 	QuarantinedUserCreateTag,
