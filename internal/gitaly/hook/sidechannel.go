66b09260567473d458235606f8af38d1ca0cb45f
Jacob Vosmaer <jacob@gitlab.com>
2021-08-12 10:53:41 +0000

    Add hook sidechannels
    This adds library code that allows hooks (i.e. incantations of
    gitaly-hooks) to establish a Unix socket sidechannel to the Gitaly
    server. This side channel can be used for high-volume data transfers,
    where gRPC would be a bottleneck.

---
+++ internal/gitaly/hook/sidechannel.go
@@ -0,0 +1,109 @@
+package hook
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"os"
+	"path"
+	"time"
+
+	gitaly_metadata "gitlab.com/gitlab-org/gitaly/v14/internal/metadata"
+	"google.golang.org/grpc/metadata"
+)
+
+const (
+	sidechannelHeader = "gitaly-sidechannel-socket"
+	sidechannelSocket = "sidechannel"
+)
+
+type errInvalidSidechannelAddress struct{ string }
+
+func (e *errInvalidSidechannelAddress) Error() string {
+	return fmt.Sprintf("invalid side channel address: %q", e.string)
+}
+
+// GetSidechannel looks for a sidechannel address in an incoming context
+// and establishes a connection if it finds an address.
+func GetSidechannel(ctx context.Context) (net.Conn, error) {
+	address := gitaly_metadata.GetValue(ctx, sidechannelHeader)
+	if path.Base(address) != sidechannelSocket {
+		return nil, &errInvalidSidechannelAddress{address}
+	}
+
+	return net.DialTimeout("unix", address, time.Second)
+}
+
+// SetupSidechannel creates a sidechannel listener in a tempdir and
+// launches a goroutine that will run the callback if the listener
+// receives a connection. The address of the listener is stored in the
+// returned context, so that the caller can propagate it to a server. The
+// caller must Close the SidechannelWaiter to prevent resource leaks.
+func SetupSidechannel(ctx context.Context, callback func(*net.UnixConn) error) (context.Context, *SidechannelWaiter, error) {
+	socketDir, err := ioutil.TempDir("", "gitaly")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	address := path.Join(socketDir, sidechannelSocket)
+	l, err := net.ListenUnix("unix", &net.UnixAddr{Net: "unix", Name: address})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	wt := &SidechannelWaiter{
+		errC:      make(chan error),
+		socketDir: socketDir,
+		listener:  l,
+	}
+	go wt.run(callback)
+
+	ctx = metadata.AppendToOutgoingContext(ctx, sidechannelHeader, address)
+	return ctx, wt, nil
+}
+
+// SidechannelWaiter provides cleanup and error propagation for a
+// sidechannel callback.
+type SidechannelWaiter struct {
+	errC      chan error
+	socketDir string
+	listener  *net.UnixListener
+}
+
+func (wt *SidechannelWaiter) run(callback func(*net.UnixConn) error) {
+	defer close(wt.errC)
+
+	wt.errC <- func() error {
+		c, err := wt.listener.AcceptUnix()
+		if err != nil {
+			return err
+		}
+		defer c.Close()
+
+		return callback(c)
+	}()
+}
+
+// Close cleans up sidechannel resources. If the callback is already
+// running, Close will block until the callback is done.
+func (wt *SidechannelWaiter) Close() error {
+	// Run all cleanup actions _before_ checking errors, so that we cannot
+	// forget one.
+	cleanupErrors := []error{
+		wt.listener.Close(),
+		os.RemoveAll(wt.socketDir),
+		wt.Wait(),
+	}
+
+	for _, err := range cleanupErrors {
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Wait waits for the callback to run and returns its error value.
+func (wt *SidechannelWaiter) Wait() error { return <-wt.errC }
---
+++ internal/gitaly/hook/sidechannel_test.go
@@ -0,0 +1,60 @@
+package hook
+
+import (
+	"context"
+	"io"
+	"io/ioutil"
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/helper"
+	"google.golang.org/grpc/metadata"
+)
+
+func TestSidechannel(t *testing.T) {
+	// Client side
+	ctxOut, wt, err := SetupSidechannel(
+		context.Background(),
+		func(c *net.UnixConn) error {
+			_, err := io.WriteString(c, "ping")
+			return err
+		},
+	)
+	require.NoError(t, err)
+	defer wt.Close()
+
+	// Server side
+	ctxIn := helper.OutgoingToIncoming(ctxOut)
+	c, err := GetSidechannel(ctxIn)
+	require.NoError(t, err)
+	defer c.Close()
+
+	buf, err := ioutil.ReadAll(c)
+	require.NoError(t, err)
+	require.Equal(t, "ping", string(buf))
+
+	// Client side
+	require.NoError(t, wt.Wait())
+}
+
+func TestGetSidechannel(t *testing.T) {
+	testCases := []string{
+		"foobar",
+		"sc.foo/../../bar",
+		"foo/../../bar",
+		"/etc/passwd",
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc, func(t *testing.T) {
+			ctx := metadata.NewIncomingContext(
+				context.Background(),
+				map[string][]string{sidechannelHeader: []string{tc}},
+			)
+			_, err := GetSidechannel(ctx)
+			require.Error(t, err)
+			require.Equal(t, &errInvalidSidechannelAddress{tc}, err)
+		})
+	}
+}
