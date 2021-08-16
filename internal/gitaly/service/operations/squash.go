6e5bf3a4d062e1eb5d8e15784139f05a754a4a53
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-08-16 11:26:45 +0200

    operations: Always enable squashing without worktrees
    Starting with 007b43e56 (operations: Support worktreeless squashing for
    non-fast-forward merges, 2021-07-28), we suppor squashing without any
    worktrees in all cases. This both improves performance by ~4x and also
    fixes issue we've had with patching the worktree when binary files were
    modified.
    
    This feature flag has been rolled out to production on August 9th, and
    no issues have been observed. This commit thus removes the feature flag
    altogether such that we never use worktrees for squashes anymore.
    
    Changelog: performance
    Changelog: bug

--- internal/gitaly/service/operations/squash.go
+++ internal/gitaly/service/operations/squash.go
@@ -5,26 +5,19 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"io"
-	"io/ioutil"
 	"math/rand"
-	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus/ctxlogrus"
-	"gitlab.com/gitlab-org/gitaly/v14/internal/command"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/git"
-	"gitlab.com/gitlab-org/gitaly/v14/internal/git/alternates"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/git2go"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/helper"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/helper/text"
-	"gitlab.com/gitlab-org/gitaly/v14/internal/metadata/featureflag"
 	"gitlab.com/gitlab-org/gitaly/v14/proto/go/gitalypb"
 )
 
 const (
-	squashWorktreePrefix  = "squash"
 	gitlabWorktreesSubDir = "gitlab-worktree"
 	ambiguousArgumentFmt  = "fatal: ambiguous argument '%s...%s': unknown revision or path not in the working tree.\nUse '--' to separate paths from revisions, like this:\n'git <command> [<revision>...] -- [<file>...]'\n"
 )
@@ -38,14 +31,7 @@ func (s *Server) UserSquash(ctx context.Context, req *gitalypb.UserSquashRequest
 		return nil, helper.ErrInvalidArgument(errors.New("worktree id can't contain slashes"))
 	}
 
-	repo := req.GetRepository()
-	repoPath, err := s.locator.GetRepoPath(repo)
-	if err != nil {
-		return nil, helper.ErrInternal(fmt.Errorf("repo path: %w", err))
-	}
-	env := alternates.Env(repoPath, repo.GetGitObjectDirectory(), repo.GetGitAlternateObjectDirectories())
-
-	sha, err := s.userSquash(ctx, req, env, repoPath)
+	sha, err := s.userSquash(ctx, req)
 	if err != nil {
 		var gitErr gitError
 		if errors.As(err, &gitErr) {
@@ -121,270 +107,99 @@ func (er gitError) Error() string {
 	return er.ErrMsg + ": " + er.Err.Error()
 }
 
-func (s *Server) userSquash(ctx context.Context, req *gitalypb.UserSquashRequest, env []string, repoPath string) (string, error) {
-	if featureflag.UserSquashWithoutWorktree.IsEnabled(ctx) {
-		repo := s.localrepo(req.GetRepository())
-		repoPath, err := s.locator.GetRepoPath(repo)
-		if err != nil {
-			return "", helper.ErrInternalf("cannot resolve repo path: %w", err)
-		}
+func (s *Server) userSquash(ctx context.Context, req *gitalypb.UserSquashRequest) (string, error) {
+	repo := s.localrepo(req.GetRepository())
 
-		// We need to retrieve the start commit such that we can create the new commit with
-		// all parents of the start commit.
-		startCommit, err := repo.ResolveRevision(ctx, git.Revision(req.GetStartSha()+"^{commit}"))
-		if err != nil {
-			return "", fmt.Errorf("cannot resolve start commit: %w", gitError{
-				// This error is simply for backwards compatibility. We should just
-				// return the plain error eventually.
-				Err:    err,
-				ErrMsg: fmt.Sprintf(ambiguousArgumentFmt, req.GetStartSha(), req.GetEndSha()),
-			})
-		}
+	repoPath, err := repo.Path()
+	if err != nil {
+		return "", helper.ErrInternalf("cannot resolve repo path: %w", err)
+	}
 
-		// And we need to take the tree of the end commit. This tree already is the result
-		endCommit, err := repo.ResolveRevision(ctx, git.Revision(req.GetEndSha()+"^{commit}"))
-		if err != nil {
-			return "", fmt.Errorf("cannot resolve end commit's tree: %w", gitError{
-				// This error is simply for backwards compatibility. We should just
-				// return the plain error eventually.
-				Err:    err,
-				ErrMsg: fmt.Sprintf(ambiguousArgumentFmt, req.GetStartSha(), req.GetEndSha()),
-			})
-		}
-
-		commitDate, err := dateFromProto(req)
-		if err != nil {
-			return "", helper.ErrInvalidArgument(err)
-		}
-
-		// We're now rebasing the end commit on top of the start commit. The resulting tree
-		// is then going to be the tree of the squashed commit.
-		rebasedCommitID, err := s.git2go.Rebase(ctx, repo, git2go.RebaseCommand{
-			Repository: repoPath,
-			Committer: git2go.NewSignature(
-				string(req.GetUser().Name), string(req.GetUser().Email), commitDate,
-			),
-			CommitID:         endCommit,
-			UpstreamCommitID: startCommit,
+	// We need to retrieve the start commit such that we can create the new commit with
+	// all parents of the start commit.
+	startCommit, err := repo.ResolveRevision(ctx, git.Revision(req.GetStartSha()+"^{commit}"))
+	if err != nil {
+		return "", fmt.Errorf("cannot resolve start commit: %w", gitError{
+			// This error is simply for backwards compatibility. We should just
+			// return the plain error eventually.
+			Err:    err,
+			ErrMsg: fmt.Sprintf(ambiguousArgumentFmt, req.GetStartSha(), req.GetEndSha()),
 		})
-		if err != nil {
-			return "", fmt.Errorf("rebasing end onto start commit: %w", gitError{
-				Err:    err,
-				ErrMsg: err.Error(),
-			})
-		}
-
-		treeID, err := repo.ResolveRevision(ctx, rebasedCommitID.Revision()+"^{tree}")
-		if err != nil {
-			return "", fmt.Errorf("cannot resolve rebased tree: %w", err)
-		}
-
-		commitEnv := []string{
-			"GIT_COMMITTER_NAME=" + string(req.GetUser().Name),
-			"GIT_COMMITTER_EMAIL=" + string(req.GetUser().Email),
-			fmt.Sprintf("GIT_COMMITTER_DATE=%d %s", commitDate.Unix(), commitDate.Format("-0700")),
-			"GIT_AUTHOR_NAME=" + string(req.GetAuthor().Name),
-			"GIT_AUTHOR_EMAIL=" + string(req.GetAuthor().Email),
-			fmt.Sprintf("GIT_AUTHOR_DATE=%d %s", commitDate.Unix(), commitDate.Format("-0700")),
-		}
-
-		flags := []git.Option{
-			git.ValueFlag{
-				Name:  "-m",
-				Value: string(req.GetCommitMessage()),
-			},
-			git.ValueFlag{
-				Name:  "-p",
-				Value: startCommit.String(),
-			},
-		}
-
-		var stdout, stderr bytes.Buffer
-		if err := repo.ExecAndWait(ctx, git.SubCmd{
-			Name:  "commit-tree",
-			Flags: flags,
-			Args: []string{
-				treeID.String(),
-			},
-		}, git.WithStdout(&stdout), git.WithStderr(&stderr), git.WithEnv(commitEnv...)); err != nil {
-			return "", fmt.Errorf("creating commit: %w", gitError{
-				Err:    err,
-				ErrMsg: stderr.String(),
-			})
-		}
-
-		return text.ChompBytes(stdout.Bytes()), nil
 	}
 
-	sparseDiffFiles, err := s.diffFiles(ctx, env, repoPath, req)
+	// And we need to take the tree of the end commit. This tree already is the result
+	endCommit, err := repo.ResolveRevision(ctx, git.Revision(req.GetEndSha()+"^{commit}"))
 	if err != nil {
-		return "", fmt.Errorf("define diff files: %w", err)
+		return "", fmt.Errorf("cannot resolve end commit's tree: %w", gitError{
+			// This error is simply for backwards compatibility. We should just
+			// return the plain error eventually.
+			Err:    err,
+			ErrMsg: fmt.Sprintf(ambiguousArgumentFmt, req.GetStartSha(), req.GetEndSha()),
+		})
 	}
 
-	if len(sparseDiffFiles) == 0 {
-		sha, err := s.userSquashWithNoDiff(ctx, req, repoPath, env)
-		if err != nil {
-			return "", fmt.Errorf("without sparse diff: %w", err)
-		}
-
-		return sha, nil
-	}
-
-	sha, err := s.userSquashWithDiffInFiles(ctx, req, repoPath, env, sparseDiffFiles)
+	commitDate, err := dateFromProto(req)
 	if err != nil {
-		return "", fmt.Errorf("with sparse diff: %w", err)
+		return "", helper.ErrInvalidArgument(err)
 	}
-	return sha, nil
-}
 
-func (s *Server) diffFiles(ctx context.Context, env []string, repoPath string, req *gitalypb.UserSquashRequest) ([]byte, error) {
+	// We're now rebasing the end commit on top of the start commit. The resulting tree
+	// is then going to be the tree of the squashed commit.
+	rebasedCommitID, err := s.git2go.Rebase(ctx, repo, git2go.RebaseCommand{
+		Repository: repoPath,
+		Committer: git2go.NewSignature(
+			string(req.GetUser().Name), string(req.GetUser().Email), commitDate,
+		),
+		CommitID:         endCommit,
+		UpstreamCommitID: startCommit,
+	})
+	if err != nil {
+		return "", fmt.Errorf("rebasing end onto start commit: %w", gitError{
+			Err:    err,
+			ErrMsg: err.Error(),
+		})
+	}
+
+	treeID, err := repo.ResolveRevision(ctx, rebasedCommitID.Revision()+"^{tree}")
+	if err != nil {
+		return "", fmt.Errorf("cannot resolve rebased tree: %w", err)
+	}
+
+	commitEnv := []string{
+		"GIT_COMMITTER_NAME=" + string(req.GetUser().Name),
+		"GIT_COMMITTER_EMAIL=" + string(req.GetUser().Email),
+		fmt.Sprintf("GIT_COMMITTER_DATE=%d %s", commitDate.Unix(), commitDate.Format("-0700")),
+		"GIT_AUTHOR_NAME=" + string(req.GetAuthor().Name),
+		"GIT_AUTHOR_EMAIL=" + string(req.GetAuthor().Email),
+		fmt.Sprintf("GIT_AUTHOR_DATE=%d %s", commitDate.Unix(), commitDate.Format("-0700")),
+	}
+
+	flags := []git.Option{
+		git.ValueFlag{
+			Name:  "-m",
+			Value: string(req.GetCommitMessage()),
+		},
+		git.ValueFlag{
+			Name:  "-p",
+			Value: startCommit.String(),
+		},
+	}
+
 	var stdout, stderr bytes.Buffer
-	cmd, err := s.gitCmdFactory.NewWithDir(ctx, repoPath,
-		git.SubCmd{
-			Name:  "diff",
-			Flags: []git.Option{git.Flag{Name: "--name-only"}, git.Flag{Name: "--diff-filter=ar"}, git.Flag{Name: "--binary"}},
-			Args:  []string{diffRange(req)},
+	if err := repo.ExecAndWait(ctx, git.SubCmd{
+		Name:  "commit-tree",
+		Flags: flags,
+		Args: []string{
+			treeID.String(),
 		},
-		git.WithEnv(env...),
-		git.WithStdout(&stdout),
-		git.WithStderr(&stderr),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("create 'git diff': %w", gitError{ErrMsg: stderr.String(), Err: err})
-	}
-
-	if err := cmd.Wait(); err != nil {
-		return nil, fmt.Errorf("on 'git diff' awaiting: %w", gitError{ErrMsg: stderr.String(), Err: err})
-	}
-
-	return stdout.Bytes(), nil
-}
-
-var errNoFilesCheckedOut = errors.New("no files checked out")
-
-func (s *Server) userSquashWithDiffInFiles(ctx context.Context, req *gitalypb.UserSquashRequest, repoPath string, env []string, diffFilesOut []byte) (string, error) {
-	repo := req.GetRepository()
-	worktreePath := newSquashWorktreePath(repoPath, req.GetSquashId())
-
-	if err := s.addWorktree(ctx, repo, worktreePath, ""); err != nil {
-		return "", fmt.Errorf("add worktree: %w", err)
-	}
-
-	defer func(worktreeName string) {
-		ctx, cancel := context.WithCancel(helper.SuppressCancellation(ctx))
-		defer cancel()
-
-		if err := s.removeWorktree(ctx, repo, worktreeName); err != nil {
-			ctxlogrus.Extract(ctx).WithField("worktree_name", worktreeName).WithError(err).Error("failed to remove worktree")
-		}
-	}(filepath.Base(worktreePath))
-
-	worktreeGitPath, err := s.revParseGitDir(ctx, worktreePath)
-	if err != nil {
-		return "", fmt.Errorf("define git dir for worktree: %w", err)
-	}
-
-	if err := s.runCmd(ctx, repo, "config", []git.Option{git.ConfigPair{Key: "core.sparseCheckout", Value: "true"}}, nil); err != nil {
-		return "", fmt.Errorf("on 'git config core.sparseCheckout true': %w", err)
-	}
-
-	if err := s.createSparseCheckoutFile(worktreeGitPath, diffFilesOut); err != nil {
-		return "", fmt.Errorf("create sparse checkout file: %w", err)
-	}
-
-	if err := s.checkout(ctx, repo, worktreePath, req); err != nil {
-		if !errors.Is(err, errNoFilesCheckedOut) {
-			return "", fmt.Errorf("perform 'git checkout' with core.sparseCheckout true: %w", err)
-		}
-
-		// try to perform checkout with disabled sparseCheckout feature
-		if err := s.runCmd(ctx, repo, "config", []git.Option{git.ConfigPair{Key: "core.sparseCheckout", Value: "false"}}, nil); err != nil {
-			return "", fmt.Errorf("on 'git config core.sparseCheckout false': %w", err)
-		}
-
-		if err := s.checkout(ctx, repo, worktreePath, req); err != nil {
-			return "", fmt.Errorf("perform 'git checkout' with core.sparseCheckout false: %w", err)
-		}
-	}
-
-	sha, err := s.applyDiff(ctx, repo, req, worktreePath, env)
-	if err != nil {
-		return "", fmt.Errorf("apply diff: %w", err)
-	}
-
-	return sha, nil
-}
-
-func (s *Server) checkout(ctx context.Context, repo *gitalypb.Repository, worktreePath string, req *gitalypb.UserSquashRequest) error {
-	var stderr bytes.Buffer
-	checkoutCmd, err := s.gitCmdFactory.NewWithDir(ctx, worktreePath,
-		git.SubCmd{
-			Name:  "checkout",
-			Flags: []git.Option{git.Flag{Name: "--detach"}},
-			Args:  []string{req.GetStartSha()},
-		},
-		git.WithStderr(&stderr),
-		git.WithRefTxHook(ctx, repo, s.cfg),
-	)
-	if err != nil {
-		return fmt.Errorf("create 'git checkout': %w", gitError{ErrMsg: stderr.String(), Err: err})
-	}
-
-	if err = checkoutCmd.Wait(); err != nil {
-		if strings.Contains(stderr.String(), "error: Sparse checkout leaves no entry on working directory") {
-			return errNoFilesCheckedOut
-		}
-
-		return fmt.Errorf("wait for 'git checkout': %w", gitError{ErrMsg: stderr.String(), Err: err})
-	}
-
-	return nil
-}
-
-func (s *Server) revParseGitDir(ctx context.Context, worktreePath string) (string, error) {
-	var stdout, stderr bytes.Buffer
-	cmd, err := s.gitCmdFactory.NewWithDir(ctx, worktreePath,
-		git.SubCmd{
-			Name:  "rev-parse",
-			Flags: []git.Option{git.Flag{Name: "--git-dir"}},
-		},
-		git.WithStdout(&stdout),
-		git.WithStderr(&stderr),
-	)
-	if err != nil {
-		return "", fmt.Errorf("creation of 'git rev-parse --git-dir': %w", gitError{ErrMsg: stderr.String(), Err: err})
-	}
-
-	if err := cmd.Wait(); err != nil {
-		return "", fmt.Errorf("wait for 'git rev-parse --git-dir': %w", gitError{ErrMsg: stderr.String(), Err: err})
+	}, git.WithStdout(&stdout), git.WithStderr(&stderr), git.WithEnv(commitEnv...)); err != nil {
+		return "", fmt.Errorf("creating commit: %w", gitError{
+			Err:    err,
+			ErrMsg: stderr.String(),
+		})
 	}
 
 	return text.ChompBytes(stdout.Bytes()), nil
-}
-
-func (s *Server) userSquashWithNoDiff(ctx context.Context, req *gitalypb.UserSquashRequest, repoPath string, env []string) (string, error) {
-	repo := req.GetRepository()
-	worktreePath := newSquashWorktreePath(repoPath, req.GetSquashId())
-
-	if err := s.addWorktree(ctx, repo, worktreePath, req.GetStartSha()); err != nil {
-		return "", fmt.Errorf("add worktree: %w", err)
-	}
-
-	defer func(worktreeName string) {
-		ctx, cancel := context.WithCancel(helper.SuppressCancellation(ctx))
-		defer cancel()
-
-		if err := s.removeWorktree(ctx, repo, worktreeName); err != nil {
-			ctxlogrus.Extract(ctx).WithField("worktree_name", worktreeName).WithError(err).Error("failed to remove worktree")
-		}
-	}(filepath.Base(worktreePath))
-
-	sha, err := s.applyDiff(ctx, repo, req, worktreePath, env)
-	if err != nil {
-		return "", fmt.Errorf("apply diff: %w", err)
-	}
-
-	return sha, nil
 }
 
 func (s *Server) addWorktree(ctx context.Context, repo *gitalypb.Repository, worktreePath string, committish string) error {
@@ -441,125 +256,6 @@ func (s *Server) removeWorktree(ctx context.Context, repo *gitalypb.Repository, 
 	}
 
 	return nil
-}
-
-func (s *Server) applyDiff(ctx context.Context, repo *gitalypb.Repository, req *gitalypb.UserSquashRequest, worktreePath string, env []string) (string, error) {
-	diffRange := diffRange(req)
-
-	var diffStderr bytes.Buffer
-	cmdDiff, err := s.gitCmdFactory.New(ctx, req.GetRepository(),
-		git.SubCmd{
-			Name: "diff",
-			Flags: []git.Option{
-				git.Flag{Name: "--binary"},
-			},
-			Args: []string{diffRange},
-		},
-		git.WithStderr(&diffStderr),
-	)
-	if err != nil {
-		return "", fmt.Errorf("creation of 'git diff' for range %q: %w", diffRange, gitError{ErrMsg: diffStderr.String(), Err: err})
-	}
-
-	var applyStderr bytes.Buffer
-	cmdApply, err := s.gitCmdFactory.NewWithDir(ctx, worktreePath,
-		git.SubCmd{
-			Name: "apply",
-			Flags: []git.Option{
-				git.Flag{Name: "--index"},
-				git.Flag{Name: "--3way"},
-				git.Flag{Name: "--whitespace=nowarn"},
-			},
-		},
-		git.WithEnv(env...),
-		git.WithStdin(command.SetupStdin),
-		git.WithStderr(&applyStderr),
-	)
-	if err != nil {
-		return "", fmt.Errorf("creation of 'git apply' for range %q: %w", diffRange, gitError{ErrMsg: applyStderr.String(), Err: err})
-	}
-
-	if _, err := io.Copy(cmdApply, cmdDiff); err != nil {
-		return "", fmt.Errorf("piping 'git diff' -> 'git apply' for range %q: %w", diffRange, gitError{ErrMsg: applyStderr.String(), Err: err})
-	}
-
-	if err := cmdDiff.Wait(); err != nil {
-		return "", fmt.Errorf("wait for 'git diff' for range %q: %w", diffRange, gitError{ErrMsg: diffStderr.String(), Err: err})
-	}
-
-	if err := cmdApply.Wait(); err != nil {
-		return "", fmt.Errorf("wait for 'git apply' for range %q: %w", diffRange, gitError{ErrMsg: applyStderr.String(), Err: err})
-	}
-
-	commitDate, err := dateFromProto(req)
-	if err != nil {
-		return "", helper.ErrInvalidArgument(err)
-	}
-
-	commitEnv := append(env,
-		"GIT_COMMITTER_NAME="+string(req.GetUser().Name),
-		"GIT_COMMITTER_EMAIL="+string(req.GetUser().Email),
-		fmt.Sprintf("GIT_COMMITTER_DATE=%d %s", commitDate.Unix(), commitDate.Format("-0700")),
-		"GIT_AUTHOR_NAME="+string(req.GetAuthor().Name),
-		"GIT_AUTHOR_EMAIL="+string(req.GetAuthor().Email),
-		fmt.Sprintf("GIT_AUTHOR_DATE=%d %s", commitDate.Unix(), commitDate.Format("-0700")),
-	)
-
-	var commitStderr bytes.Buffer
-	cmdCommit, err := s.gitCmdFactory.NewWithDir(ctx, worktreePath, git.SubCmd{
-		Name: "commit",
-		Flags: []git.Option{
-			git.Flag{Name: "--no-verify"},
-			git.Flag{Name: "--quiet"},
-			git.ValueFlag{Name: "--message", Value: string(req.GetCommitMessage())},
-		},
-	}, git.WithEnv(commitEnv...), git.WithStderr(&commitStderr), git.WithRefTxHook(ctx, repo, s.cfg))
-	if err != nil {
-		return "", fmt.Errorf("creation of 'git commit': %w", gitError{ErrMsg: commitStderr.String(), Err: err})
-	}
-
-	if err := cmdCommit.Wait(); err != nil {
-		return "", fmt.Errorf("wait for 'git commit': %w", gitError{ErrMsg: commitStderr.String(), Err: err})
-	}
-
-	var revParseStdout, revParseStderr bytes.Buffer
-	revParseCmd, err := s.gitCmdFactory.NewWithDir(ctx, worktreePath, git.SubCmd{
-		Name: "rev-parse",
-		Flags: []git.Option{
-			git.Flag{Name: "--quiet"},
-			git.Flag{Name: "--verify"},
-		},
-		Args: []string{"HEAD^{commit}"},
-	}, git.WithEnv(env...), git.WithStdout(&revParseStdout), git.WithStderr(&revParseStderr))
-	if err != nil {
-		return "", fmt.Errorf("creation of 'git rev-parse': %w", gitError{ErrMsg: revParseStderr.String(), Err: err})
-	}
-
-	if err := revParseCmd.Wait(); err != nil {
-		return "", fmt.Errorf("wait for 'git rev-parse': %w", gitError{ErrMsg: revParseStderr.String(), Err: err})
-	}
-
-	return text.ChompBytes(revParseStdout.Bytes()), nil
-}
-
-func (s *Server) createSparseCheckoutFile(worktreeGitPath string, diffFilesOut []byte) error {
-	if err := os.MkdirAll(filepath.Join(worktreeGitPath, "info"), 0755); err != nil {
-		return fmt.Errorf("create 'info' dir for worktree %q: %w", worktreeGitPath, err)
-	}
-
-	if err := ioutil.WriteFile(filepath.Join(worktreeGitPath, "info", "sparse-checkout"), diffFilesOut, 0666); err != nil {
-		return fmt.Errorf("create 'sparse-checkout' file for worktree %q: %w", worktreeGitPath, err)
-	}
-
-	return nil
-}
-
-func diffRange(req *gitalypb.UserSquashRequest) string {
-	return req.GetStartSha() + "..." + req.GetEndSha()
-}
-
-func newSquashWorktreePath(repoPath, squashID string) string {
-	return newWorktreePath(repoPath, squashWorktreePrefix+"-"+squashID+"-")
 }
 
 func newWorktreePath(repoPath, prefix string) string {
