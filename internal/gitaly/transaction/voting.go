448fdf1659be90aeba14b3c0f1ca31cea640f3c6
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-09-01 12:47:04 +0200

    transaction: Implement function to easily vote and commit locked files
    Initially, transactional voting has only been implemented when updating
    Git references. Given that these use the reference-transaction hook, we
    know that we have proper locking semantics because Git will first lock
    the references which are about to be updated, then call the hook in
    "prepared" state, commit locked references to disk and then call the
    hook in "committed" state again. That is, at the point of our first vote
    via this hook, all references are locked and cannot be changed by any
    other concurrent caller.
    
    We have since extended this mechanism to non-reference files, like
    gitattributes or gitconfig. In the current implementation, we do not
    provide the same safeguards though as described above: there is no
    locking, no checking for concurrent writes and no guaranteed atomicity.
    
    This is about to change via the new LockingFileWriter interface. For any
    given file update, we now want to do the following sequence in order to
    commit file changes:
    
        1. Create a new LockingFileWriter for the target file, optionally
           containing the same content as the current target file.
    
        2. Update the temporary file to contain the desired changes.
    
        3. Lock the file such that no concurrent changes are possible to the
           target file anymore. At this point in time, we also verify that
           the target file hasn't been updated while we were updating our
           copy of it. This step is required to make sure that there was no
           race with another process.
    
        4. Vote on the updated file contents. This is required to assert
           that the Gitaly cluster performed the same change on each node.
    
        5. Commit the file by moving it into place. The change is now
           visible to other clients and the lock has been released.
    
        6. Do another vote to demonstrate that the modification has been
           performed.
    
    This sequence is rather involved, and it is easy to mix up any of the
    steps. Implement a new function `transaction.CommitLockedFile()` which
    encapsulates the logic such that we only need a single implementation of
    it.

--- internal/gitaly/transaction/voting.go
+++ internal/gitaly/transaction/voting.go
@@ -3,7 +3,11 @@ package transaction
 import (
 	"context"
 	"errors"
+	"fmt"
+	"io"
+	"os"
 
+	"gitlab.com/gitlab-org/gitaly/v14/internal/safe"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/transaction/txinfo"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/transaction/voting"
 )
@@ -25,4 +29,49 @@ func VoteOnContext(ctx context.Context, m Manager, vote voting.Vote) error {
 	return RunOnContext(ctx, func(transaction txinfo.Transaction) error {
 		return m.Vote(ctx, transaction, vote)
 	})
+}
+
+// CommitLockedFile will lock, vote and commit the LockingFileWriter in a race-free manner.
+func CommitLockedFile(ctx context.Context, m Manager, writer *safe.LockingFileWriter) (returnedErr error) {
+	if err := writer.Lock(); err != nil {
+		return fmt.Errorf("locking file: %w", err)
+	}
+
+	var vote voting.Vote
+	if err := RunOnContext(ctx, func(tx txinfo.Transaction) error {
+		hasher := voting.NewVoteHash()
+
+		lockedFile, err := os.Open(writer.Path())
+		if err != nil {
+			return fmt.Errorf("opening locked file: %w", err)
+		}
+		defer lockedFile.Close()
+
+		if _, err := io.Copy(hasher, lockedFile); err != nil {
+			return fmt.Errorf("hashing locked file: %w", err)
+		}
+
+		vote, err = hasher.Vote()
+		if err != nil {
+			return fmt.Errorf("computing vote for locked file: %w", err)
+		}
+
+		if err := m.Vote(ctx, tx, vote); err != nil {
+			return fmt.Errorf("preimage vote: %w", err)
+		}
+
+		return nil
+	}); err != nil {
+		return fmt.Errorf("voting on locked file: %w", err)
+	}
+
+	if err := writer.Commit(); err != nil {
+		return fmt.Errorf("committing file: %w", err)
+	}
+
+	if err := VoteOnContext(ctx, m, vote); err != nil {
+		return fmt.Errorf("postimage vote: %w", err)
+	}
+
+	return nil
 }
--- internal/gitaly/transaction/voting_test.go
+++ internal/gitaly/transaction/voting_test.go
@@ -3,10 +3,13 @@ package transaction
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/backchannel"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/safe"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/transaction/txinfo"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/transaction/voting"
@@ -111,5 +114,97 @@ func TestVoteOnContext(t *testing.T) {
 				return expectedErr
 			},
 		}, vote))
+	})
+}
+
+func TestCommitLockedFile(t *testing.T) {
+	ctx, cancel := testhelper.Context()
+	defer cancel()
+
+	backchannelPeer := &peer.Peer{
+		AuthInfo: backchannel.WithID(nil, 1234),
+	}
+
+	t.Run("without transaction", func(t *testing.T) {
+		file := filepath.Join(testhelper.TempDir(t), "file")
+
+		writer, err := safe.NewLockingFileWriter(file)
+		require.NoError(t, err)
+		_, err = writer.Write([]byte("contents"))
+		require.NoError(t, err)
+
+		require.NoError(t, CommitLockedFile(ctx, &MockManager{}, writer))
+		require.Equal(t, []byte("contents"), testhelper.MustReadFile(t, file))
+	})
+
+	ctx, err := txinfo.InjectTransaction(ctx, 5678, "node", true)
+	require.NoError(t, err)
+	ctx = peer.NewContext(ctx, backchannelPeer)
+
+	t.Run("successful transaction", func(t *testing.T) {
+		file := filepath.Join(testhelper.TempDir(t), "file")
+
+		writer, err := safe.NewLockingFileWriter(file)
+		require.NoError(t, err)
+		_, err = writer.Write([]byte("contents"))
+		require.NoError(t, err)
+
+		calls := 0
+		require.NoError(t, CommitLockedFile(ctx, &MockManager{
+			VoteFn: func(ctx context.Context, tx txinfo.Transaction, vote voting.Vote) error {
+				require.Equal(t, txinfo.Transaction{
+					ID:            5678,
+					Node:          "node",
+					Primary:       true,
+					BackchannelID: 1234,
+				}, tx)
+				require.Equal(t, voting.VoteFromData([]byte("contents")), vote)
+				calls++
+				return nil
+			},
+		}, writer))
+		require.Equal(t, 2, calls, "expected two votes")
+
+		require.Equal(t, []byte("contents"), testhelper.MustReadFile(t, file))
+	})
+
+	t.Run("failing transaction", func(t *testing.T) {
+		file := filepath.Join(testhelper.TempDir(t), "file")
+
+		writer, err := safe.NewLockingFileWriter(file)
+		require.NoError(t, err)
+		_, err = writer.Write([]byte("contents"))
+		require.NoError(t, err)
+
+		err = CommitLockedFile(ctx, &MockManager{
+			VoteFn: func(context.Context, txinfo.Transaction, voting.Vote) error {
+				return fmt.Errorf("some error")
+			},
+		}, writer)
+		require.EqualError(t, err, "voting on locked file: preimage vote: some error")
+
+		require.NoFileExists(t, file)
+	})
+
+	t.Run("concurrent modification", func(t *testing.T) {
+		file := filepath.Join(testhelper.TempDir(t), "file")
+
+		writer, err := safe.NewLockingFileWriter(file)
+		require.NoError(t, err)
+		_, err = writer.Write([]byte("contents"))
+		require.NoError(t, err)
+
+		err = CommitLockedFile(ctx, &MockManager{
+			VoteFn: func(context.Context, txinfo.Transaction, voting.Vote) error {
+				// This shouldn't typically happen given that the file is locked,
+				// but we concurrently update the file after our first vote.
+				require.NoError(t, ioutil.WriteFile(file, []byte("something"),
+					0o666))
+				return nil
+			},
+		}, writer)
+		require.EqualError(t, err, "committing file: file concurrently created")
+
+		require.Equal(t, []byte("something"), testhelper.MustReadFile(t, file))
 	})
 }
