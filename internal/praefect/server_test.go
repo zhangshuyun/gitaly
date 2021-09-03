a889114230771cf457394b3f154eed674aace4b7
Sami Hiltunen <shiltunen@gitlab.com>
2021-09-03 12:09:42 +0200

    Workaround Rails filesystem ID tests in Praefect
    Rails tests configure Praefect in front of the tests that exercise
    the Rugged direct git access code. As Praefect is now deriving the
    filesystem IDs from the names of the virtual storages, the filesystem
    id checks fail and thus the test fail. This is not a problem in practice
    as one wouldn't use rugged in a real-world setup with Praefect. This
    commit worksaround the tests by returning the filesystem ID from the
    Gitaly node if a virtual storage has only one Gitaly node configured.
    This matches the setup the tests use and thus pass them. The workaround
    and the filesystem ID code can be removed in 15.0 once the rugged patches
    and NFS support are dropped.

--- internal/praefect/server_test.go
+++ internal/praefect/server_test.go
@@ -8,6 +8,7 @@ import (
 	"net"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 	"sync"
 	"testing"
@@ -36,6 +37,7 @@ import (
 	serversvc "gitlab.com/gitlab-org/gitaly/internal/praefect/service/server"
 	"gitlab.com/gitlab-org/gitaly/internal/praefect/service/transaction"
 	"gitlab.com/gitlab-org/gitaly/internal/praefect/transactions"
+	"gitlab.com/gitlab-org/gitaly/internal/storage"
 	"gitlab.com/gitlab-org/gitaly/internal/testhelper"
 	"gitlab.com/gitlab-org/gitaly/internal/testhelper/promtest"
 	"gitlab.com/gitlab-org/gitaly/internal/testhelper/testcfg"
@@ -116,8 +118,21 @@ func TestGitalyServerInfo(t *testing.T) {
 		secondCfg := testcfg.Build(t, testcfg.WithStorages("praefect-internal-2"))
 		secondCfg.SocketPath = testserver.RunGitalyServer(t, secondCfg, nil, setup.RegisterAll, testserver.WithDisablePraefect())
 
+		require.NoError(t, storage.WriteMetadataFile(firstCfg.Storages[0].Path))
+		firstMetadata, err := storage.ReadMetadataFile(firstCfg.Storages[0].Path)
+		require.NoError(t, err)
+
 		conf := config.Config{
 			VirtualStorages: []*config.VirtualStorage{
+				{
+					Name: "passthrough-filesystem-id",
+					Nodes: []*config.Node{
+						{
+							Storage: firstCfg.Storages[0].Name,
+							Address: firstCfg.SocketPath,
+						},
+					},
+				},
 				{
 					Name: "virtual-storage",
 					Nodes: []*config.Node{
@@ -152,7 +167,14 @@ func TestGitalyServerInfo(t *testing.T) {
 			StorageStatuses: []*gitalypb.ServerInfoResponse_StorageStatus{
 				{
 					StorageName:       conf.VirtualStorages[0].Name,
-					FilesystemId:      serversvc.DeriveFilesystemID(conf.VirtualStorages[0].Name).String(),
+					FilesystemId:      firstMetadata.GitalyFilesystemID,
+					Readable:          true,
+					Writeable:         true,
+					ReplicationFactor: 1,
+				},
+				{
+					StorageName:       conf.VirtualStorages[1].Name,
+					FilesystemId:      serversvc.DeriveFilesystemID(conf.VirtualStorages[1].Name).String(),
 					Readable:          true,
 					Writeable:         true,
 					ReplicationFactor: 2,
@@ -166,6 +188,12 @@ func TestGitalyServerInfo(t *testing.T) {
 		for _, ss := range actual.StorageStatuses {
 			ss.FsType = ""
 		}
+
+		// sort the storages by name so they match the expected
+		sort.Slice(actual.StorageStatuses, func(i, j int) bool {
+			return actual.StorageStatuses[i].StorageName < actual.StorageStatuses[j].StorageName
+		})
+
 		require.True(t, proto.Equal(expected, actual), "expected: %v, got: %v", expected, actual)
 	})
 
