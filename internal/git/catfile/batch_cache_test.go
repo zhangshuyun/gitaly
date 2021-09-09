9f78103f55967c6e11464b8661abf144a8f46d32
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-09-09 13:21:19 +0200

    catfile: Refactor batch cache tests to use real structs
    The `BatchCache` tests are currently working with sentinel values which
    are added to and retrieved from the cache, which is simply an empty
    `batch` struct. While this is easy to use for tests, it doesn't reflect
    real-world use and furthermore keeps us from extending functionality of
    the `batch` struct because we always have an uninitialized struct, only.
    
    Fix this by creating a new `mustCreateBatch()` helper function which
    creates real `batch` structs for us via `newBatch()`.
    
    This also uncovers a bug in our `addTwice` test, where we used to
    compare values retrieved from the cache, but expected the wrong value to
    be returned: because all values were empty, comparing them with each
    other for equality will always return `true`. This test bug is fixed by
    checking for the correct value.

--- internal/git/catfile/batch_cache_test.go
+++ internal/git/catfile/batch_cache_test.go
@@ -1,70 +1,80 @@
 package catfile
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/git/repository"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/gitaly/config"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper/testcfg"
 )
 
-func TestCacheAdd(t *testing.T) {
+func TestCache_add(t *testing.T) {
 	const maxLen = 3
 	bc := newCache(time.Hour, maxLen, defaultEvictionInterval)
 
-	key0 := testKey(0)
-	value0 := testValue()
+	cfg, repo, _ := testcfg.BuildWithRepo(t)
+
+	key0 := newCacheKey("0", repo)
+	value0 := mustCreateBatch(t, cfg, bc, repo)
 	bc.add(key0, value0)
 	requireCacheValid(t, bc)
 
-	key1 := testKey(1)
-	bc.add(key1, testValue())
+	key1 := newCacheKey("1", repo)
+	bc.add(key1, mustCreateBatch(t, cfg, bc, repo))
 	requireCacheValid(t, bc)
 
-	key2 := testKey(2)
-	bc.add(key2, testValue())
+	key2 := newCacheKey("2", repo)
+	bc.add(key2, mustCreateBatch(t, cfg, bc, repo))
 	requireCacheValid(t, bc)
 
 	// Because maxLen is 3, and key0 is oldest, we expect that adding key3
 	// will kick out key0.
-	key3 := testKey(3)
-	bc.add(key3, testValue())
+	key3 := newCacheKey("3", repo)
+	bc.add(key3, mustCreateBatch(t, cfg, bc, repo))
 	requireCacheValid(t, bc)
 
 	require.Equal(t, maxLen, bc.len(), "length should be maxLen")
 	require.True(t, value0.isClosed(), "value0 should be closed")
-	require.Equal(t, []key{key1, key2, key3}, keys(bc))
+	require.Equal(t, []key{key1, key2, key3}, keys(t, bc))
 }
 
-func TestCacheAddTwice(t *testing.T) {
+func TestCache_addTwice(t *testing.T) {
 	bc := newCache(time.Hour, 10, defaultEvictionInterval)
 
-	key0 := testKey(0)
-	value0 := testValue()
+	cfg, repo, _ := testcfg.BuildWithRepo(t)
+
+	key0 := newCacheKey("0", repo)
+	value0 := mustCreateBatch(t, cfg, bc, repo)
 	bc.add(key0, value0)
 	requireCacheValid(t, bc)
 
-	key1 := testKey(1)
-	bc.add(key1, testValue())
+	key1 := newCacheKey("1", repo)
+	value1 := mustCreateBatch(t, cfg, bc, repo)
+	bc.add(key1, value1)
 	requireCacheValid(t, bc)
 
 	require.Equal(t, key0, bc.head().key, "key0 should be oldest key")
 
-	value2 := testValue()
+	value2 := mustCreateBatch(t, cfg, bc, repo)
 	bc.add(key0, value2)
 	requireCacheValid(t, bc)
 
 	require.Equal(t, key1, bc.head().key, "key1 should be oldest key")
-	require.Equal(t, value2, bc.head().value)
+	require.Equal(t, value1, bc.head().value)
 
 	require.True(t, value0.isClosed(), "value0 should be closed")
 }
 
-func TestCacheCheckout(t *testing.T) {
+func TestCache_checkout(t *testing.T) {
 	bc := newCache(time.Hour, 10, defaultEvictionInterval)
 
-	key0 := testKey(0)
-	value0 := testValue()
+	cfg, repo, _ := testcfg.BuildWithRepo(t)
+
+	key0 := newCacheKey("0", repo)
+	value0 := mustCreateBatch(t, cfg, bc, repo)
 	bc.add(key0, value0)
 
 	v, ok := bc.checkout(key{sessionID: "foo"})
@@ -85,31 +95,33 @@ func TestCacheCheckout(t *testing.T) {
 	require.Nil(t, v, "value from second checkout")
 }
 
-func TestCacheEnforceTTL(t *testing.T) {
+func TestCache_enforceTTL(t *testing.T) {
 	ttl := time.Hour
 	bc := newCache(ttl, 10, defaultEvictionInterval)
 
+	cfg, repo, _ := testcfg.BuildWithRepo(t)
+
 	sleep := func() { time.Sleep(2 * time.Millisecond) }
 
-	key0 := testKey(0)
-	value0 := testValue()
+	key0 := newCacheKey("0", repo)
+	value0 := mustCreateBatch(t, cfg, bc, repo)
 	bc.add(key0, value0)
 	sleep()
 
-	key1 := testKey(1)
-	value1 := testValue()
+	key1 := newCacheKey("1", repo)
+	value1 := mustCreateBatch(t, cfg, bc, repo)
 	bc.add(key1, value1)
 	sleep()
 
 	cutoff := time.Now().Add(ttl)
 	sleep()
 
-	key2 := testKey(2)
-	bc.add(key2, testValue())
+	key2 := newCacheKey("2", repo)
+	bc.add(key2, mustCreateBatch(t, cfg, bc, repo))
 	sleep()
 
-	key3 := testKey(3)
-	bc.add(key3, testValue())
+	key3 := newCacheKey("3", repo)
+	bc.add(key3, mustCreateBatch(t, cfg, bc, repo))
 	sleep()
 
 	requireCacheValid(t, bc)
@@ -123,37 +135,39 @@ func TestCacheEnforceTTL(t *testing.T) {
 		require.True(t, v.isClosed(), "value %d %v should be closed", i, v)
 	}
 
-	require.Equal(t, []key{key2, key3}, keys(bc), "remaining keys after EnforceTTL")
+	require.Equal(t, []key{key2, key3}, keys(t, bc), "remaining keys after EnforceTTL")
 
 	bc.enforceTTL(cutoff)
 
 	requireCacheValid(t, bc)
-	require.Equal(t, []key{key2, key3}, keys(bc), "remaining keys after second EnforceTTL")
+	require.Equal(t, []key{key2, key3}, keys(t, bc), "remaining keys after second EnforceTTL")
 }
 
-func TestAutoExpiry(t *testing.T) {
+func TestCache_autoExpiry(t *testing.T) {
 	ttl := 5 * time.Millisecond
 	refresh := 1 * time.Millisecond
 	bc := newCache(ttl, 10, refresh)
 
-	key0 := testKey(0)
-	value0 := testValue()
+	cfg, repo, _ := testcfg.BuildWithRepo(t)
+
+	key0 := newCacheKey("0", repo)
+	value0 := mustCreateBatch(t, cfg, bc, repo)
 	bc.add(key0, value0)
 	requireCacheValid(t, bc)
 
-	require.Contains(t, keys(bc), key0, "key should still be in map")
+	require.Contains(t, keys(t, bc), key0, "key should still be in map")
 	require.False(t, value0.isClosed(), "value should not have been closed")
 
 	// Wait for the monitor goroutine to do its thing
 	for i := 0; i < 100; i++ {
-		if len(keys(bc)) == 0 {
+		if len(keys(t, bc)) == 0 {
 			break
 		}
 
 		time.Sleep(refresh)
 	}
 
-	require.Empty(t, keys(bc), "key should no longer be in map")
+	require.Empty(t, keys(t, bc), "key should no longer be in map")
 	require.True(t, value0.isClosed(), "value should be closed after eviction")
 }
 
@@ -167,11 +181,21 @@ func requireCacheValid(t *testing.T, bc *BatchCache) {
 	}
 }
 
-func testValue() *batch { return &batch{} }
+func mustCreateBatch(t *testing.T, cfg config.Cfg, bc *BatchCache, repo repository.GitRepo) *batch {
+	t.Helper()
 
-func testKey(i int) key { return key{sessionID: fmt.Sprintf("key-%d", i)} }
+	ctx, cancel := testhelper.Context()
+	t.Cleanup(cancel)
 
-func keys(bc *BatchCache) []key {
+	batch, _, err := bc.newBatch(ctx, newRepoExecutor(t, cfg, repo))
+	require.NoError(t, err)
+
+	return batch
+}
+
+func keys(t *testing.T, bc *BatchCache) []key {
+	t.Helper()
+
 	bc.entriesMutex.Lock()
 	defer bc.entriesMutex.Unlock()
 
