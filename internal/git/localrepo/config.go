e94977d20da5eb5a77bfc0e5e51918c17ed3483d
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-09-15 13:43:34 +0200

    localrepo: Provide function to remove config entries with transactions
    This commit implements removal of config entries from the gitconfig with
    proper transactional semantics, inclduing locking and voting on the
    change. This is in the same spirit as the preceding addition of
    `SetConfig()` and will be used to make callers properly aware of
    transactions.

--- internal/git/localrepo/config.go
+++ internal/git/localrepo/config.go
@@ -12,6 +12,7 @@ import (
 	"gitlab.com/gitlab-org/gitaly/v14/internal/command"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/git"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/gitaly/transaction"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/helper/text"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/safe"
 )
 
@@ -250,6 +251,93 @@ func (repo *Repo) SetConfig(ctx context.Context, key, value string, txManager tr
 		case isExitWithCode(err, 2):
 			// no section or name was provided
 			return fmt.Errorf("%w: missing section or name", git.ErrInvalidArg)
+		}
+	}
+
+	if err := transaction.CommitLockedFile(ctx, txManager, writer); err != nil {
+		return fmt.Errorf("committing config: %w", err)
+	}
+
+	return nil
+}
+
+// UnsetMatchingConfig removes all config entries whose key match the given regular expression. If
+// no matching keys are found, then this function returns an `git.ErrNotFound` error. The change
+// will use transactional semantics.
+func (repo *Repo) UnsetMatchingConfig(
+	ctx context.Context,
+	regex string,
+	txManager transaction.Manager,
+) (returnedErr error) {
+	// An empty regular expression would match every key and is thus quite dangerous.
+	if err := validateNotBlank(regex, "regex"); err != nil {
+		return err
+	}
+
+	repoPath, err := repo.Path()
+	if err != nil {
+		return fmt.Errorf("getting repo path: %w", err)
+	}
+
+	writer, err := safe.NewLockingFileWriter(filepath.Join(repoPath, "config"), safe.LockingFileWriterConfig{
+		SeedContents: true,
+	})
+	if err != nil {
+		return fmt.Errorf("creating file write: %w", err)
+	}
+	defer func() {
+		if err := writer.Close(); err != nil && returnedErr == nil {
+			returnedErr = fmt.Errorf("closing config writer: %w", err)
+		}
+	}()
+
+	// There is no way to directly unset all keys matching a given regular expression, so we
+	// need to go the indirect route and first discover all matching keys via `--get-regex`.
+	var stdout bytes.Buffer
+	if err := repo.ExecAndWait(ctx, git.SubCmd{
+		Name: "config",
+		Flags: []git.Option{
+			git.Flag{Name: "--name-only"},
+			git.Flag{Name: "--get-regex"},
+			git.ValueFlag{Name: "--file", Value: writer.Path()},
+		},
+		Args: []string{regex},
+	}, git.WithStdout(&stdout)); err != nil {
+		switch {
+		case isExitWithCode(err, 1):
+			return fmt.Errorf("%w: no matching keys", git.ErrNotFound)
+		case isExitWithCode(err, 6):
+			// no section or name was provided
+			return fmt.Errorf("%w: invalid regular expression", git.ErrInvalidArg)
+		}
+		return fmt.Errorf("getting matching keys: %w", err)
+	}
+
+	keys := strings.Split(text.ChompBytes(stdout.Bytes()), "\n")
+	if len(keys) == 0 {
+		return fmt.Errorf("%w: no matching keys", git.ErrNotFound)
+	}
+
+	keySeen := map[string]bool{}
+	for _, key := range keys {
+		// We're using `--unset-all`, which will remove each occurrence of the given key
+		// even if it's a multi-valued config. We thus have to de-duplicate keys or
+		// otherwise we'd try to unset the same key multiple times even though we already
+		// removed all entries the first time.
+		if keySeen[key] {
+			continue
+		}
+		keySeen[key] = true
+
+		if err := repo.ExecAndWait(ctx, git.SubCmd{
+			Name: "config",
+			Flags: []git.Option{
+				git.Flag{Name: "--unset-all"},
+				git.ValueFlag{Name: "--file", Value: writer.Path()},
+			},
+			Args: []string{key},
+		}); err != nil {
+			return fmt.Errorf("unsetting key %q: %w", key, err)
 		}
 	}
 
--- internal/git/localrepo/config_test.go
+++ internal/git/localrepo/config_test.go
@@ -429,3 +429,134 @@ func TestRepo_SetConfig(t *testing.T) {
 		require.Equal(t, 2, votes)
 	})
 }
+
+func TestRepo_UnsetMatchingConfig(t *testing.T) {
+	ctx, cancel := testhelper.Context()
+	defer cancel()
+
+	cfg := testcfg.Build(t)
+
+	standardKeys := []string{
+		"core.repositoryformatversion",
+		"core.filemode",
+		"core.bare",
+	}
+
+	for _, tc := range []struct {
+		desc         string
+		addEntries   map[string]string
+		regex        string
+		locked       bool
+		expectedErr  error
+		expectedKeys []string
+	}{
+		{
+			desc:         "empty regex is refused",
+			regex:        "",
+			expectedErr:  fmt.Errorf("%w: \"regex\" is blank or empty", git.ErrInvalidArg),
+			expectedKeys: standardKeys,
+		},
+		{
+			desc: "simple match",
+			addEntries: map[string]string{
+				"foo.bar": "value1",
+				"foo.qux": "value2",
+			},
+			regex:        "foo.bar",
+			expectedKeys: append(standardKeys, "foo.qux"),
+		},
+		{
+			desc: "multiple matches",
+			addEntries: map[string]string{
+				"foo.bar": "value1",
+				"foo.qux": "value2",
+			},
+			regex:        "foo.",
+			expectedKeys: standardKeys,
+		},
+		{
+			desc: "unanchored",
+			addEntries: map[string]string{
+				"foo.matchme": "value1",
+				"foo.qux":     "value2",
+			},
+			regex:        "matchme",
+			expectedKeys: append(standardKeys, "foo.qux"),
+		},
+		{
+			desc: "anchored",
+			addEntries: map[string]string{
+				"foo.matchme": "value1",
+				"matchme.foo": "value2",
+			},
+			regex:        "^matchme",
+			expectedKeys: append(standardKeys, "foo.matchme"),
+		},
+		{
+			desc:         "no matches",
+			regex:        "idontmatch",
+			expectedErr:  fmt.Errorf("%w: no matching keys", git.ErrNotFound),
+			expectedKeys: standardKeys,
+		},
+		{
+			desc:         "invalid regex",
+			regex:        "?",
+			expectedErr:  fmt.Errorf("%w: invalid regular expression", git.ErrInvalidArg),
+			expectedKeys: standardKeys,
+		},
+		{
+			desc:         "locked",
+			regex:        ".*",
+			locked:       true,
+			expectedErr:  fmt.Errorf("committing config: %w", fmt.Errorf("locking file: %w", errors.New("file already locked"))),
+			expectedKeys: standardKeys,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			repoProto, repoPath := gittest.InitRepo(t, cfg, cfg.Storages[0])
+			repo := NewTestRepo(t, cfg, repoProto)
+
+			for key, value := range tc.addEntries {
+				gittest.Exec(t, cfg, "-C", repoPath, "config", "--add", key, value)
+			}
+
+			if tc.locked {
+				writer, err := safe.NewLockingFileWriter(filepath.Join(repoPath, "config"))
+				require.NoError(t, err)
+				defer func() { require.NoError(t, writer.Close()) }()
+				require.NoError(t, writer.Lock())
+			}
+
+			require.Equal(t, tc.expectedErr, repo.UnsetMatchingConfig(ctx, tc.regex, &transaction.MockManager{}))
+
+			output := gittest.Exec(t, cfg, "-C", repoPath, "config", "--list", "--name-only", "--local")
+			require.Equal(t, tc.expectedKeys, strings.Split(text.ChompBytes(output), "\n"))
+		})
+	}
+
+	t.Run("transactional", func(t *testing.T) {
+		repoProto, repoPath := gittest.InitRepo(t, cfg, cfg.Storages[0])
+		repo := NewTestRepo(t, cfg, repoProto)
+
+		gittest.Exec(t, cfg, "-C", repoPath, "config", "--add", "some.key", "value")
+
+		backchannelPeer := &peer.Peer{
+			AuthInfo: backchannel.WithID(nil, 1234),
+		}
+
+		ctx, err := txinfo.InjectTransaction(ctx, 1, "node", true)
+		ctx = peer.NewContext(ctx, backchannelPeer)
+
+		votes := 0
+
+		require.NoError(t, err)
+		require.NoError(t, repo.UnsetMatchingConfig(ctx, "some.key", &transaction.MockManager{
+			VoteFn: func(context.Context, txinfo.Transaction, voting.Vote) error {
+				votes++
+				return nil
+			},
+		}))
+
+		require.Equal(t, 2, votes)
+	})
+}
