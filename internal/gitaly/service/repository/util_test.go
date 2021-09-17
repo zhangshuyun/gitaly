567160d48fbb6a3a4d41b033211f5cff5a8190be
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-09-17 09:32:24 +0200

    repository: Implement logic to create repos with atomic semantics
    While our RPCs which create repositories all do mostly the same thing
    except for how the repository is seeded, their behaviour is wildly
    different when it comes to how preexisting repositories are handled.
    E.g. `CreateRepository()` is happy with the repository already existing,
    `CreateRepositoryFromBundle()` is happy with the target path to exist as
    long as it is an empty directory, and the others disallow the target
    path existing altogether. This behaviour is confusing, inconsistent and
    has likely grown organically over time.
    
    The issue with this divergent behaviour is that we cannot really
    guarantee atomic transactional semantics for the former two RPC calls:
    especially in `CreateRepository()`, it is impossible to guarantee that
    we either do no changes at all if the call will fail, or to do only
    specific changes. This keeps us from implementing proper two-phase
    voting in Gitaly Cluster given that we cannot roll back changes, and
    neither can we meaningfully lock the repository for any additional
    changes.
    
    To fix this, we must assert that the target repository path does not
    exist at the time of creation and use proper locking at repository
    creation time. This ensures that we can assert an exact state of each
    repository on which we want to vote, it ensures that no other RPC calls
    modify the repository (it does not exist and cannot be created
    concurrently given it is locked), and it ensures that we can roll back
    changes in case an error happens by using a temporary repo into which
    all changes will first be written.
    
    Implement a new helper function `createRepository()` which handles all
    this logic for us:
    
        1. It asserts that the target path does not exist.
    
        2. It creates a temporary repository.
    
        3. This temporary repository is getting seeded by the caller via a
           provided callback function.
    
        4. We compute the vote, which is the complete contents of the
           repository. This will guarantee that all nodes are about to
           perform the same change.
    
        5. The target repository path is locked now, where we assert after
           the lock has been taken that no other concurrent RPC call has
           created it meanwhile. This ensures that no two concurrent calls
           can ever touch this repository and thus it cannot be modified by
           anything else.
    
        6. We vote on the state computed in step 4.
    
        7. If the vote was successful, we know that other nodes did end up
           with the same result. We thus rename the temporary directory into
           place.
    
        8. We do a finalizing vote to assert that we have performed the
           change.
    
    This mechanism is thus race-free and can be reused across the different
    RPCs. While it is a breaking change that will first require changes in
    Ruby, there really is no other way to provide atomicity in the context
    of Gitaly Cluster.

---
+++ internal/gitaly/service/repository/util_test.go
@@ -0,0 +1,264 @@
+package repository
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/git"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/git/gittest"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/gitaly/config"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/gitaly/transaction"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/helper"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/helper/text"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/safe"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper/testcfg"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/transaction/txinfo"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/transaction/voting"
+	"gitlab.com/gitlab-org/gitaly/v14/proto/go/gitalypb"
+	"google.golang.org/grpc/peer"
+)
+
+func TestCreateRepository(t *testing.T) {
+	t.Parallel()
+
+	ctx, cancel := testhelper.Context()
+	defer cancel()
+
+	cfg := testcfg.Build(t)
+
+	txManager := &transaction.MockManager{}
+	locator := config.NewLocator(cfg)
+	gitCmdFactory := git.NewExecCommandFactory(cfg)
+
+	server := &server{
+		cfg:           cfg,
+		locator:       locator,
+		txManager:     txManager,
+		gitCmdFactory: gitCmdFactory,
+	}
+
+	votes := 0
+
+	for _, tc := range []struct {
+		desc   string
+		setup  func(t *testing.T, repo *gitalypb.Repository, repoPath string)
+		seed   func(t *testing.T, repo *gitalypb.Repository, repoPath string) error
+		verify func(
+			t *testing.T,
+			tempRepo *gitalypb.Repository,
+			tempRepoPath string,
+			realRepo *gitalypb.Repository,
+			realRepoPath string,
+		)
+		transactional bool
+		expectedErr   error
+	}{
+		{
+			desc: "no seeding",
+			verify: func(t *testing.T, tempRepo *gitalypb.Repository, tempRepoPath string, realRepo *gitalypb.Repository, realRepoPath string) {
+				// Assert that the temporary repository does not exist anymore.
+				require.NoDirExists(t, tempRepoPath)
+
+				// But the new repository must exist.
+				isBareRepo := gittest.Exec(t, cfg, "-C", realRepoPath, "rev-parse", "--is-bare-repository")
+				require.Equal(t, "true", text.ChompBytes(isBareRepo))
+			},
+		},
+		{
+			desc: "seeding",
+			seed: func(t *testing.T, repo *gitalypb.Repository, _ string) error {
+				// We're using the command factory on purpose here to assert that we
+				// can execute regular Git commands on the temporary repository.
+				cmd, err := gitCmdFactory.New(ctx, repo, git.SubCmd{
+					Name: "config",
+					Args: []string{"custom.key", "value"},
+				})
+				require.NoError(t, err)
+				require.NoError(t, cmd.Wait())
+				return nil
+			},
+			verify: func(t *testing.T, tempRepo *gitalypb.Repository, tempRepoPath string, realRepo *gitalypb.Repository, realRepoPath string) {
+				value := gittest.Exec(t, cfg, "-C", realRepoPath, "config", "custom.key")
+				require.Equal(t, "value", text.ChompBytes(value))
+			},
+		},
+		{
+			desc: "error while seeding",
+			seed: func(t *testing.T, repo *gitalypb.Repository, _ string) error {
+				return errors.New("some error")
+			},
+			verify: func(t *testing.T, tempRepo *gitalypb.Repository, tempRepoPath string, realRepo *gitalypb.Repository, realRepoPath string) {
+				require.NoDirExists(t, realRepoPath)
+				require.NoDirExists(t, tempRepoPath)
+			},
+			expectedErr: errors.New("some error"),
+		},
+		{
+			desc: "preexisting directory",
+			setup: func(t *testing.T, repo *gitalypb.Repository, repoPath string) {
+				require.NoError(t, os.MkdirAll(repoPath, 0o777))
+			},
+			verify: func(t *testing.T, tempRepo *gitalypb.Repository, tempRepoPath string, realRepo *gitalypb.Repository, realRepoPath string) {
+				require.NoDirExists(t, tempRepoPath)
+
+				require.DirExists(t, realRepoPath)
+				dirEntries, err := os.ReadDir(realRepoPath)
+				require.NoError(t, err)
+				require.Empty(t, dirEntries, "directory should not have been modified")
+			},
+			expectedErr: helper.ErrAlreadyExistsf("repository exists already"),
+		},
+		{
+			desc: "locked",
+			setup: func(t *testing.T, repo *gitalypb.Repository, repoPath string) {
+				require.NoError(t, os.MkdirAll(filepath.Dir(repoPath), 0o777))
+
+				// Lock the target repository such that we must fail.
+				lock, err := os.Create(repoPath + ".lock")
+				require.NoError(t, err)
+				require.NoError(t, lock.Close())
+			},
+			verify: func(t *testing.T, tempRepo *gitalypb.Repository, tempRepoPath string, realRepo *gitalypb.Repository, realRepoPath string) {
+				require.NoDirExists(t, tempRepoPath)
+				require.NoDirExists(t, realRepoPath)
+				require.FileExists(t, realRepoPath+".lock")
+			},
+			expectedErr: fmt.Errorf("locking repository: %w", safe.ErrFileAlreadyLocked),
+		},
+		{
+			desc:          "successful transaction",
+			transactional: true,
+			setup: func(t *testing.T, repo *gitalypb.Repository, repoPath string) {
+				votes = 0
+				txManager.VoteFn = func(context.Context, txinfo.Transaction, voting.Vote) error {
+					votes++
+					return nil
+				}
+			},
+			verify: func(t *testing.T, tempRepo *gitalypb.Repository, tempRepoPath string, realRepo *gitalypb.Repository, realRepoPath string) {
+				require.Equal(t, 2, votes)
+			},
+		},
+		{
+			desc:          "failing preparatory vote",
+			transactional: true,
+			setup: func(t *testing.T, repo *gitalypb.Repository, repoPath string) {
+				txManager.VoteFn = func(context.Context, txinfo.Transaction, voting.Vote) error {
+					return errors.New("vote failed")
+				}
+			},
+			verify: func(t *testing.T, tempRepo *gitalypb.Repository, tempRepoPath string, realRepo *gitalypb.Repository, realRepoPath string) {
+				require.NoDirExists(t, tempRepoPath)
+				require.NoDirExists(t, realRepoPath)
+			},
+			expectedErr: helper.ErrFailedPreconditionf("preparatory vote: %w", errors.New("vote failed")),
+		},
+		{
+			desc:          "failing post-commit vote",
+			transactional: true,
+			setup: func(t *testing.T, repo *gitalypb.Repository, repoPath string) {
+				votes = 0
+				txManager.VoteFn = func(context.Context, txinfo.Transaction, voting.Vote) error {
+					votes++
+					if votes == 1 {
+						return nil
+					}
+					return errors.New("vote failed")
+				}
+			},
+			verify: func(t *testing.T, tempRepo *gitalypb.Repository, tempRepoPath string, realRepo *gitalypb.Repository, realRepoPath string) {
+				require.NoDirExists(t, tempRepoPath)
+
+				// The second vote is only a confirming vote that the node did the
+				// change. So if the second vote fails, then the change must have
+				// been performed and thus we'd see the repository.
+				require.DirExists(t, realRepoPath)
+			},
+			expectedErr: helper.ErrFailedPreconditionf("committing vote: %w", errors.New("vote failed")),
+		},
+		{
+			desc:          "voting happens after lock",
+			transactional: true,
+			setup: func(t *testing.T, repo *gitalypb.Repository, repoPath string) {
+				// We both set up transactions and create the lock. Given that we
+				// should try locking the repository before casting any votes, we do
+				// not expect to see a voting error.
+
+				require.NoError(t, os.MkdirAll(filepath.Dir(repoPath), 0o777))
+				lock, err := os.Create(repoPath + ".lock")
+				require.NoError(t, err)
+				require.NoError(t, lock.Close())
+
+				txManager.VoteFn = func(context.Context, txinfo.Transaction, voting.Vote) error {
+					require.FailNow(t, "no votes should have happened")
+					return nil
+				}
+			},
+			verify: func(t *testing.T, tempRepo *gitalypb.Repository, tempRepoPath string, realRepo *gitalypb.Repository, realRepoPath string) {
+				require.NoDirExists(t, tempRepoPath)
+				require.NoDirExists(t, realRepoPath)
+			},
+			expectedErr: fmt.Errorf("locking repository: %w", errors.New("file already locked")),
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			repo := &gitalypb.Repository{
+				StorageName:  cfg.Storages[0].Name,
+				RelativePath: gittest.NewRepositoryName(t, true),
+			}
+
+			if tc.transactional {
+				var err error
+				ctx, err = txinfo.InjectTransaction(ctx, 1, "node", true)
+				require.NoError(t, err)
+				ctx = peer.NewContext(ctx, &peer.Peer{})
+			}
+
+			repoPath, err := locator.GetPath(repo)
+			require.NoError(t, err)
+
+			if tc.setup != nil {
+				tc.setup(t, repo, repoPath)
+			}
+
+			var tempRepo *gitalypb.Repository
+			require.Equal(t, tc.expectedErr, server.createRepository(ctx, repo, func(tr *gitalypb.Repository) error {
+				tempRepo = tr
+
+				// The temporary repository must have been created in Gitaly's
+				// temporary storage path.
+				require.Equal(t, repo.StorageName, tempRepo.StorageName)
+				require.True(t, strings.HasPrefix(tempRepo.RelativePath, "+gitaly/tmp/repo"))
+
+				// Verify that the temporary repository exists and is a real Git
+				// repository.
+				tempRepoPath, err := locator.GetRepoPath(tempRepo)
+				require.NoError(t, err)
+				isBareRepo := gittest.Exec(t, cfg, "-C", tempRepoPath, "rev-parse", "--is-bare-repository")
+				require.Equal(t, "true", text.ChompBytes(isBareRepo))
+
+				if tc.seed != nil {
+					return tc.seed(t, tempRepo, tempRepoPath)
+				}
+
+				return nil
+			}))
+
+			var tempRepoPath string
+			if tempRepo != nil {
+				tempRepoPath, err = locator.GetPath(tempRepo)
+				require.NoError(t, err)
+			}
+
+			require.NotNil(t, tc.verify, "test must verify results")
+			tc.verify(t, tempRepo, tempRepoPath, repo, repoPath)
+		})
+	}
+}
