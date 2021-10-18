2f2a001d6d224209ea0952de50d420ff5369884c
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-10-18 08:18:57 +0200

    objectpool: Drop UnlinkRepostioryFromObjectPool RPC
    The UnlikRepostioryFromObjectPool RPC is not used by anything anymore,
    and the implementation from it is dangerous given that it doesn't
    actually unlink a repository from its object pool: it only tries to
    remove a remote named after the pool's project path, which we wouldn't
    ever create in the first place. The RPC has thus been deprecated in
    release v14.3.
    
    Remove the RPC and its backing code.
    
    Changelog: removed

--- ruby/proto/gitaly/objectpool_services_pb.rb
+++ ruby/proto/gitaly/objectpool_services_pb.rb
@@ -18,17 +18,6 @@ module Gitaly
       rpc :DeleteObjectPool, Gitaly::DeleteObjectPoolRequest, Gitaly::DeleteObjectPoolResponse
       # Repositories are assumed to be stored on the same disk
       rpc :LinkRepositoryToObjectPool, Gitaly::LinkRepositoryToObjectPoolRequest, Gitaly::LinkRepositoryToObjectPoolResponse
-      # UnlinkRepositoryFromObjectPool does not unlink the repository from the
-      # object pool as you'd think, but all it really does is to remove the object
-      # pool's remote pointing to the repository. And even this is a no-op given
-      # that we'd try to remove the remote by the repository's `GlRepository()`
-      # name, which we never create in the first place. To unlink repositories
-      # from an object pool, you'd really want to execute DisconnectGitAlternates
-      # to remove the repository's link to the pool's object database.
-      #
-      # This function is never called by anyone and highly misleading. It's thus
-      # deprecated and will be removed in v14.4.
-      rpc :UnlinkRepositoryFromObjectPool, Gitaly::UnlinkRepositoryFromObjectPoolRequest, Gitaly::UnlinkRepositoryFromObjectPoolResponse
       rpc :ReduplicateRepository, Gitaly::ReduplicateRepositoryRequest, Gitaly::ReduplicateRepositoryResponse
       rpc :DisconnectGitAlternates, Gitaly::DisconnectGitAlternatesRequest, Gitaly::DisconnectGitAlternatesResponse
       rpc :FetchIntoObjectPool, Gitaly::FetchIntoObjectPoolRequest, Gitaly::FetchIntoObjectPoolResponse
