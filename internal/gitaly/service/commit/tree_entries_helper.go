zhangshuyun/gitaly

commit 31205084d5cbf9372fb58e24d5661ddb29bf8558

commit: Convert tree entry helpers to accept catfile readers
Replace use of the deprecated Batch interface with use of ObjectReader
and ObjectInfoReaders in the tree entry helpers. There shouldn't be any
change in performance given that we still require both processes.

internal/gitaly/service/commit/tree_entries_helper.go
@@ -20,15 +20,17 @@ type revisionPath struct{ revision, path string }
 
 // TreeEntryFinder is a struct for searching through a tree with caching.
 type TreeEntryFinder struct {
-	c         catfile.Batch
-	treeCache map[revisionPath][]*gitalypb.TreeEntry
+	objectReader     catfile.ObjectReader
+	objectInfoReader catfile.ObjectInfoReader
+	treeCache        map[revisionPath][]*gitalypb.TreeEntry
 }
 
 // NewTreeEntryFinder initializes a TreeEntryFinder with an empty tree cache.
-func NewTreeEntryFinder(c catfile.Batch) *TreeEntryFinder {
+func NewTreeEntryFinder(objectReader catfile.ObjectReader, objectInfoReader catfile.ObjectInfoReader) *TreeEntryFinder {
 	return &TreeEntryFinder{
-		c:         c,
-		treeCache: make(map[revisionPath][]*gitalypb.TreeEntry),
+		objectReader:     objectReader,
+		objectInfoReader: objectInfoReader,
+		treeCache:        make(map[revisionPath][]*gitalypb.TreeEntry),
 	}
 }
 
@@ -40,7 +42,7 @@ func (tef *TreeEntryFinder) FindByRevisionAndPath(ctx context.Context, revision,
 
 	if !ok {
 		var err error
-		entries, err = treeEntries(ctx, tef.c, revision, dir, "", false)
+		entries, err = treeEntries(ctx, tef.objectReader, tef.objectInfoReader, revision, dir, "", false)
 		if err != nil {
 			return nil, err
 		}
@@ -104,7 +106,13 @@ func extractEntryInfoFromTreeData(treeData io.Reader, commitOid, rootOid, rootPa
 	return entries, nil
 }
 
-func treeEntries(ctx context.Context, c catfile.Batch, revision, path string, rootOid string, recursive bool) ([]*gitalypb.TreeEntry, error) {
+func treeEntries(
+	ctx context.Context,
+	objectReader catfile.ObjectReader,
+	objectInfoReader catfile.ObjectInfoReader,
+	revision, path, rootOid string,
+	recursive bool,
+) ([]*gitalypb.TreeEntry, error) {
 	if path == "." {
 		path = ""
 	}
@@ -116,7 +124,7 @@ func treeEntries(ctx context.Context, c catfile.Batch, revision, path string, ro
 	}
 
 	if len(rootOid) == 0 {
-		rootTreeInfo, err := c.Info(ctx, git.Revision(revision+"^{tree}"))
+		rootTreeInfo, err := objectInfoReader.Info(ctx, git.Revision(revision+"^{tree}"))
 		if err != nil {
 			if catfile.IsNotFound(err) {
 				return nil, nil
@@ -128,7 +136,7 @@ func treeEntries(ctx context.Context, c catfile.Batch, revision, path string, ro
 		rootOid = rootTreeInfo.Oid.String()
 	}
 
-	treeObj, err := c.Tree(ctx, git.Revision(fmt.Sprintf("%s:%s", revision, path)))
+	treeObj, err := objectReader.Object(ctx, git.Revision(fmt.Sprintf("%s:%s", revision, path)))
 	if err != nil {
 		if catfile.IsNotFound(err) {
 			return nil, nil
@@ -150,7 +158,7 @@ func treeEntries(ctx context.Context, c catfile.Batch, revision, path string, ro
 		orderedEntries = append(orderedEntries, entry)
 
 		if entry.Type == gitalypb.TreeEntry_TREE {
-			subentries, err := treeEntries(ctx, c, revision, string(entry.Path), rootOid, true)
+			subentries, err := treeEntries(ctx, objectReader, objectInfoReader, revision, string(entry.Path), rootOid, true)
 			if err != nil {
 				return nil, err
 			}

internal/gitaly/service/blob/get_blobs.go
@@ -19,10 +19,15 @@ var treeEntryToObjectType = map[gitalypb.TreeEntry_EntryType]gitalypb.ObjectType
 	gitalypb.TreeEntry_COMMIT: gitalypb.ObjectType_COMMIT,
 }
 
-func sendGetBlobsResponse(req *gitalypb.GetBlobsRequest, stream gitalypb.BlobService_GetBlobsServer, c catfile.Batch) error {
+func sendGetBlobsResponse(
+	req *gitalypb.GetBlobsRequest,
+	stream gitalypb.BlobService_GetBlobsServer,
+	objectReader catfile.ObjectReader,
+	objectInfoReader catfile.ObjectInfoReader,
+) error {
 	ctx := stream.Context()
 
-	tef := commit.NewTreeEntryFinder(c)
+	tef := commit.NewTreeEntryFinder(objectReader, objectInfoReader)
 
 	for _, revisionPath := range req.RevisionPaths {
 		revision := revisionPath.Revision
@@ -61,7 +66,7 @@ func sendGetBlobsResponse(req *gitalypb.GetBlobsRequest, stream gitalypb.BlobSer
 			continue
 		}
 
-		objectInfo, err := c.Info(ctx, git.Revision(treeEntry.Oid))
+		objectInfo, err := objectInfoReader.Info(ctx, git.Revision(treeEntry.Oid))
 		if err != nil {
 			return status.Errorf(codes.Internal, "GetBlobs: %v", err)
 		}
@@ -82,7 +87,7 @@ func sendGetBlobsResponse(req *gitalypb.GetBlobsRequest, stream gitalypb.BlobSer
 			continue
 		}
 
-		if err = sendBlobTreeEntry(response, stream, c, req.GetLimit()); err != nil {
+		if err = sendBlobTreeEntry(response, stream, objectReader, req.GetLimit()); err != nil {
 			return err
 		}
 	}
@@ -90,7 +95,7 @@ func sendGetBlobsResponse(req *gitalypb.GetBlobsRequest, stream gitalypb.BlobSer
 	return nil
 }
 
-func sendBlobTreeEntry(response *gitalypb.GetBlobsResponse, stream gitalypb.BlobService_GetBlobsServer, c catfile.Batch, limit int64) error {
+func sendBlobTreeEntry(response *gitalypb.GetBlobsResponse, stream gitalypb.BlobService_GetBlobsServer, objectReader catfile.ObjectReader, limit int64) error {
 	ctx := stream.Context()
 
 	var readLimit int64
@@ -110,9 +115,12 @@ func sendBlobTreeEntry(response *gitalypb.GetBlobsResponse, stream gitalypb.Blob
 		return nil
 	}
 
-	blobObj, err := c.Blob(ctx, git.Revision(response.Oid))
+	blobObj, err := objectReader.Object(ctx, git.Revision(response.Oid))
 	if err != nil {
 		return status.Errorf(codes.Internal, "GetBlobs: %v", err)
+	}
+	if blobObj.Type != "blob" {
+		return status.Errorf(codes.Internal, "blob got unexpected type %q", blobObj.Type)
 	}
 
 	sw := streamio.NewWriter(func(p []byte) error {
@@ -146,12 +154,17 @@ func (s *server) GetBlobs(req *gitalypb.GetBlobsRequest, stream gitalypb.BlobSer
 
 	repo := s.localrepo(req.GetRepository())
 
-	c, err := s.catfileCache.BatchProcess(stream.Context(), repo)
+	objectReader, err := s.catfileCache.ObjectReader(stream.Context(), repo)
 	if err != nil {
 		return err
 	}
 
-	return sendGetBlobsResponse(req, stream, c)
+	objectInfoReader, err := s.catfileCache.ObjectInfoReader(stream.Context(), repo)
+	if err != nil {
+		return err
+	}
+
+	return sendGetBlobsResponse(req, stream, objectReader, objectInfoReader)
 }
 
 func validateGetBlobsRequest(req *gitalypb.GetBlobsRequest) error {

internal/gitaly/service/commit/tree_entries.go
@@ -29,7 +29,12 @@ func validateGetTreeEntriesRequest(in *gitalypb.GetTreeEntriesRequest) error {
 	return nil
 }
 
-func populateFlatPath(ctx context.Context, c catfile.Batch, entries []*gitalypb.TreeEntry) error {
+func populateFlatPath(
+	ctx context.Context,
+	objectReader catfile.ObjectReader,
+	objectInfoReader catfile.ObjectInfoReader,
+	entries []*gitalypb.TreeEntry,
+) error {
 	for _, entry := range entries {
 		entry.FlatPath = entry.Path
 
@@ -38,7 +43,7 @@ func populateFlatPath(ctx context.Context, c catfile.Batch, entries []*gitalypb.
 		}
 
 		for i := 1; i < defaultFlatTreeRecursion; i++ {
-			subEntries, err := treeEntries(ctx, c, entry.CommitOid, string(entry.FlatPath), "", false)
+			subEntries, err := treeEntries(ctx, objectReader, objectInfoReader, entry.CommitOid, string(entry.FlatPath), "", false)
 			if err != nil {
 				return err
 			}
@@ -54,10 +59,18 @@ func populateFlatPath(ctx context.Context, c catfile.Batch, entries []*gitalypb.
 	return nil
 }
 
-func sendTreeEntries(stream gitalypb.CommitService_GetTreeEntriesServer, c catfile.Batch, revision, path string, recursive bool, sort gitalypb.GetTreeEntriesRequest_SortBy, p *gitalypb.PaginationParameter) error {
+func sendTreeEntries(
+	stream gitalypb.CommitService_GetTreeEntriesServer,
+	objectReader catfile.ObjectReader,
+	objectInfoReader catfile.ObjectInfoReader,
+	revision, path string,
+	recursive bool,
+	sort gitalypb.GetTreeEntriesRequest_SortBy,
+	p *gitalypb.PaginationParameter,
+) error {
 	ctx := stream.Context()
 
-	entries, err := treeEntries(ctx, c, revision, path, "", recursive)
+	entries, err := treeEntries(ctx, objectReader, objectInfoReader, revision, path, "", recursive)
 	if err != nil {
 		return err
 	}
@@ -77,7 +90,7 @@ func sendTreeEntries(stream gitalypb.CommitService_GetTreeEntriesServer, c catfi
 	}
 
 	if !recursive {
-		if err := populateFlatPath(ctx, c, entries); err != nil {
+		if err := populateFlatPath(ctx, objectReader, objectInfoReader, entries); err != nil {
 			return err
 		}
 	}
@@ -176,14 +189,19 @@ func (s *server) GetTreeEntries(in *gitalypb.GetTreeEntriesRequest, stream gital
 
 	repo := s.localrepo(in.GetRepository())
 
-	c, err := s.catfileCache.BatchProcess(stream.Context(), repo)
+	objectReader, err := s.catfileCache.ObjectReader(stream.Context(), repo)
+	if err != nil {
+		return err
+	}
+
+	objectInfoReader, err := s.catfileCache.ObjectInfoReader(stream.Context(), repo)
 	if err != nil {
 		return err
 	}
 
 	revision := string(in.GetRevision())
 	path := string(in.GetPath())
-	return sendTreeEntries(stream, c, revision, path, in.Recursive, in.GetSort(), in.GetPaginationParams())
+	return sendTreeEntries(stream, objectReader, objectInfoReader, revision, path, in.Recursive, in.GetSort(), in.GetPaginationParams())
 }
 
 func paginateTreeEntries(entries []*gitalypb.TreeEntry, p *gitalypb.PaginationParameter) ([]*gitalypb.TreeEntry, string, error) {

internal/gitaly/service/commit/tree_entry.go
@@ -12,10 +12,16 @@ import (
 	"gitlab.com/gitlab-org/gitaly/v14/streamio"
 )
 
-func sendTreeEntry(stream gitalypb.CommitService_TreeEntryServer, c catfile.Batch, revision, path string, limit, maxSize int64) error {
+func sendTreeEntry(
+	stream gitalypb.CommitService_TreeEntryServer,
+	objectReader catfile.ObjectReader,
+	objectInfoReader catfile.ObjectInfoReader,
+	revision, path string,
+	limit, maxSize int64,
+) error {
 	ctx := stream.Context()
 
-	treeEntry, err := NewTreeEntryFinder(c).FindByRevisionAndPath(ctx, revision, path)
+	treeEntry, err := NewTreeEntryFinder(objectReader, objectInfoReader).FindByRevisionAndPath(ctx, revision, path)
 	if err != nil {
 		return err
 	}
@@ -38,7 +44,7 @@ func sendTreeEntry(stream gitalypb.CommitService_TreeEntryServer, c catfile.Batc
 	}
 
 	if treeEntry.Type == gitalypb.TreeEntry_TREE {
-		treeInfo, err := c.Info(ctx, git.Revision(treeEntry.Oid))
+		treeInfo, err := objectInfoReader.Info(ctx, git.Revision(treeEntry.Oid))
 		if err != nil {
 			return err
 		}
@@ -52,7 +58,7 @@ func sendTreeEntry(stream gitalypb.CommitService_TreeEntryServer, c catfile.Batc
 		return helper.ErrUnavailable(stream.Send(response))
 	}
 
-	objectInfo, err := c.Info(ctx, git.Revision(treeEntry.Oid))
+	objectInfo, err := objectInfoReader.Info(ctx, git.Revision(treeEntry.Oid))
 	if err != nil {
 		return helper.ErrInternalf("TreeEntry: %v", err)
 	}
@@ -87,9 +93,12 @@ func sendTreeEntry(stream gitalypb.CommitService_TreeEntryServer, c catfile.Batc
 		return helper.ErrUnavailable(stream.Send(response))
 	}
 
-	blobObj, err := c.Blob(ctx, git.Revision(objectInfo.Oid))
+	blobObj, err := objectReader.Object(ctx, git.Revision(objectInfo.Oid))
 	if err != nil {
 		return err
+	}
+	if blobObj.Type != "blob" {
+		return fmt.Errorf("blob has unexpected type %q", blobObj.Type)
 	}
 
 	sw := streamio.NewWriter(func(p []byte) error {
@@ -123,12 +132,17 @@ func (s *server) TreeEntry(in *gitalypb.TreeEntryRequest, stream gitalypb.Commit
 		requestPath = strings.TrimRight(requestPath, "/")
 	}
 
-	c, err := s.catfileCache.BatchProcess(stream.Context(), repo)
+	objectReader, err := s.catfileCache.ObjectReader(stream.Context(), repo)
 	if err != nil {
 		return err
 	}
 
-	return sendTreeEntry(stream, c, string(in.GetRevision()), requestPath, in.GetLimit(), in.GetMaxSize())
+	objectInfoReader, err := s.catfileCache.ObjectInfoReader(stream.Context(), repo)
+	if err != nil {
+		return err
+	}
+
+	return sendTreeEntry(stream, objectReader, objectInfoReader, string(in.GetRevision()), requestPath, in.GetLimit(), in.GetMaxSize())
 }
 
 func validateRequest(in *gitalypb.TreeEntryRequest) error {

internal/gitaly/service/repository/archive.go
@@ -141,12 +141,17 @@ func (s *server) validateGetArchivePrecondition(
 	path string,
 	exclude []string,
 ) error {
-	c, err := s.catfileCache.BatchProcess(ctx, repo)
+	objectReader, err := s.catfileCache.ObjectReader(ctx, repo)
 	if err != nil {
 		return err
 	}
 
-	f := commit.NewTreeEntryFinder(c)
+	objectInfoReader, err := s.catfileCache.ObjectInfoReader(ctx, repo)
+	if err != nil {
+		return err
+	}
+
+	f := commit.NewTreeEntryFinder(objectReader, objectInfoReader)
 	if path != "." {
 		if ok, err := findGetArchivePath(ctx, f, commitID, path); err != nil {
 			return err

commit 7b85df16fe6343f4131fba0d598689a15684bbbd

commit: Fix `GetTreeEntries()` failing hard when reading a blob
The `GetTreeEntries()` will, given a commit and a path inside that
commit, return all tree entries below that path. While behaviour is
obvious in the case where the path does point to a subtree of the commit
in question, it's less obvious in case it's pointing to a blob given
that there cannot be any tree entries returned by such a query. While it
would be feasible to just return an error in that case given that the
query is misformed, we historically handled such queries gracefully by
returning no results at all.

This behaviour broke when converting the tree entries helper to use the
new catfile interfaces: the old `Batch` interface used to return a
`NotFoundError` if the expected type didn't match the actual type, and
we explicitly handle that case to gracefully fail and swallow the error.
Wtih the conversion, We do not check for the type of the object returned
by the catfile process anymore, but just blindly try to parse it as a
tree object even if it refers to a blob. Luckily, this regression was
catched by Rails' test suite.

Fix the regression by properly checking for the object type again: in
case it is not a tree, we just return nothing at all. While at it, this
commit also makes sure to always discard objects in case we fail with an
error such that the object reader is not dirty on failure.

Changelog: fixed
Fixes: 31205084d (commit: Convert tree entry helpers to accept catfile readers, 2021-10-12)

internal/gitaly/service/commit/tree_entries_helper.go
@@ -112,7 +112,7 @@ func treeEntries(
 	objectInfoReader catfile.ObjectInfoReader,
 	revision, path, rootOid string,
 	recursive bool,
-) ([]*gitalypb.TreeEntry, error) {
+) (_ []*gitalypb.TreeEntry, returnedErr error) {
 	if path == "." {
 		path = ""
 	}
@@ -142,6 +142,18 @@ func treeEntries(
 			return nil, nil
 		}
 		return nil, err
+	}
+	defer func() {
+		if _, err := io.Copy(io.Discard, treeObj); err != nil && returnedErr == nil {
+			returnedErr = fmt.Errorf("discarding object: %w", err)
+		}
+	}()
+
+	// The tree entry may not refer to a subtree, but instead to a blob. Historically, we have
+	// simply ignored such objects altogether and didn't return an error, so we keep the same
+	// behaviour.
+	if treeObj.Type != "tree" {
+		return nil, nil
 	}
 
 	entries, err := extractEntryInfoFromTreeData(treeObj, revision, rootOid, path, treeObj.Oid.String())

internal/gitaly/service/commit/tree_entries_test.go
@@ -12,10 +12,8 @@ import (
 	"gitlab.com/gitlab-org/gitaly/v14/internal/git/gittest"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/helper/text"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper"
-	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper/testassert"
 	"gitlab.com/gitlab-org/gitaly/v14/proto/go/gitalypb"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
 )
 
 func TestSuccessfulGetTreeEntriesWithCurlyBraces(t *testing.T) {
@@ -613,11 +611,11 @@ func TestGetTreeEntries_file(t *testing.T) {
 	})
 	require.NoError(t, err)
 
-	_, err = stream.Recv()
-	// When reading the tree entry, we do not check whether it refers to a blob or to an
-	// subtree. Instead, we always parse the object as a subtree, which will obviously fail in
-	// case it is a blob instead.
-	testassert.GrpcEqualErr(t, status.Error(codes.Unknown, "read entry path: EOF"), err)
+	// When trying to read a blob, the expectation is that we fail gracefully by just returning
+	// nothing.
+	entries, err := stream.Recv()
+	require.Equal(t, io.EOF, err)
+	require.Empty(t, entries)
 }
 
 func TestFailedGetTreeEntriesRequestDueToValidationError(t *testing.T) {