af6a9bc8e8d10ac0636165662f5fc1e3bff7a40f
James Fargher <jfargher@gitlab.com>
2021-10-28 10:24:59 +1300

    localrepo: Replace manual for-each-ref decoding in localrepo
    Previously ref parsing was done in place. This replaces the parsing with
    a shared implementation.

--- internal/git/localrepo/refs.go
+++ internal/git/localrepo/refs.go
@@ -107,25 +107,20 @@ func (repo *Repo) getReferences(ctx context.Context, limit uint, patterns ...str
 		return nil, err
 	}
 
-	scanner := bufio.NewScanner(cmd)
+	decoder := git.NewForEachRefDecoder(cmd)
 
 	var refs []git.Reference
-	for scanner.Scan() {
-		line := bytes.SplitN(scanner.Bytes(), []byte{0}, 3)
-		if len(line) != 3 {
-			return nil, errors.New("unexpected reference format")
+	for {
+		var ref git.Reference
+		err := decoder.Decode(&ref)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, fmt.Errorf("reading standard input: %v", err)
 		}
-
-		if len(line[2]) == 0 {
-			refs = append(refs, git.NewReference(git.ReferenceName(line[0]), string(line[1])))
-		} else {
-			refs = append(refs, git.NewSymbolicReference(git.ReferenceName(line[0]), string(line[1])))
-		}
+		refs = append(refs, ref)
 	}
 
-	if err := scanner.Err(); err != nil {
-		return nil, fmt.Errorf("reading standard input: %v", err)
-	}
 	if err := cmd.Wait(); err != nil {
 		return nil, err
 	}
