63cb4dc719d6a5d166358da35772191fcc120ee7
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-11-03 12:20:50 +0100

    objectpool: Demonstrate broken fetching of refs
    When fetching into an object pool, then we accidentally end up with
    duplicated branches and tags because of how refspecs are set up.
    Demonstrate these bugs by adding a new testcase.

--- internal/git/objectpool/clone_test.go
+++ internal/git/objectpool/clone_test.go
@@ -1,50 +1,12 @@
 package objectpool
 
 import (
-	"context"
 	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
-	"gitlab.com/gitlab-org/gitaly/v14/internal/backchannel"
-	"gitlab.com/gitlab-org/gitaly/v14/internal/git"
-	"gitlab.com/gitlab-org/gitaly/v14/internal/git/catfile"
-	"gitlab.com/gitlab-org/gitaly/v14/internal/git/gittest"
-	"gitlab.com/gitlab-org/gitaly/v14/internal/gitaly/config"
-	"gitlab.com/gitlab-org/gitaly/v14/internal/gitaly/transaction"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper"
-	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper/testcfg"
-	"gitlab.com/gitlab-org/gitaly/v14/proto/go/gitalypb"
 )
-
-func setupObjectPool(t *testing.T) (*ObjectPool, *gitalypb.Repository) {
-	t.Helper()
-
-	cfg, repo, _ := testcfg.BuildWithRepo(t)
-	gitCommandFactory := git.NewExecCommandFactory(cfg)
-
-	catfileCache := catfile.NewCache(cfg)
-	t.Cleanup(catfileCache.Stop)
-
-	pool, err := NewObjectPool(
-		cfg,
-		config.NewLocator(cfg),
-		gitCommandFactory,
-		catfileCache,
-		transaction.NewManager(cfg, backchannel.NewRegistry()),
-		repo.GetStorageName(),
-		gittest.NewObjectPoolName(t),
-	)
-	require.NoError(t, err)
-
-	t.Cleanup(func() {
-		if err := pool.Remove(context.TODO()); err != nil {
-			panic(err)
-		}
-	})
-
-	return pool, repo
-}
 
 func TestClone(t *testing.T) {
 	ctx, cancel := testhelper.Context()
