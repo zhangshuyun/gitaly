58746ded095324d8f726f3fad3c3d913a3be6620
John Cai <jcai@gitlab.com>
2021-12-06 17:55:30 -0500

    Revert "Merge branch 'mk-grpc-1.42' into 'master'"
    This reverts commit 1a173fd8eefcea695c30d8faa2b2948e868a8e2c, reversing
    changes made to 9388cf42b3065b0e7b636bfe22883717eedb5623.

--- ruby/proto/gitaly/ref_services_pb.rb
+++ ruby/proto/gitaly/ref_services_pb.rb
@@ -8,44 +8,44 @@ module Gitaly
   module RefService
     class Service
 
-      include ::GRPC::GenericService
+      include GRPC::GenericService
 
       self.marshal_class_method = :encode
       self.unmarshal_class_method = :decode
       self.service_name = 'gitaly.RefService'
 
-      rpc :FindDefaultBranchName, ::Gitaly::FindDefaultBranchNameRequest, ::Gitaly::FindDefaultBranchNameResponse
-      rpc :FindAllBranchNames, ::Gitaly::FindAllBranchNamesRequest, stream(::Gitaly::FindAllBranchNamesResponse)
-      rpc :FindAllTagNames, ::Gitaly::FindAllTagNamesRequest, stream(::Gitaly::FindAllTagNamesResponse)
+      rpc :FindDefaultBranchName, Gitaly::FindDefaultBranchNameRequest, Gitaly::FindDefaultBranchNameResponse
+      rpc :FindAllBranchNames, Gitaly::FindAllBranchNamesRequest, stream(Gitaly::FindAllBranchNamesResponse)
+      rpc :FindAllTagNames, Gitaly::FindAllTagNamesRequest, stream(Gitaly::FindAllTagNamesResponse)
       # Return a stream so we can divide the response in chunks of branches
-      rpc :FindLocalBranches, ::Gitaly::FindLocalBranchesRequest, stream(::Gitaly::FindLocalBranchesResponse)
-      rpc :FindAllBranches, ::Gitaly::FindAllBranchesRequest, stream(::Gitaly::FindAllBranchesResponse)
+      rpc :FindLocalBranches, Gitaly::FindLocalBranchesRequest, stream(Gitaly::FindLocalBranchesResponse)
+      rpc :FindAllBranches, Gitaly::FindAllBranchesRequest, stream(Gitaly::FindAllBranchesResponse)
       # Returns a stream of tags repository has.
-      rpc :FindAllTags, ::Gitaly::FindAllTagsRequest, stream(::Gitaly::FindAllTagsResponse)
-      rpc :FindTag, ::Gitaly::FindTagRequest, ::Gitaly::FindTagResponse
-      rpc :FindAllRemoteBranches, ::Gitaly::FindAllRemoteBranchesRequest, stream(::Gitaly::FindAllRemoteBranchesResponse)
-      rpc :RefExists, ::Gitaly::RefExistsRequest, ::Gitaly::RefExistsResponse
+      rpc :FindAllTags, Gitaly::FindAllTagsRequest, stream(Gitaly::FindAllTagsResponse)
+      rpc :FindTag, Gitaly::FindTagRequest, Gitaly::FindTagResponse
+      rpc :FindAllRemoteBranches, Gitaly::FindAllRemoteBranchesRequest, stream(Gitaly::FindAllRemoteBranchesResponse)
+      rpc :RefExists, Gitaly::RefExistsRequest, Gitaly::RefExistsResponse
       # FindBranch finds a branch by its unqualified name (like "master") and
       # returns the commit it currently points to.
-      rpc :FindBranch, ::Gitaly::FindBranchRequest, ::Gitaly::FindBranchResponse
-      rpc :DeleteRefs, ::Gitaly::DeleteRefsRequest, ::Gitaly::DeleteRefsResponse
-      rpc :ListBranchNamesContainingCommit, ::Gitaly::ListBranchNamesContainingCommitRequest, stream(::Gitaly::ListBranchNamesContainingCommitResponse)
-      rpc :ListTagNamesContainingCommit, ::Gitaly::ListTagNamesContainingCommitRequest, stream(::Gitaly::ListTagNamesContainingCommitResponse)
+      rpc :FindBranch, Gitaly::FindBranchRequest, Gitaly::FindBranchResponse
+      rpc :DeleteRefs, Gitaly::DeleteRefsRequest, Gitaly::DeleteRefsResponse
+      rpc :ListBranchNamesContainingCommit, Gitaly::ListBranchNamesContainingCommitRequest, stream(Gitaly::ListBranchNamesContainingCommitResponse)
+      rpc :ListTagNamesContainingCommit, Gitaly::ListTagNamesContainingCommitRequest, stream(Gitaly::ListTagNamesContainingCommitResponse)
       # GetTagSignatures returns signatures for annotated tags resolved from a set of revisions. Revisions
       # which don't resolve to an annotated tag are silently discarded. Revisions which cannot be resolved
       # result in an error. Tags which are annotated but not signed will return a TagSignature response
       # which has no signature, but its unsigned contents will still be returned.
-      rpc :GetTagSignatures, ::Gitaly::GetTagSignaturesRequest, stream(::Gitaly::GetTagSignaturesResponse)
-      rpc :GetTagMessages, ::Gitaly::GetTagMessagesRequest, stream(::Gitaly::GetTagMessagesResponse)
-      rpc :PackRefs, ::Gitaly::PackRefsRequest, ::Gitaly::PackRefsResponse
+      rpc :GetTagSignatures, Gitaly::GetTagSignaturesRequest, stream(Gitaly::GetTagSignaturesResponse)
+      rpc :GetTagMessages, Gitaly::GetTagMessagesRequest, stream(Gitaly::GetTagMessagesResponse)
+      rpc :PackRefs, Gitaly::PackRefsRequest, Gitaly::PackRefsResponse
       # ListRefs returns a stream of all references in the repository. By default, pseudo-revisions like HEAD
       # will not be returned by this RPC. Any symbolic references will be resolved to the object ID it is
       # pointing at.
-      rpc :ListRefs, ::Gitaly::ListRefsRequest, stream(::Gitaly::ListRefsResponse)
+      rpc :ListRefs, Gitaly::ListRefsRequest, stream(Gitaly::ListRefsResponse)
       # FindRefsByOID returns an array of fully qualified reference names that point to an object ID.
       # It returns nothing if the object ID doesn't exist, or doesn't point to
       # any branches or tags. Prefixes can be also be used as the object ID.
-      rpc :FindRefsByOID, ::Gitaly::FindRefsByOIDRequest, ::Gitaly::FindRefsByOIDResponse
+      rpc :FindRefsByOID, Gitaly::FindRefsByOIDRequest, Gitaly::FindRefsByOIDResponse
     end
 
     Stub = Service.rpc_stub_class
