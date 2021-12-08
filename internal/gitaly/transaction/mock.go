8b719005d03a0c6f17d5bd2a00dcf34d51d58695
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-12-08 13:10:06 +0100

    transaction: Introduce tracking manager
    A lot of tests need to create a mock transaction manager to track the
    transactional votes and assert that we've got the votes we expected.
    This is quite repetitive and ultimately requires us to change a lot of
    function signatures if the `transaction.Manager` interface changes.
    
    Improve the situation by introducing a new `TrackingManager` which
    records all votes.

--- internal/gitaly/transaction/mock.go
+++ internal/gitaly/transaction/mock.go
@@ -3,6 +3,7 @@ package transaction
 import (
 	"context"
 	"errors"
+	"sync"
 
 	"gitlab.com/gitlab-org/gitaly/v14/internal/transaction/txinfo"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/transaction/voting"
@@ -28,4 +29,45 @@ func (m *MockManager) Stop(ctx context.Context, tx txinfo.Transaction) error {
 		return errors.New("mock does not implement Stop function")
 	}
 	return m.StopFn(ctx, tx)
+}
+
+// TrackingManager is a transaction manager which tracks all votes. Voting functions never return
+// an error.
+type TrackingManager struct {
+	MockManager
+
+	votesLock sync.Mutex
+	votes     []voting.Vote
+}
+
+// NewTrackingManager creates a new TrackingManager which is ready for use.
+func NewTrackingManager() *TrackingManager {
+	manager := &TrackingManager{}
+
+	manager.VoteFn = func(_ context.Context, _ txinfo.Transaction, vote voting.Vote) error {
+		manager.votesLock.Lock()
+		defer manager.votesLock.Unlock()
+		manager.votes = append(manager.votes, vote)
+		return nil
+	}
+
+	return manager
+}
+
+// Votes returns a copy of all votes which have been cast.
+func (m *TrackingManager) Votes() []voting.Vote {
+	m.votesLock.Lock()
+	defer m.votesLock.Unlock()
+
+	votes := make([]voting.Vote, len(m.votes))
+	copy(votes, m.votes)
+
+	return votes
+}
+
+// Reset resets all votes which have been recorded up to this point.
+func (m *TrackingManager) Reset() {
+	m.votesLock.Lock()
+	defer m.votesLock.Unlock()
+	m.votes = nil
 }
