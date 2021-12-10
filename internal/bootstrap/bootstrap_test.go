481c31d92f1670eacd02bad898d1e6d7d8a0f2c1
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-12-10 10:11:17 +0100

    bootstrap: Rewrite tests to not use HTTP server
    Our tests for the bootstrap package are quite complex and really hard to
    understand. Furthermore, they're using timeouts everywhere, which slows
    down execution of the tests, futher obscures what we actually want to
    test, and has the potential for flakiness.
    
    Rewrite the tests to not rely on the HTTP server anymore. This server
    was used to simulate slow requests which caused us to exceed the grace
    period in some cases, but doing so via an HTTP server is roundabout: we
    can just block test execution via channels in a much easier way.
    Instead, we now use a set of channels which allow us control when the
    upgrader is doing various things, like when the upgrade process starts
    or when the parent process supposedly exits. This allows us to directly
    exercise what we want to in a much more direct way.
    
    Furthermore, tests are refactored to not use real listeners anymore.
    We do not care about whether the listeners work as expected: lifetime
    wouldn't be controlled by the bootstrapper anyway, but instead by the
    shutdown function that's provided. Using a mock listener thus allows us
    to test the effects we want to see on the listeners directly.

--- internal/bootstrap/bootstrap_test.go
+++ internal/bootstrap/bootstrap_test.go
@@ -2,231 +2,216 @@ package bootstrap
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"io"
 	"net"
-	"net/http"
 	"os"
 	"path/filepath"
-	"strconv"
 	"syscall"
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper"
 )
 
-type mockUpgrader struct {
-	exit      chan struct{}
-	hasParent bool
-}
+func TestBootstrap_unixListener(t *testing.T) {
+	for _, tc := range []struct {
+		desc               string
+		hasParent          bool
+		preexistingSocket  bool
+		expectSocketExists bool
+	}{
+		{
+			desc:               "no parent, no preexisting socket",
+			hasParent:          false,
+			preexistingSocket:  false,
+			expectSocketExists: false,
+		},
+		{
+			desc:              "no parent, preexisting socket",
+			hasParent:         false,
+			preexistingSocket: true,
+			// On first boot, the bootstrapper is expected to remove any preexisting
+			// sockets.
+			expectSocketExists: false,
+		},
+		{
+			desc:               "parent, no preexisting socket",
+			hasParent:          true,
+			preexistingSocket:  false,
+			expectSocketExists: false,
+		},
+		{
+			desc:              "parent, preexisting socket",
+			hasParent:         true,
+			preexistingSocket: true,
+			// When we do have a parent, then we cannot remove the socket or otherwise
+			// we might impact the parent process's ability to serve requests.
+			expectSocketExists: true,
+		},
+	} {
+		t.Run(tc.desc, func(t *testing.T) {
+			tempDir := testhelper.TempDir(t)
+			socketPath := filepath.Join(tempDir, "gitaly-test-unix-socket")
 
-func (m *mockUpgrader) Exit() <-chan struct{} {
-	return m.exit
-}
+			sentinel := &mockListener{}
+			listen := func(network, addr string) (net.Listener, error) {
+				require.Equal(t, "unix", network)
+				require.Equal(t, socketPath, addr)
+				if tc.expectSocketExists {
+					require.FileExists(t, socketPath)
+				} else {
+					require.NoFileExists(t, socketPath)
+				}
 
-func (m *mockUpgrader) Stop() {}
+				return sentinel, nil
+			}
 
-func (m *mockUpgrader) HasParent() bool {
-	return m.hasParent
-}
+			upgrader := &mockUpgrader{
+				hasParent: tc.hasParent,
+			}
 
-func (m *mockUpgrader) Ready() error { return nil }
+			b, err := _new(upgrader, listen, false)
+			require.NoError(t, err)
 
-func (m *mockUpgrader) Upgrade() error {
-	// to upgrade we close the exit channel
-	close(m.exit)
-	return nil
-}
+			if tc.preexistingSocket {
+				require.NoError(t, os.WriteFile(socketPath, nil, 0o755))
+			}
 
-type testServer struct {
-	t         *testing.T
-	ctx       context.Context
-	server    *http.Server
-	listeners map[string]net.Listener
-	url       string
-}
-
-func (s *testServer) slowRequest(duration time.Duration) <-chan error {
-	done := make(chan error)
-
-	go func() {
-		request, err := http.NewRequestWithContext(s.ctx, http.MethodGet, fmt.Sprintf("%sslow?seconds=%d", s.url, int(duration.Seconds())), nil)
-		require.NoError(s.t, err)
-
-		response, err := http.DefaultClient.Do(request)
-		if response != nil {
-			_, err := io.Copy(io.Discard, response.Body)
-			require.NoError(s.t, err)
-			require.NoError(s.t, response.Body.Close())
-		}
-
-		done <- err
-	}()
-
-	return done
-}
-
-func TestCreateUnixListener(t *testing.T) {
-	tempDir := testhelper.TempDir(t)
-
-	socketPath := filepath.Join(tempDir, "gitaly-test-unix-socket")
-	if err := os.Remove(socketPath); err != nil {
-		require.True(t, os.IsNotExist(err), "cannot delete dangling socket: %v", err)
+			listener, err := b.listen("unix", socketPath)
+			require.NoError(t, err)
+			require.Equal(t, sentinel, listener)
+		})
 	}
-
-	// simulate a dangling socket
-	require.NoError(t, os.WriteFile(socketPath, nil, 0o755))
-
-	listen := func(network, addr string) (net.Listener, error) {
-		require.Equal(t, "unix", network)
-		require.Equal(t, socketPath, addr)
-
-		return net.Listen(network, addr)
-	}
-	u := &mockUpgrader{}
-	b, err := _new(u, listen, false)
-	require.NoError(t, err)
-
-	// first boot
-	l, err := b.listen("unix", socketPath)
-	require.NoError(t, err, "failed to bind on first boot")
-	require.NoError(t, l.Close())
-
-	// simulate binding during an upgrade
-	u.hasParent = true
-	l, err = b.listen("unix", socketPath)
-	require.NoError(t, err, "failed to bind on upgrade")
-	require.NoError(t, l.Close())
 }
 
-func waitWithTimeout(t *testing.T, waitCh <-chan error, timeout time.Duration) error {
-	select {
-	case <-time.After(timeout):
-		t.Fatal("time out waiting for waitCh")
-	case waitErr := <-waitCh:
-		return waitErr
-	}
-
-	return nil
-}
-
-func TestImmediateTerminationOnSocketError(t *testing.T) {
+func TestBootstrap_listenerError(t *testing.T) {
 	ctx, cancel := testhelper.Context()
 	defer cancel()
 
-	b, server, stopAction := makeBootstrap(t, ctx)
+	b, upgrader, listeners := setup(t, ctx)
 
 	waitCh := make(chan error)
-	go func() { waitCh <- b.Wait(2*time.Second, stopAction) }()
+	go func() { waitCh <- b.Wait(time.Hour, nil) }()
 
-	require.NoError(t, server.listeners["tcp"].Close(), "Closing first listener")
+	// Signal readiness, but don't start the upgrade. Like this, we can close the listener in a
+	// raceless manner and wait for the error to propagate.
+	upgrader.readyCh <- nil
 
-	err := waitWithTimeout(t, waitCh, 1*time.Second)
-	require.Error(t, err)
-	require.True(t, errors.Is(err, net.ErrClosed), "expected closed connection error, got %T: %q", err, err)
+	// Inject a listener error.
+	listeners["tcp"].errorCh <- assert.AnError
+
+	require.Equal(t, assert.AnError, <-waitCh)
 }
 
-func TestImmediateTerminationOnSignal(t *testing.T) {
+func TestBootstrap_signal(t *testing.T) {
 	for _, sig := range []syscall.Signal{syscall.SIGTERM, syscall.SIGINT} {
 		t.Run(sig.String(), func(t *testing.T) {
 			ctx, cancel := testhelper.Context()
 			defer cancel()
 
-			b, server, stopAction := makeBootstrap(t, ctx)
-
-			done := server.slowRequest(3 * time.Minute)
+			b, upgrader, _ := setup(t, ctx)
 
 			waitCh := make(chan error)
-			go func() { waitCh <- b.Wait(2*time.Second, stopAction) }()
+			go func() { waitCh <- b.Wait(time.Hour, nil) }()
 
-			// make sure we are inside b.Wait() or we'll kill the test suite
-			time.Sleep(100 * time.Millisecond)
+			// Start the upgrade, but don't unblock `Exit()` such that we'll be blocked
+			// waiting on the parent.
+			upgrader.readyCh <- nil
 
+			// We can now kill ourselves. This signal should be retrieved by `Wait()`,
+			// which would then return an error.
 			self, err := os.FindProcess(os.Getpid())
 			require.NoError(t, err)
 			require.NoError(t, self.Signal(sig))
 
-			waitErr := waitWithTimeout(t, waitCh, 1*time.Second)
-			require.Error(t, waitErr)
-			require.Contains(t, waitErr.Error(), "received signal")
-			require.Contains(t, waitErr.Error(), sig.String())
-
-			server.server.Close()
-
-			require.Error(t, <-done)
+			require.Equal(t, fmt.Errorf("received signal %q", sig), <-waitCh)
 		})
 	}
 }
 
-func TestGracefulTerminationStuck(t *testing.T) {
+func TestBootstrap_gracefulTerminationStuck(t *testing.T) {
 	ctx, cancel := testhelper.Context()
 	defer cancel()
 
-	b, server, stopAction := makeBootstrap(t, ctx)
+	b, upgrader, _ := setup(t, ctx)
 
-	err := testGracefulUpdate(t, server, b, 3*time.Second, 2*time.Second, nil, stopAction)
-	require.Contains(t, err.Error(), "grace period expired")
+	doneCh := make(chan struct{})
+	err := performUpgrade(t, b, upgrader, time.Second, nil, func() {
+		defer close(doneCh)
+
+		// We block on context cancellation here, which essentially means that this won't
+		// terminate and thus the graceful termination will be stuck.
+		<-ctx.Done()
+	})
+	require.Equal(t, fmt.Errorf("graceful upgrade: grace period expired"), err)
+
+	cancel()
+	<-doneCh
 }
 
-func TestGracefulTerminationWithSignals(t *testing.T) {
-	self, err := os.FindProcess(os.Getpid())
-	require.NoError(t, err)
-
+func TestBootstrap_gracefulTerminationWithSignals(t *testing.T) {
 	for _, sig := range []syscall.Signal{syscall.SIGTERM, syscall.SIGINT} {
 		t.Run(sig.String(), func(t *testing.T) {
 			ctx, cancel := testhelper.Context()
 			defer cancel()
-			b, server, stopAction := makeBootstrap(t, ctx)
 
-			err := testGracefulUpdate(t, server, b, 1*time.Second, 2*time.Second, func() {
+			b, upgrader, _ := setup(t, ctx)
+
+			doneCh := make(chan struct{})
+			err := performUpgrade(t, b, upgrader, time.Hour, func() {
+				self, err := os.FindProcess(os.Getpid())
+				require.NoError(t, err)
 				require.NoError(t, self.Signal(sig))
-			}, stopAction)
-			require.Contains(t, err.Error(), "force shutdown")
+			}, func() {
+				defer close(doneCh)
+				// Block the upgrade indefinitely such that we can be sure that the
+				// signal was processed.
+				<-ctx.Done()
+			})
+			require.Equal(t, fmt.Errorf("graceful upgrade: force shutdown"), err)
+
+			cancel()
+			<-doneCh
 		})
 	}
 }
 
-func TestGracefulTerminationServerErrors(t *testing.T) {
+func TestBootstrap_gracefulTerminationTimeoutWithListenerError(t *testing.T) {
 	ctx, cancel := testhelper.Context()
 	defer cancel()
-	b, server, _ := makeBootstrap(t, ctx)
 
-	done := make(chan error, 1)
-	// This is a simulation of receiving a listener error during waitGracePeriod
-	stopAction := func() {
-		// we close the unix listener in order to test that the shutdown will not fail, but it keep waiting for the TCP request
-		require.NoError(t, server.listeners["unix"].Close())
+	b, upgrader, listeners := setup(t, ctx)
 
-		// we start a new TCP request that if faster than the grace period
-		req := server.slowRequest(time.Second)
-		done <- <-req
-		close(done)
+	doneCh := make(chan struct{})
+	err := performUpgrade(t, b, upgrader, time.Second, nil, func() {
+		defer close(doneCh)
 
-		require.NoError(t, server.server.Shutdown(context.Background()))
-	}
+		// We inject an error into the Unix socket to assert that this won't kill the server
+		// immediately, but waits for the TCP connection to terminate as expected.
+		listeners["unix"].errorCh <- assert.AnError
 
-	err := testGracefulUpdate(t, server, b, 3*time.Second, 2*time.Second, nil, stopAction)
-	require.Contains(t, err.Error(), "grace period expired")
+		// We block on context cancellation here, which essentially means that this won't
+		// terminate.
+		<-ctx.Done()
+	})
+	require.Equal(t, fmt.Errorf("graceful upgrade: grace period expired"), err)
 
-	require.NoError(t, <-done)
+	cancel()
+	<-doneCh
 }
 
-func TestGracefulTermination(t *testing.T) {
+func TestBootstrap_gracefulTermination(t *testing.T) {
 	ctx, cancel := testhelper.Context()
 	defer cancel()
-	b, server, _ := makeBootstrap(t, ctx)
 
-	// Using server.Close we bypass the graceful shutdown faking a completed shutdown
-	stopAction := func() { server.server.Close() }
+	b, upgrader, _ := setup(t, ctx)
 
-	err := testGracefulUpdate(t, server, b, 1*time.Second, 2*time.Second, nil, stopAction)
-	require.Contains(t, err.Error(), "completed")
+	require.Equal(t, fmt.Errorf("graceful upgrade: completed"), performUpgrade(t, b, upgrader, time.Hour, nil, nil))
 }
 
-func TestPortReuse(t *testing.T) {
+func TestBootstrap_portReuse(t *testing.T) {
 	b, err := New()
 	require.NoError(t, err)
 
@@ -244,84 +229,54 @@ func TestPortReuse(t *testing.T) {
 	b.upgrader.Stop()
 }
 
-func testGracefulUpdate(t *testing.T, server *testServer, b *Bootstrap, waitTimeout, gracefulWait time.Duration, duringGracePeriodCallback func(), stopAction func()) error {
+func performUpgrade(
+	t *testing.T,
+	b *Bootstrap,
+	upgrader *mockUpgrader,
+	gracefulWait time.Duration,
+	duringGracePeriodCallback func(),
+	stopAction func(),
+) error {
 	waitCh := make(chan error)
 	go func() { waitCh <- b.Wait(gracefulWait, stopAction) }()
 
-	// Start a slow request to keep the old server from shutting down immediately.
-	req := server.slowRequest(2 * gracefulWait)
+	// Simulate an upgrade request after entering into the blocking b.Wait() and during the
+	// slowRequest execution
+	upgrader.readyCh <- nil
+	upgrader.exitCh <- struct{}{}
 
-	// make sure slow request is being handled
-	time.Sleep(100 * time.Millisecond)
-
-	// Simulate an upgrade request after entering into the blocking b.Wait() and during the slowRequest execution
-	require.NoError(t, b.upgrader.Upgrade())
-
+	// We know that `exitCh` has been consumed, so we're now in the grace period where we wait
+	// for the old server to exit.
 	if duringGracePeriodCallback != nil {
-		// make sure we are on the grace period
-		time.Sleep(100 * time.Millisecond)
-
 		duringGracePeriodCallback()
 	}
 
-	waitErr := waitWithTimeout(t, waitCh, waitTimeout)
-	require.Error(t, waitErr)
-	require.Contains(t, waitErr.Error(), "graceful upgrade")
-
-	server.server.Close()
-
-	clientErr := waitWithTimeout(t, req, 1*time.Second)
-	require.Error(t, clientErr, "slow request not terminated after the grace period")
-
-	return waitErr
+	return <-waitCh
 }
 
-func makeBootstrap(t *testing.T, ctx context.Context) (*Bootstrap, *testServer, func()) {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
-		w.WriteHeader(200)
-	})
-	mux.HandleFunc("/slow", func(w http.ResponseWriter, r *http.Request) {
-		sec, err := strconv.Atoi(r.URL.Query().Get("seconds"))
-		require.NoError(t, err)
-
-		select {
-		case <-ctx.Done():
-		case <-time.After(time.Duration(sec) * time.Second):
-		}
-
-		w.WriteHeader(200)
-	})
-
-	s := http.Server{Handler: mux}
-	t.Cleanup(func() { testhelper.MustClose(t, &s) })
-	u := &mockUpgrader{exit: make(chan struct{})}
+func setup(t *testing.T, ctx context.Context) (*Bootstrap, *mockUpgrader, mockListeners) {
+	u := &mockUpgrader{
+		exitCh:  make(chan struct{}),
+		readyCh: make(chan error),
+	}
 
 	b, err := _new(u, net.Listen, false)
 	require.NoError(t, err)
 
-	listeners := make(map[string]net.Listener)
+	listeners := mockListeners{}
 	start := func(network, address string) Starter {
+		listeners[network] = &mockListener{}
+
 		return func(listen ListenFunc, errors chan<- error) error {
-			l, err := listen(network, address)
-			if err != nil {
-				return err
-			}
-			listeners[network] = l
-
-			go func() {
-				errors <- s.Serve(l)
-			}()
-
+			listeners[network].errorCh = errors
+			listeners[network].listening = true
 			return nil
 		}
 	}
 
-	tempDir := testhelper.TempDir(t)
-
 	for network, address := range map[string]string{
 		"tcp":  "127.0.0.1:0",
-		"unix": filepath.Join(tempDir, "gitaly-test-unix-socket"),
+		"unix": "some-socket",
 	} {
 		b.RegisterStarter(start(network, address))
 	}
@@ -329,44 +284,50 @@ func makeBootstrap(t *testing.T, ctx context.Context) (*Bootstrap, *testServer, 
 	require.NoError(t, b.Start())
 	require.Equal(t, 2, len(listeners))
 
-	// test connection
-	testAllListeners(t, ctx, listeners)
-
-	addr := listeners["tcp"].Addr()
-	url := fmt.Sprintf("http://%s/", addr.String())
-
-	return b, &testServer{
-		t:         t,
-		ctx:       ctx,
-		server:    &s,
-		listeners: listeners,
-		url:       url,
-	}, func() { require.NoError(t, s.Shutdown(context.Background())) }
-}
-
-func testAllListeners(t *testing.T, ctx context.Context, listeners map[string]net.Listener) {
-	for network, listener := range listeners {
-		addr := listener.Addr().String()
-
-		// overriding Client.Transport.Dial we can connect to TCP and UNIX sockets
-		client := &http.Client{
-			Transport: &http.Transport{
-				Dial: func(_, _ string) (net.Conn, error) {
-					return net.Dial(network, addr)
-				},
-			},
-		}
-
-		request, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://fakeHost/", nil)
-		require.NoError(t, err)
-
-		r, err := client.Do(request)
-		require.NoError(t, err)
-
-		_, err = io.Copy(io.Discard, r.Body)
-		require.NoError(t, err)
-		require.NoError(t, r.Body.Close())
-
-		require.Equal(t, 200, r.StatusCode)
+	for _, listener := range listeners {
+		require.True(t, listener.listening)
 	}
+
+	return b, u, listeners
 }
+
+type mockUpgrader struct {
+	exitCh    chan struct{}
+	readyCh   chan error
+	hasParent bool
+}
+
+func (m *mockUpgrader) Exit() <-chan struct{} {
+	return m.exitCh
+}
+
+func (m *mockUpgrader) Stop() {}
+
+func (m *mockUpgrader) HasParent() bool {
+	return m.hasParent
+}
+
+func (m *mockUpgrader) Ready() error {
+	return <-m.readyCh
+}
+
+func (m *mockUpgrader) Upgrade() error {
+	// To upgrade, we send a message on the exit channel. Like this, we can assert that the exit
+	// signal has been consumed given that we'd otherwise block forever.
+	m.exitCh <- struct{}{}
+	return nil
+}
+
+type mockListener struct {
+	net.Listener
+	errorCh   chan<- error
+	closed    bool
+	listening bool
+}
+
+func (m *mockListener) Close() error {
+	m.closed = true
+	return nil
+}
+
+type mockListeners map[string]*mockListener
