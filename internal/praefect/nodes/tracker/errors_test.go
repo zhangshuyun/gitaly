e5e1132e0d55b5f5fc54cfa4fba4e41c93a8f626
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-12-14 12:10:11 +0100

    nodes/tracker: Convert error tracker tests to not require sleeps
    Error tracking uses an error window during which errors are considered.
    Any error older than this window is simply ignored. To test this, we use
    various sleeps in our tests such that we can be sure that the window is
    over and that errors are treated as expected.
    
    Convert the code to instead use an error window function which can be
    injected by the caller. Like this, production code can use the normal
    error window function which we currently use, and tests can use a
    different one which doesn't use timing as the source and thus allows us
    to get rid of the sleeps.

--- internal/praefect/nodes/tracker/errors_test.go
+++ internal/praefect/nodes/tracker/errors_test.go
@@ -10,13 +10,18 @@ import (
 	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper"
 )
 
+var (
+	alwaysInErrorWindow = func(time.Time, time.Time) bool { return true }
+	neverInErrorWindow  = func(time.Time, time.Time) bool { return false }
+)
+
 func TestErrorTracker_IncrErrors(t *testing.T) {
 	ctx, cancel := testhelper.Context()
 	defer cancel()
 
 	writeThreshold, readThreshold := 10, 10
 
-	errors, err := newErrors(ctx, time.Second, uint32(readThreshold), uint32(writeThreshold))
+	errors, err := newErrors(ctx, alwaysInErrorWindow, uint32(readThreshold), uint32(writeThreshold))
 	require.NoError(t, err)
 
 	node := "backend-node-1"
@@ -36,8 +41,8 @@ func TestErrorTracker_IncrErrors(t *testing.T) {
 
 	assert.True(t, errors.ReadThresholdReached(node))
 
-	// use negative value for window so we are ensured to clear all of the errors in the queue
-	errors, err = newErrors(ctx, -time.Second, uint32(readThreshold), uint32(writeThreshold))
+	// Use `neverInErrorWindow()` to make sure that all errors in the queue are cleared.
+	errors, err = newErrors(ctx, neverInErrorWindow, uint32(readThreshold), uint32(writeThreshold))
 	require.NoError(t, err)
 
 	errors.clear()
@@ -51,7 +56,7 @@ func TestErrorTracker_Concurrency(t *testing.T) {
 	defer cancel()
 
 	readAndWriteThreshold := 10
-	errors, err := newErrors(ctx, 1*time.Second, uint32(readAndWriteThreshold), uint32(readAndWriteThreshold))
+	errors, err := newErrors(ctx, alwaysInErrorWindow, uint32(readAndWriteThreshold), uint32(readAndWriteThreshold))
 	require.NoError(t, err)
 
 	node := "backend-node-1"
@@ -80,7 +85,7 @@ func TestErrorTracker_ClearErrors(t *testing.T) {
 	defer cancel()
 
 	writeThreshold, readThreshold := 10, 10
-	errors, err := newErrors(ctx, time.Second, uint32(readThreshold), uint32(writeThreshold))
+	errors, err := newErrors(ctx, alwaysInErrorWindow, uint32(readThreshold), uint32(writeThreshold))
 	require.NoError(t, err)
 
 	node := "backend-node-1"
@@ -88,10 +93,11 @@ func TestErrorTracker_ClearErrors(t *testing.T) {
 	errors.IncrWriteErr(node)
 	errors.IncrReadErr(node)
 
-	clearBeforeNow := time.Now()
-
-	errors.olderThan = func() time.Time {
-		return clearBeforeNow
+	now := time.Now()
+	errors.isInErrorWindow = func(_ time.Time, errTime time.Time) bool {
+		// Consider all errors which have been added until now to not be part of the error
+		// window anymore. All new events will be considered part of it though.
+		return errTime.After(now)
 	}
 
 	errors.IncrWriteErr(node)
@@ -107,7 +113,7 @@ func TestErrorTracker_Expired(t *testing.T) {
 	defer cancel()
 
 	threshold := 10
-	errors, err := newErrors(ctx, 10*time.Second, uint32(threshold), uint32(threshold))
+	errors, err := newErrors(ctx, alwaysInErrorWindow, uint32(threshold), uint32(threshold))
 	require.NoError(t, err)
 
 	node := "node"
