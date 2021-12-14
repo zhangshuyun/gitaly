3f75bd26c7b4b4e6c87e0550c7f10a121fe45069
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-12-14 09:31:04 +0100

    testcfg: Return binary paths from build helpers
    The build helpers we use do not return the path to the binary they have
    built, so callers have to manually reconstruct paths if they rely on
    them. Convert them to do so.

--- internal/testhelper/testcfg/build.go
+++ internal/testhelper/testcfg/build.go
@@ -17,37 +17,41 @@ import (
 var buildOnceByName sync.Map
 
 // BuildGitalyGit2Go builds the gitaly-git2go command and installs it into the binary directory.
-func BuildGitalyGit2Go(t testing.TB, cfg config.Cfg) {
-	BuildBinary(t, cfg.BinDir, gitalyCommandPath("gitaly-git2go"))
+func BuildGitalyGit2Go(t testing.TB, cfg config.Cfg) string {
+	binaryPath := BuildBinary(t, cfg.BinDir, gitalyCommandPath("gitaly-git2go"))
+	symlinkPath := filepath.Join(cfg.BinDir, "gitaly-git2go-"+version.GetModuleVersion())
+
 	// The link is needed because gitaly uses version-named binary.
 	// Please check out https://gitlab.com/gitlab-org/gitaly/-/issues/3647 for more info.
-	if err := os.Link(filepath.Join(cfg.BinDir, "gitaly-git2go"), filepath.Join(cfg.BinDir, "gitaly-git2go-"+version.GetModuleVersion())); err != nil {
+	if err := os.Link(binaryPath, symlinkPath); err != nil {
 		if errors.Is(err, os.ErrExist) {
-			return
+			return symlinkPath
 		}
 		require.NoError(t, err)
 	}
+
+	return symlinkPath
 }
 
 // BuildGitalyLFSSmudge builds the gitaly-lfs-smudge command and installs it into the binary
 // directory.
-func BuildGitalyLFSSmudge(t *testing.T, cfg config.Cfg) {
-	BuildBinary(t, cfg.BinDir, gitalyCommandPath("gitaly-lfs-smudge"))
+func BuildGitalyLFSSmudge(t *testing.T, cfg config.Cfg) string {
+	return BuildBinary(t, cfg.BinDir, gitalyCommandPath("gitaly-lfs-smudge"))
 }
 
 // BuildGitalyHooks builds the gitaly-hooks command and installs it into the binary directory.
-func BuildGitalyHooks(t testing.TB, cfg config.Cfg) {
-	BuildBinary(t, cfg.BinDir, gitalyCommandPath("gitaly-hooks"))
+func BuildGitalyHooks(t testing.TB, cfg config.Cfg) string {
+	return BuildBinary(t, cfg.BinDir, gitalyCommandPath("gitaly-hooks"))
 }
 
 // BuildGitalySSH builds the gitaly-ssh command and installs it into the binary directory.
-func BuildGitalySSH(t testing.TB, cfg config.Cfg) {
-	BuildBinary(t, cfg.BinDir, gitalyCommandPath("gitaly-ssh"))
+func BuildGitalySSH(t testing.TB, cfg config.Cfg) string {
+	return BuildBinary(t, cfg.BinDir, gitalyCommandPath("gitaly-ssh"))
 }
 
 // BuildPraefect builds the praefect command and installs it into the binary directory.
-func BuildPraefect(t testing.TB, cfg config.Cfg) {
-	BuildBinary(t, cfg.BinDir, gitalyCommandPath("praefect"))
+func BuildPraefect(t testing.TB, cfg config.Cfg) string {
+	return BuildBinary(t, cfg.BinDir, gitalyCommandPath("praefect"))
 }
 
 var (
