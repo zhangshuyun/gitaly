80e9c85eab630c691f7e6c7ad6c4a0c50ff6d9e8
Patrick Steinhardt <psteinhardt@gitlab.com>
2021-12-15 08:38:05 +0100

    objectpool: Convert to set hooks path via config
    The objectpool package overrides the hooks path by setting the global
    `config.OverrideHooksPath` variable, which is deprecated and about to be
    removed. Convert it to instead set up the hooks path via the Gitaly
    configuration.

--- internal/git/objectpool/testhelper_test.go
+++ internal/git/objectpool/testhelper_test.go
@@ -17,16 +17,15 @@ import (
 )
 
 func TestMain(m *testing.M) {
-	testhelper.Run(m, testhelper.WithSetup(func() error {
-		config.OverrideHooksPath = "/"
-		return nil
-	}))
+	testhelper.Run(m)
 }
 
 func setupObjectPool(t *testing.T, ctx context.Context) (*ObjectPool, *gitalypb.Repository) {
 	t.Helper()
 
 	cfg, repo, _ := testcfg.BuildWithRepo(t)
+	// We have no Gitaly server set up in these tests, so we cannot use hooks.
+	cfg.Git.HooksPath = "/"
 	gitCommandFactory := git.NewExecCommandFactory(cfg)
 
 	catfileCache := catfile.NewCache(cfg)
