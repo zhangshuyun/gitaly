71b783e6df3f5a749503daba840fe2ee6f2eab9f
Pavlo Strokov <pstrokov@gitlab.com>
2021-12-20 22:30:30 +0200

    conflicts: Drop support of the JSON encoded flag
    As we have migrated git2go sub-commands 'conflicts'
    to the gob serialization format in 7bb310e1e (git2go:
    Use gob for conflicts sub-command invocation,
    2021-11-03) and released it, it's time to drop backwards
    compatibility code that was used for smooth version
    upgrade.

--- internal/git2go/conflicts.go
+++ internal/git2go/conflicts.go
@@ -6,7 +6,6 @@ import (
 	"encoding/gob"
 	"errors"
 	"fmt"
-	"io"
 
 	"gitlab.com/gitlab-org/gitaly/v14/internal/git/repository"
 	"google.golang.org/grpc/codes"
@@ -16,31 +15,31 @@ import (
 // ConflictsCommand contains parameters to perform a merge and return its conflicts.
 type ConflictsCommand struct {
 	// Repository is the path to execute merge in.
-	Repository string `json:"repository"`
+	Repository string
 	// Ours is the commit that is to be merged into theirs.
-	Ours string `json:"ours"`
+	Ours string
 	// Theirs is the commit into which ours is to be merged.
-	Theirs string `json:"theirs"`
+	Theirs string
 }
 
 // ConflictEntry represents a conflict entry which is one of the sides of a conflict.
 type ConflictEntry struct {
 	// Path is the path of the conflicting file.
-	Path string `json:"path"`
+	Path string
 	// Mode is the mode of the conflicting file.
-	Mode int32 `json:"mode"`
+	Mode int32
 }
 
 // Conflict represents a merge conflict for a single file.
 type Conflict struct {
 	// Ancestor is the conflict entry of the merge-base.
-	Ancestor ConflictEntry `json:"ancestor"`
+	Ancestor ConflictEntry
 	// Our is the conflict entry of ours.
-	Our ConflictEntry `json:"our"`
+	Our ConflictEntry
 	// Their is the conflict entry of theirs.
-	Their ConflictEntry `json:"their"`
+	Their ConflictEntry
 	// Content contains the conflicting merge results.
-	Content []byte `json:"content"`
+	Content []byte
 }
 
 // ConflictError is an error which happened during conflict resolution.
@@ -54,28 +53,9 @@ type ConflictError struct {
 // ConflictsResult contains all conflicts resulting from a merge.
 type ConflictsResult struct {
 	// Conflicts
-	Conflicts []Conflict `json:"conflicts"`
+	Conflicts []Conflict
 	// Error is an optional conflict error
-	Error ConflictError `json:"error"`
-}
-
-// ConflictsCommandFromSerialized constructs a ConflictsCommand from its serialized representation.
-func ConflictsCommandFromSerialized(serialized string) (ConflictsCommand, error) {
-	var request ConflictsCommand
-	if err := deserialize(serialized, &request); err != nil {
-		return ConflictsCommand{}, err
-	}
-
-	if err := request.verify(); err != nil {
-		return ConflictsCommand{}, fmt.Errorf("conflicts: %w: %s", ErrInvalidArgument, err.Error())
-	}
-
-	return request, nil
-}
-
-// SerializeTo serializes the conflicts result and writes it into the writer.
-func (m ConflictsResult) SerializeTo(writer io.Writer) error {
-	return serializeTo(writer, m)
+	Error ConflictError
 }
 
 // Conflicts performs a merge via gitaly-git2go and returns all resulting conflicts.
