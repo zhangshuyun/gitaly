e1c29d571ea1db571681f865c42fd795a2c97b3b
Pavlo Strokov <pstrokov@gitlab.com>
2021-12-23 17:31:06 +0200

    ref: Sort results of ListRefs
    In order to provide more flexibility to our API
    we are extending ListRefs RPC with sorting.
    The caller now can set a sorting key and a direction
    in which the results will be returned back.
    
    Closes: https://gitlab.com/gitlab-org/gitaly/-/issues/3855
    Change: added

--- internal/gitaly/service/ref/list_refs.go
+++ internal/gitaly/service/ref/list_refs.go
@@ -2,6 +2,7 @@ package ref
 
 import (
 	"errors"
+	"fmt"
 	"strings"
 
 	"gitlab.com/gitlab-org/gitaly/v14/internal/git"
@@ -34,10 +35,12 @@ func (s *server) ListRefs(in *gitalypb.ListRefsRequest, stream gitalypb.RefServi
 		patterns = append(patterns, string(pattern))
 	}
 
+	sorting := sortDirectionByEnum[in.GetSortBy().GetDirection()] + sortKeyByEnum[in.GetSortBy().GetKey()]
 	opts := buildFindRefsOpts(ctx, nil)
 	opts.cmdArgs = []git.Option{
 		// %00 inserts the null character into the output (see for-each-ref docs)
-		git.Flag{Name: "--format=" + strings.Join(localBranchFormatFields, "%00")},
+		git.ValueFlag{Name: "--format", Value: strings.Join(localBranchFormatFields, "%00")},
+		git.ValueFlag{Name: "--sort", Value: sorting},
 	}
 
 	return s.findRefs(ctx, writer, repo, patterns, opts)
@@ -49,6 +52,16 @@ func validateListRefsRequest(in *gitalypb.ListRefsRequest) error {
 	}
 	if len(in.GetPatterns()) < 1 {
 		return errors.New("patterns must have at least one entry")
+	}
+
+	if sortBy := in.GetSortBy(); sortBy != nil {
+		if _, found := sortKeyByEnum[sortBy.GetKey()]; !found {
+			return fmt.Errorf("sorting key %q is not supported", sortBy.GetKey())
+		}
+
+		if _, found := sortDirectionByEnum[sortBy.GetDirection()]; !found {
+			return errors.New("sorting direction is not supported")
+		}
 	}
 
 	return nil
@@ -80,4 +93,16 @@ func newListRefsWriter(stream gitalypb.RefService_ListRefsServer, headOID git.Ob
 
 		return stream.Send(&gitalypb.ListRefsResponse{References: refNames})
 	}
+}
+
+var sortKeyByEnum = map[gitalypb.ListRefsRequest_SortBy_Key]string{
+	gitalypb.ListRefsRequest_SortBy_REFNAME:       "refname",
+	gitalypb.ListRefsRequest_SortBy_CREATORDATE:   "creatordate",
+	gitalypb.ListRefsRequest_SortBy_AUTHORDATE:    "authordate",
+	gitalypb.ListRefsRequest_SortBy_COMMITTERDATE: "committerdate",
+}
+
+var sortDirectionByEnum = map[gitalypb.SortDirection]string{
+	gitalypb.SortDirection_ASCENDING:  "",
+	gitalypb.SortDirection_DESCENDING: "-",
 }
