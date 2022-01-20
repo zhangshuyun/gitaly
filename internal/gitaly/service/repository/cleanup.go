a9e0258230823e2630eeed3ae424840b2569c868
Pavlo Strokov <pstrokov@gitlab.com>
2022-01-20 19:55:27 +0200

    repository: Extend test coverage for Cleanup RPC
    If repository is not provided the RPC returns specific
    error about it.
    Make sure NotFound code and specific message is returned
    in case requested repository doesn't exist on the disk.

--- internal/gitaly/service/repository/cleanup.go
+++ internal/gitaly/service/repository/cleanup.go
@@ -3,11 +3,17 @@ package repository
 import (
 	"context"
 
+	gitalyerrors "gitlab.com/gitlab-org/gitaly/v14/internal/errors"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/git/housekeeping"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/helper"
 	"gitlab.com/gitlab-org/gitaly/v14/proto/go/gitalypb"
 )
 
 func (s *server) Cleanup(ctx context.Context, in *gitalypb.CleanupRequest) (*gitalypb.CleanupResponse, error) {
+	if in.GetRepository() == nil {
+		return nil, helper.ErrInvalidArgument(gitalyerrors.ErrEmptyRepository)
+	}
+
 	repo := s.localrepo(in.GetRepository())
 
 	if err := housekeeping.CleanupWorktrees(ctx, repo); err != nil {
