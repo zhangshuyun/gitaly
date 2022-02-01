58900ff4dc926dc33ef857910cfb559b6380ef6f
Sami Hiltunen <shiltunen@gitlab.com>
2022-02-01 11:47:02 +0200

    Disable metadata creation hack in diff service tests
    This commit amends the diff service's tests to not rely on Praefect's
    metadata force creation hack. The hack creates metadata for repositories
    when Praefect first sees a request for the repository. This has potential
    to hide problems in tests and doesn't match the production scenario. To
    prepare for removing the hack entirely, this commit refactors the tests
    to create the test repositories through the API rather than placing them
    directly in the storage.

--- internal/gitaly/service/diff/testhelper_test.go
+++ internal/gitaly/service/diff/testhelper_test.go
@@ -1,11 +1,14 @@
 package diff
 
 import (
+	"context"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/git/gittest"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/gitaly/config"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/gitaly/service"
+	"gitlab.com/gitlab-org/gitaly/v14/internal/gitaly/service/repository"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper/testcfg"
 	"gitlab.com/gitlab-org/gitaly/v14/internal/testhelper/testserver"
@@ -18,18 +21,36 @@ func TestMain(m *testing.M) {
 }
 
 func setupDiffService(t testing.TB, opt ...testserver.GitalyServerOpt) (config.Cfg, *gitalypb.Repository, string, gitalypb.DiffServiceClient) {
-	cfg, repo, repoPath := testcfg.BuildWithRepo(t)
+	cfg := testcfg.Build(t)
+
+	opt = append(opt, testserver.WithDisableMetadataForceCreation())
+
 	addr := testserver.RunGitalyServer(t, cfg, nil, func(srv *grpc.Server, deps *service.Dependencies) {
 		gitalypb.RegisterDiffServiceServer(srv, NewServer(
 			deps.GetLocator(),
 			deps.GetGitCmdFactory(),
 			deps.GetCatfileCache(),
 		))
+		gitalypb.RegisterRepositoryServiceServer(srv, repository.NewServer(
+			cfg,
+			deps.GetRubyServer(),
+			deps.GetLocator(),
+			deps.GetTxManager(),
+			deps.GetGitCmdFactory(),
+			deps.GetCatfileCache(),
+			deps.GetConnsPool(),
+			deps.GetGit2goExecutor(),
+		))
 	}, opt...)
+	cfg.SocketPath = addr
 
 	conn, err := grpc.Dial(addr, grpc.WithInsecure())
 	require.NoError(t, err)
 	t.Cleanup(func() { testhelper.MustClose(t, conn) })
+
+	repo, repoPath := gittest.CreateRepository(context.TODO(), t, cfg, gittest.CreateRepositoryConfig{
+		Seed: gittest.SeedGitLabTest,
+	})
 
 	return cfg, repo, repoPath, gitalypb.NewDiffServiceClient(conn)
 }
