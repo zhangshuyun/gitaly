52625870d2f973dc56128fb2aadb26715b521b2f
Patrick Steinhardt <psteinhardt@gitlab.com>
2022-02-28 09:14:50 +0100

    git: Enable use of bundled Git by default
    With commit 073727abd (git: Add a feature flag to toggle between bundled
    and external Git, 2022-01-13) we have added the ability to switch
    between bundled and distributed Git via a feature flag in case both have
    been enabled via Gitaly's configuration. We have rolled out the use of
    bundled Git in production on February 23rd without any observed issues
    so far.
    
    Enable the use of bundled Git by default in case it's enabled in the
    configuration. We do not remove the flag right away given that we don't
    have coverage in internal operations, where we don't have a gRPC context
    injected by Rails available. This change thus further increases coverage
    of where we use bundled Git.
    
    Changelog: changed

--- internal/metadata/featureflag/ff_use_bundled_git.go
+++ internal/metadata/featureflag/ff_use_bundled_git.go
@@ -2,4 +2,4 @@ package featureflag
 
 // UseBundledGit enables the use of bundled Git if the Gitaly configuration has both a binary path
 // and the bundled Git enabled.
-var UseBundledGit = NewFeatureFlag("use_bundled_git", false)
+var UseBundledGit = NewFeatureFlag("use_bundled_git", true)
